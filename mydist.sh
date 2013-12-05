e947b1bd91d30c441671f905c3278207212d850a
CIAX <ciax@naoj.org>
2013-12-04 16:06:31 -1000

    add mydist,myos;

---
+++ mydist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+case `uname` in
+    Linux)
+        set - $(< /etc/issue)
+        case $1 in
+            Debian*)
+                echo "debian${3%.*}";;
+            Ubuntu*)
+                echo "ubuntu${2%.*}";;
+            White*)
+                echo "redhat${6%.*}";;
+            *);;
+        esac;;
+    SunOS)
+        ver=$(uname -r)
+        echo "sunos${ver%.*}";;
+    *);;
+esac
