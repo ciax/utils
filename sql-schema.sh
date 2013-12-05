15b0ab93487365dfe4f84aa3786d9a00f157a148
CIAX <ciax@naoj.org>
2013-12-05 10:57:47 -1000

    sql-schema/ use process subst for while loop to use global variables.   If pipe is used to while loop, it behaves as another process and inside var becomes independent from outside;

--- sql-schema.sh
+++ sql-schema.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
-# Required script: set.usage.sh, set.tempfile.sh, db-expand.pl
-# Required command: cat
-#schema file (sc-*.tsv) contains 'table' and 'field' separated by <tab>
-#The primary key 'id' is automatically added to each tables.
-#If a 'field' content matches with a name of another 'table',
-#it is treated as a "foreign key" refering to the 'id' field of the table.
-#So the refered table must be shown above those field of line.
+# Required script: set.usage.sh, db-expand.pl
+#  schema file (sc-*.tsv) contains 'table' and 'field' separated by <tab>
+#  The primary key 'id' is automatically added to each tables.
+#  If a 'field' content matches with a name of another 'table',
+#  it is treated as a "foreign key" refering to the 'id' field of the table.
+#  So the refered table must be shown above those field of line.
 [ -e "$1" ] || . set.usage "[schema tsv file]"
-. set.tempfile drop create rstr
-db-expand $1|while read tbl fld; do
-if [ "$table" != "$tbl" ]; then
-    cat $rstr >> $create
-    [ -s $create ] && echo ");" >> $create
-    table=$tbl
-    tables="$tables $tbl"
-    echo "drop table if exists $tbl;" >> $drop
-    echo -n "create table $tbl ('id' primary key" >> $create
-    echo -n '' > $rstr
-fi
-echo -n ",'$fld'" >> $create
-case "$tables" in
-    *$fld*) echo -n ",foreign key('$fld') references $fld('id')" >> $rstr;;
-    *);;
-esac
-done
 echo "begin;"
 echo "pragma foreign_keys=on;"
-cat $drop
-cat $create
+while read tbl fld; do
+    if [ "$table" != "$tbl" ]; then
+        create="$create$rstr"
+        [ "$create" ] && create="$create);\n"
+        table=$tbl
+        tables="$tables $tbl"
+        echo "drop table if exists $tbl;"
+        create="${create}create table $tbl ('id' primary key"
+        rstr=
+    fi
+    create="$create,'$fld'"
+    case "$tables" in
+        *$fld*) rstr="$rstr,foreign key('$fld') references $fld('id')";;
+        *);;
+    esac
+done < <(db-expand $1)
+# Process Substitution makes the internal(while do) var effective at outside of that
+echo -en $create
 echo ");"
 echo "commit;"
