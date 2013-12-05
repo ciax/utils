f17601ebca1024a271483f53c45a741b30bc1e9e
CIAX <ciax@naoj.org>
2013-12-04 14:42:32 -1000

    file-owner (chx) is added

---
+++ file-owner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# File owner will be uniformed under the current or specified dir tree
+# Usage file-owner [dir..]
+_chx(){
+    local dir=${1:-.} SU=
+    [ -d "$dir" ] || return 1
+    local ug=$(stat -c "%U:%G" $dir)
+    echo "`cd $dir;pwd -P`($ug)"
+    sudo chown -R $ug $dir
+}
+if [ "$1" ] ;then
+    for i ; do
+        _chx $i
+    done
+else
+    _chx .
+fi
