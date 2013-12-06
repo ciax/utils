48fc16655ebaad3b3e9a3bdbe64bbd7dfad79286
CIAX <ciax@naoj.org>
2013-12-06 12:11:59 -1000

    sch-device/ add feature for setting unique restriction to the field;

--- sql-schema.sh
+++ sql-schema.sh
@@ -3,17 +3,20 @@
 #
 ## schema file (sch-*.tsv) contains 'table' and 'field' separated by <tab>
 ## The 'id' field is automatically added to the table as 'primary key'.
-## If table name has ':' at the end, auto-incremented 'id' is set to the table.
+## If ':' is attached to the table name, auto-incremented 'id' is set to the table.
 ## If the following 'field' content matches with a name of another 'table',
 ## it is treated as a "foreign key" refering to the 'id' field of the corresponding table.
 ## So the refered table must be shown above those field of line.
-#
+## If '!' is attached to the field name, this field is set to unique restriction. 
+
 [ -e "$1" ] || . set.usage "[schema tsv file]"
-sfx=':'
+tsfx=':'
+fsfx='!'
 echo "begin;"
 echo "pragma foreign_keys=on;"
 while read key val; do
-    tbase=${key%"$sfx"}
+    tbase=${key%"$tsfx"}
+    fbase=${val%"$fsfx"}
     if [ "$crnt" != "$tbase" ]; then
         create="$create$rstr"
         [ "$create" ] && create="$create);\n"
@@ -22,14 +25,17 @@ while read key val; do
         echo "drop table if exists $crnt;"
         rstr=
         create="${create}create table $crnt ('id'"
-        if [[ "$key" != *"$sfx" ]] ; then
+        if [ "$key" = "$tbase" ] ; then
             create="$create primary key"
         else
             create="$create integer primary key autoincrement"
         fi
     fi
-    create="$create,'$val'"
-    [[ "$tables" == *$val* ]] && rstr="$rstr,foreign key('$val') references $val('id')"
+    create="$create,'$fbase'"
+    [ "$val" = "$fbase" ] || create="$create unique"
+    if [[ "$tables" == *$fbase* ]] ; then
+        rstr="$rstr,foreign key('$fbase') references $val('id')"
+    fi
 done < <(db-expand $1)
 # Process Substitution makes the internal(while do) var effective at outside of that
 echo -en $create
