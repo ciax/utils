796602bf749a7c557992b2bd72b6c086d6c67029
CIAX <ciax@naoj.org>
2013-12-05 14:02:55 -1000

    using file-move;

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Required script: set.tempfile.sh, ssh-perm.sh
+# Required script: set.tempfile.sh, ssh-trim.sh
 # Required command: ssh,ssh-keygen,grep,cut,cat,sort,cmp
 # Usage: ${0##*/} (-r:remove keys)
 ath=~/.ssh/authorized_keys
@@ -11,21 +11,18 @@ type ssh > /dev/null || apt-get install ssh
 [ -e $sec -a -e $pub ] || ssh-keygen
 [ -e $ath ] || touch $ath
 [ -e $inv ] || touch $inv
-# Make temp
-. set.tempfile tinv tath
+#
+# Add my id_rsa.pub to authorized_keys (if not exist)
+#
 # If the line with own name is found in authorized_keys,
 # maching own id_rsa.pub and the line, otherwise move older one to invalid_keys
 me=`cut -d' ' -f3 $pub`
 key=`cut -d' ' -f2 $pub`
+# Make temp
+. set.tempfile tinv tath
 { egrep "$me" $ath|grep -v "$key"|cut -d' ' -f2;cat $inv; }|sort -u > $tinv
 { grep -v "$me" $ath;cat $pub; }|sort -u > $tath
 # No change, no rewrite
-if ! cmp -s $ath $tath ; then
-    cp $tath $ath
-    echo "added new key to authorized_keys"
-fi
-if ! cmp -s $inv $tinv ; then
-    cp $tinv $inv
-    echo "added new key to invalid_keys"
-fi
-ssh-perm
+file-move $tath $ath && echo "added new key to authorized_keys"
+file-move $tinv $inv && echo "added new key to invalid_keys"
+ssh-trim
--- ssh-trim.sh
+++ ssh-trim.sh
@@ -2,16 +2,15 @@
 # Required script: set.tempfile, file-move.sh, ssh-perm.sh
 # Required command: cp,cut,grep,sort
 # Remove dup key from authorized_keys
-auth=~/.ssh/authorized_keys
+ath=~/.ssh/authorized_keys
 inv=~/.ssh/invalid_keys
 pub=~/.ssh/id_rsa.pub
-me=`cut -d' ' -f3 $pub`
 . set.tempfile tath1 tath2 tinv1 tinv2
 #
 # Split file into #headed line(invalid keys) and others
 #
-egrep -v '[#*]' $auth|grep '.' > $tath1
-egrep '^#' $auth > $tinv1
+egrep -v '[#*]' $ath|grep '.' > $tath1
+egrep '^#' $ath > $tinv1
 if [ -f "$inv" ] ; then
     while read key; do
         grep -v "$key" $tath1 > $tath2
@@ -20,5 +19,5 @@ if [ -f "$inv" ] ; then
     done < <(grep '.' $inv $tinv|cut -d' ' -f2|sort -u)
     file-move $tinv2 $inv
 fi
-file-move $tath1 $auth
+file-move $tath1 $ath
 ssh-perm
