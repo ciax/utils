45e24747e28e9f7c2e638e694c721bc198b176cb
CIAX <ciax@naoj.org>
2013-12-09 12:01:21 -1000

    add edit-column for switching column of file;

---
+++ edit-column.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Reorder columns except key
+[ "$2" ] || . set.usage "[order(1,2,3..)] [file] (-w)"
+order="\$${1//,/,\$}"
+file=$2
+. set.tempfile tmp1
+<$file awk -F "\t" "BEGIN{ OFS=FS; }{ print $order; }" > $tmp1
+if [ "$3" = "-w" ] ; then
+    file-move $tmp1 $file
+else
+    cat $tmp1
+fi
