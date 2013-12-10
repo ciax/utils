d3e670f6150cbcd5794cbb3a57fad13e3ee1075c
CIAX <ciax@naoj.org>
2013-12-10 11:21:38 -1000

    sql-make/ input only relevant table in sch-device.tsv

--- sql-make.sh
+++ sql-make.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+[ "$1" ] && exec 1> >(sqlite3 $1)
 cd ~/db
 sql-schema sch-device.tsv
-for i in *.tsv; do
-    sql-insert $i
-done
+while read name other; do
+    file="db-$name.tsv"
+    [ -e "$file" ] && sql-insert $file
+done < <(grep '^[a-z]' sch-device.tsv)
