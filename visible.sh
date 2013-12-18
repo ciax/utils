9ce7b8418114f61cf685bef98759edf504b07128
Koji Omata <omata@naoj.org>
2013-12-17 20:38:05 -1000

    visible tab

--- visible.sh
+++ visible.sh
@@ -8,6 +8,8 @@
 for c in `od -v -A n -t x1 $1` ; do
     if [ "$c" = '0a' -a "$nl" ] ; then
         echo
+    elif [ "$c" = '09' -a "$nl" ] ; then
+        echo -en "$C3>$C0\t"
     elif [ "$c" \< '20' -o "$c" \> '80' ] ; then
         echo -n "$C4($c)$C0"
         unset nl
