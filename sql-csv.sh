8a1353e74a5ec0d273a9d996581b6cd71489cac5
CIAX <ciax@naoj.org>
2014-01-28 13:11:47 -1000

    merge

--- sql-csv.sh
+++ sql-csv.sh
@@ -32,14 +32,9 @@ schema(){
 insert(){
     pfx="insert or ignore into $1 values ('"
     while read line ;do
-<<<<<<< HEAD
-        echo "$pfx${line//,/','}');"
-    done < <(egrep -v '^([#!].*|[ \t]*)$' $1.csv)
-=======
         list="${line//,/','}'"
         echo "$pfx${list//\'\'/null});"
-    done < <(egrep -v '^([#!].*|[	 ]*)$' $1.csv)
->>>>>>> 9ae05baa7ed0d76dfd14497236ebc0780664627c
+    done < <(egrep -v '^([#!].*|[ \t]*)$' $1.csv)
 }
 echo "begin;"
 echo "pragma foreign_keys=on;"
