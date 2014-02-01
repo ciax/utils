a3d7e154f081c6bf6d63cd63da51360bba20042f
CIAX <ciax@naoj.org>
2014-01-31 16:38:12 -1000

    sql-schema/ adapt to both db-*.tsv and *.csv files;

--- sql-schema.sh
+++ sql-schema.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
-# Required script: set.usage.sh, db-expand.pl
+# Required script: set.usage.sh
 #
-## schema file (sch-*.tsv) contains 'table' and 'field' separated by <tab>
 ## The 'id' field is automatically added to the table as 'primary key'.
-## If ':' is attached to the table name, auto-incremented 'id' is set to the table.
 ## If the following 'field' content matches with a name of another 'table',
 ## it is treated as a "foreign key" refering to the 'id' field of the corresponding table.
-## So the refered table must be shown above those field of line.
-## If '!' is attached to the field name, this field is set to unique restriction. 
 
-[ -e "$1" ] || . set.usage "[schema tsv file]"
-tsfx=':'
-fsfx='!'
+schema(){
+    local body="${1#db-}"
+    local tbl="${body%.$ext}"
+    [[ "$tables" == *$tbl* ]] && return
+    tables="$tables $tbl"
+    local drop="drop table if exists $tbl;"
+    local create="create table $tbl ('id' primary key"
+    local fkeys
+    local col
+    local db
+    while read col other; do
+        [ $col = '!id' ] && continue
+        for db in *$col.$ext; do
+            create="$create,'$col'"
+            fkeys="$fkeys $col"
+            schema $db
+        done
+    done < <(egrep "^!" $1|head -1|tr ',\t' $'\n')
+    for i in $fkeys; do
+        create="$create,foreign key('$i') references $i('id')"
+    done
+    echo "$drop"
+    echo "$create);"
+}
+
+shopt -s nullglob
+[ -e "$1" ] || . set.usage "[csv|tsv file]"
+ext=${1##*.}
 echo "pragma foreign_keys=on;"
-while read key val; do
-    tbase=${key%"$tsfx"}
-    fbase=${val%"$fsfx"}
-    if [ "$crnt" != "$tbase" ]; then
-        create="$create$rstr"
-        [ "$create" ] && create="$create);\n"
-        crnt="$tbase"
-        tables="$tables $crnt"
-        echo "drop table if exists $crnt;"
-        rstr=
-        create="${create}create table $crnt ('id'"
-        if [ "$key" = "$tbase" ] ; then
-            create="$create primary key"
-        else
-            create="$create integer primary key autoincrement"
-        fi
-    fi
-    create="$create,'$fbase'"
-    [ "$val" = "$fbase" ] || create="$create unique"
-    if [[ "$tables" == *$fbase* ]] ; then
-        rstr="$rstr,foreign key('$fbase') references $fbase('id')"
-    fi
-done < <(db-expand $1)
-# Process Substitution makes the internal(while do) var effective at outside of that
-echo -en $create
-echo ");"
+schema $1
