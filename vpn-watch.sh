ea9dd52f43636db9b6f4e5da49e248bd217c3546
Koji Omata <omata@naoj.org>
2014-02-08 08:43:27 -1000

    rename alias vpn -> vpn-client;

--- vpn-watch.sh
+++ vpn-watch.sh
@@ -6,6 +6,6 @@ host="$1"
 alive=`echo "select alive from vpn where id = '$host';"|db-device`
 [ "$alive" ] || { echo "No such host"; exit 1; }
 while sleep 5;do
-    ping -c3 $alive > /dev/null 2>&1 || vpn $host
+    ping -c3 $alive > /dev/null 2>&1 || vpn-client $host
 done &
 echo $! > $pid
