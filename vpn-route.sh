1b9c8f588faac45df65f50272e5e25c783c56ac6
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-11 13:14:21 -1000

    vpn-route/ use sql command to build string;

--- vpn-route.sh
+++ vpn-route.sh
@@ -2,8 +2,4 @@
 mode="add"
 [ "$1" = "-r" ] && { shift; mode="del"; }
 [ "$1" ] || . set.usage "(-r:remove) [vpnhost]"
-while read res; do
-    net=${res%,*}
-    mask=${res#*,}
-    echo "route $mode -net $net netmask $mask"
-done < <(echo "select network,netmask from route where vpn = '$1';"|db-device)
+echo "select 'route $mode -net '||network||' netmask '||netmask from route where vpn = '$1';"|db-device
