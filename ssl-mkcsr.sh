a2eeeb11d973374737e93c771bb55355103cd1eb
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-11 11:46:55 -1000

    debug:ssl-mkcsr/ empty line was deleted by grep.;

--- ssl-mkcsr.sh
+++ ssl-mkcsr.sh
@@ -2,12 +2,10 @@
 ## SSL files (Server vs CA vs Client)
 ##@ Server
 ##  private.key(+pub.key) -> csr(Signning Request file)
-lookup(){
-    echo "select * from ssl where id = '$1';"|db-device|cut -d, -f2-|tr , '\n'|grep .
-}
 . ssl-newkey $1
 . set.tempfile input
-if lookup $site > $input; then
+echo "select * from ssl where id = '$1';"|db-device|cut -d, -f2-|tr , '\n' > $input
+if [ -s $input ] ; then
     openssl req -new -key $site.key <$input > $site.csr
 else
     openssl req -new -key $site.key > $site.csr
