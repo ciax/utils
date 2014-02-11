efbdaf6c3754f68668d77bba58622e0eb2d3db3f
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-10 15:29:14 -1000

    cfg-openvpn/ make client cfg only, use db for routing;

--- cfg-openvpn.sh
+++ cfg-openvpn.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Client for dd-wrt openvpn server
-commoncfg(){
-    cat<<EOF
+[ "$1" ] || . set.usage "[remote host]"
+remote=$1
+vardir=$HOME/.var
+host=`hostname`
+cfgfile=~/.var/openvpn-$remote.conf
+net=$(echo "select network,netmask from route where vpn = '$remote';"|db-device|tr ',' ' ')
+[ "$net" ] || { echo "No such host in DB"; exit; }
+cat > $cfgfile <<EOF
 script-security 2
 verb 3
 comp-lzo
@@ -10,50 +16,17 @@ management localhost 7505
 proto udp
 dev tun
 daemon
-ca $keydir/ca.crt
-EOF
-}
-clientcfg(){
-  commoncfg
-  cat<<EOF
-cert $keydir/$host.crt
-key $keydir/$host.key
+ca $vardir/ca.crt
+cert $vardir/$host.crt
+key $vardir/$host.key
 client
 tls-client
-remote $rhost 1194
+remote $remote 1194
 resolv-retry infinite
 nobind
-persist-key
-persist-tun
+#persist-key
+#persist-tun
 ns-cert-type server
-route 172.16.$ch.0 255.255.255.0
+route $net
 status /var/log/openvpn-status.log
 EOF
-  [ "$proxy" ] && cat <<EOF
-http-proxy-retry
-http-proxy $proxy
-EOF
-}
-
-servercfg(){
-  commoncfg
-  cat<<EOF
-cert $keydir/server.crt
-key $keydir/server.key
-dh $keydir/dh1024.pem
-port 1194
-tls-server
-ifconfig 10.0.$ch.1 10.0.$ch.2
-EOF
-}
-
-[ "$1" ] || . set.usage "[server|host(coqui/sansho/oyabi)]"
-keydir=$HOME/.var
-host=`hostname`
-case $1 in
-    server) servercfg;exit;;
-    coqui)  ch=1;clientcfg;;
-    sansho) ch=2;proxy="proxy 3128";clientcfg;;
-    oyabi)  ch=3;clientcfg;;
-    *);;
-esac
