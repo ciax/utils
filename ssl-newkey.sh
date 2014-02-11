ec05a4f1c0c3fb0ffc45679d9a627bf542c533ad
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-10 15:27:00 -1000

    ssl-newkey/add error handling: error if no entry of the site in the db;

--- ssl-newkey.sh
+++ ssl-newkey.sh
@@ -5,7 +5,14 @@
 [ "$1" ] || . set.usage "[site name]"
 cd ~/.var
 site="$1"
-[ -e "$site.key" ] || openssl genrsa 2048 > $site.key
-echo "select * from ssl where id = '$site';"|db-device|tr ',' '\n'\
-|{ read;cat; }|openssl req -new -key $site.key > $site.csr
+IFS=','
+set - $(echo "select * from ssl where id = '$site';"|db-device)
+shift
+[ "$1" ] || { echo "No such site"; exit; }
+if [ -e "$site.key" ] ;then
+    echo "[[$site.key exists]]"
+else
+    openssl genrsa 2048 > $site.key
+fi
+for i;do echo $i;done|openssl req -new -key $site.key > $site.csr
 echo
