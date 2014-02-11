1527e886f8453076579447cb48a6e42e9354f228
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-10 19:16:11 -1000

    rename ssl-sign/ use CA options;

---
+++ ssl-sign.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## SSL files (Server vs CA vs Client)
+##@ Server Site
+##  csr(Signning Request file) -> Submit to CA
+##@ CA
+## csr -> (Convert with ca.key) -> crt(Certificate) -> Send back to Server
+[ "$1" ] || . set.usage "[ca name] [site name]"
+cd ~/.var
+ca=$1;shift
+[ -s "$ca.key" ] || { echo "No ca key file"; exit; }
+site=$1;shift
+. ssl-mkcsr "$site"
+[ -s $ca.srl ] || opt="-CAcreateserial"
+openssl x509 -CA $ca.crt -CAkey $ca.key -req -in $site.csr -out $site.crt $opt
+#openssl x509 -days 9999 -req -signkey $cakey < $site.csr > $site.crt
