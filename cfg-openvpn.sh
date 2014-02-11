3394771808ebb63b9640de8c7a81784991a43de4
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-10 16:24:00 -1000

    cfg-openvpn/ add -i option;

--- cfg-openvpn.sh
+++ cfg-openvpn.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Client for dd-wrt openvpn server
-[ "$1" ] || . set.usage "[remote host]"
+if [ "$1" = "-i" ]; then
+    shift
+    cfgfile=~/.var/openvpn-$1.conf
+else
+    cfgfile=/dev/stdout
+fi
+[ "$1" ] || . set.usage "(-i:make file) [remote host]"
 remote=$1
 vardir=$HOME/.var
 host=`hostname`
-cfgfile=~/.var/openvpn-$remote.conf
 net=$(echo "select network,netmask from route where vpn = '$remote';"|db-device|tr ',' ' ')
 [ "$net" ] || { echo "No such host in DB"; exit; }
 cat > $cfgfile <<EOF
@@ -24,8 +29,8 @@ tls-client
 remote $remote 1194
 resolv-retry infinite
 nobind
-#persist-key
-#persist-tun
+persist-key
+persist-tun
 ns-cert-type server
 route $net
 status /var/log/openvpn-status.log
