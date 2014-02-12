5cc5d1e4cb3fdcd8f62d9a88ef662377bed21de0
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-11 15:46:39 -1000

    ssl-check/ no output if no file;

--- ssl-check.sh
+++ ssl-check.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ## Check the consistency of SSL files
+show(){
+    [ -s "$site.$1" ] || return
+    echo -n "$1:"
+    openssl $2 -in "$site.$1" -modulus -noout| openssl md5
+}
 [ "$1" ] || . set.usage "[site]"
 cd ~/.var
 site="$1"
-echo -n "KEY:"
-openssl rsa -in "$site.key" -modulus -noout| openssl md5
-echo -n "CSR:"
-openssl req -in "$site.csr" -modulus -noout| openssl md5
-echo -n "CRT:"
-openssl x509 -in "$site.crt" -modulus -noout |openssl md5
+show key rsa
+show csr req
+show crt x509
