c84a5603a3e32cacfc86086a1d6b991de062a05e
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-13 15:45:12 -1000

    cfg-openvpn/ connected somehow;

--- cfg-openvpn.sh
+++ cfg-openvpn.sh
@@ -7,25 +7,25 @@ host=`hostname`
 server=$(echo "select host from vpn where id = '$remote';"|db-device)
 [ "$server" ] || { echo "No such host in DB"; exit; }
 cat > ${2:-/dev/stdout} <<EOF
+#verb 3
 script-security 2
-verb 3
-comp-lzo
-keepalive 15 60
-management localhost 7505
-proto udp
-dev tun
-daemon
-ca $vardir/rootca.crt
-cert $vardir/$host.crt
-key $vardir/$host.key
 client
-tls-client
-remote $server 1194
+comp-lzo
+dev tun
+proto udp
 resolv-retry infinite
 nobind
 persist-key
 persist-tun
-ns-cert-type server
+float
+daemon
+keepalive 15 60
+ca $vardir/rootca.crt
+cert $vardir/$host.crt
+key $vardir/$host.key
+remote $server 1194
 $(vpn-route $remote|cut -d' ' -f1,4,6)
 status $vardir/openvpn-status.log
+auth MD5
+ns-cert-type server
 EOF
--- rc.alias.sh
+++ rc.alias.sh
@@ -3,3 +3,6 @@
 alias rf=reg-files
 alias rwx=file-perm
 alias chx=file-owner
+alias pptp=vpn-client-pptp
+alias ovpn=vpn-client-openvpn
+alias syslog=show-syslog
