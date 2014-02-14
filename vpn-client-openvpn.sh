74307af8e91761c172ffbe77d50f56bd51a42bc9
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-13 14:26:35 -1000

    add vpn-nat;

--- vpn-client-openvpn.sh
+++ vpn-client-openvpn.sh
@@ -5,5 +5,5 @@ cfgfile=~/.var/openvpn-$1.cfg
 . set.link vpn-client
 ifconfig tun || { sudo openvpn --mktun --dev tun0;sleep 5; }
 sudo openvpn --config $cfgfile
-nat set # Need NAT setup (naoj can't look up 172 address)
+vpn-nat set # Need NAT setup (naoj can't look up 172 address)
 show-syslog openvpn
