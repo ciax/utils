33ccafc65213fe1a7627f689a3872547b434be83
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-14 18:39:55 -1000

    recall even if no file;

--- file-recall.sh
+++ file-recall.sh
@@ -2,12 +2,13 @@
 [ "$1" ] ||  . set.usage "[file] (host)"
 name="$1"
 host=${2:-$(hostname)}
-cfid=$(md5sum $1|head -c10)
 dist=$(info-dist)
-file-stash $name
-echo "select * from list;"|db-files
-maxid="select max(id) from list where name == '$name' and host == '$host' and dist == '$dist' and fid != '$cfid'"
-echo "$maxid;"|db-files
+if [ -s "$name" ] ; then
+    file-stash $name
+    cfid=$(md5sum $1|head -c10)
+    opt=" and fid != '$cfid'"
+fi
+maxid="select max(id) from list where name == '$name' and host == '$host' and dist == '$dist' $opt"
 newest="select fid from list where id == ($maxid)"
 fid=$(echo "$newest;"|db-files)
 [ "$fid" ] || { echo "No such id"; exit; }
