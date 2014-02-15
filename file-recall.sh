c16bd260a6d7b7fb5749404c85b36a493250b6be
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-14 18:45:53 -1000

    add option -r;

--- file-recall.sh
+++ file-recall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-[ "$1" ] ||  . set.usage "[file] (host)"
+# -r means pick the original one (first stashed)
+# otherwise pick last one
+
+sel="max"
+[ "$1" = -r ] && { shift;sel="min"; }
+[ "$1" ] ||  . set.usage "(-r:revert)[file] (host)"
 name="$1"
 host=${2:-$(hostname)}
 dist=$(info-dist)
@@ -8,8 +13,9 @@ if [ -s "$name" ] ; then
     cfid=$(md5sum $1|head -c10)
     opt=" and fid != '$cfid'"
 fi
-maxid="select max(id) from list where name == '$name' and host == '$host' and dist == '$dist' $opt"
-newest="select fid from list where id == ($maxid)"
+
+selid="select $sel(id) from list where name == '$name' and host == '$host' and dist == '$dist' $opt"
+newest="select fid from list where id == ($selid)"
 fid=$(echo "$newest;"|db-files)
 [ "$fid" ] || { echo "No such id"; exit; }
 echo "select base64 from content where id == '$fid';"|db-files|base64 -d > $name
