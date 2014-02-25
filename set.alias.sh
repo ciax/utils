d7ac621a32d2e1f630dee09de05117f9e90ef1c3
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-25 13:46:29 -1000

    debug:set.alias/ never insert space between # and 'alias';

--- set.alias.sh
+++ set.alias.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# Generate alias by pick up '#alias XXX' line from each files
 while read head name par; do
     file=$(realpath ${head%:*})
-    alias $name="$file${par:+ $par}"
-done < <(grep '^# *alias' ~/utils/*.sh)
+    alias $name=$file${par:+ $par}
+done < <(grep '^#alias' ~/utils/*.sh)
