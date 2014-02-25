630e586554cc8cbb30f6fbcd2b636ff8c57f2320
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-25 11:04:06 -1000

    set.tempfile/ tempfile was remain in ~/.trash;

--- set.tempfile.sh
+++ set.tempfile.sh
@@ -2,7 +2,7 @@
 # Required packages: coreutils(cat),diffutils(cmp)
 # Usage: create_tempfile [varname1] [varname2] ..
 create_tempfile(){
-    local trp="rm -f -- "
+    local trp="/bin/rm -f -- "
     local i=
     for i ; do
     local tmp=$(tempfile) || { echo "Can't make mktemp"; exit 1; }
@@ -15,7 +15,7 @@ create_tempfile(){
 # Overwrite if these are different.
 overwrite(){
     if cmp -s $1 $2 ; then
-        rm $1;return 1
+        /bin/rm $1;return 1
     else
         mv $1 $2
     fi
