cbf2f51720fa63b021d95e800351112fee13b435
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-25 14:47:58 -1000

    set.alias/ show grep result if not sourced;

--- set.alias.sh
+++ set.alias.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Generate alias by pick up '#alias XXX' line from each files
-while read head name par; do
-    alias $name=${head%:*}${par:+ $par}
-done < <(cd ~/bin;grep '^#alias' *)
+pick(){ cd ~/bin; grep '^#alias' *; }
+case $0 in
+    *set.alias*) pick;;
+    *)
+        while read head name par; do
+            alias $name=${head%:*}${par:+ $par}
+        done < <(pick)
+        ;;
+esac
