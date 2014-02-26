d970141fd43c5bed5f2f46b35ccab17d38f78e63
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-26 02:18:58 -1000

    add login-remote.sh;

---
+++ login-remote.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Required Packages: expect
+#alias lo
+getstr(){
+    db-device ' ' <<< "select command,user,host from login where id = '$1';"
+}
+[ "$1" ] || . set.usage "[host]" $(db-device <<< "select id from login;")
+id=$1;shift
+str="$(getstr $id) $*"
+tun=$(db-device <<< "select tunnel from login where id == '$id';")
+[ "$tun" ] && str="$(getstr $tun) $str"
+pass=$(db-device <<< "select password from login where id == '$id';")
+if [ "$pass" ] ; then
+    expect <(cat <<-EOF
+	set timeout 10
+	spawn -noecho $str
+	expect {
+	  "(yes/no)?" { send "yes\n" ; exp_continue };
+	  "word:" { send "$pass\n" ; exp_continue };
+	  -re ".+ " { interact };
+	}
+	EOF
+    )
+else
+    $str
+fi
