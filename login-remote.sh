14640ac7287654235131cb554e6db3295ed9e62d
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-26 12:18:56 -1000

    login-remote/ eliminate query of (yes/no)? by adding ssh option;

--- login-remote.sh
+++ login-remote.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Required Packages: expect,bsdmainutils(column)
+# Required Packages: expect,bsdmainutils(column),sed
+# Required DB:db-debice/login (!id,command,tunnel,user,password,host,rcmd)
 #alias lo
 getstr(){
-    db-device ' ' <<< "select command,user,host from login where id = '$1';"
+    db-device ' ' <<< "select command,user,host from login where id = '$1';"|sed -e 's/ssh/ssh -o StrictHostKeyChecking=no/g'
 }
 list(){
     db-device <<< 'select id from login;'|sort|column -c60
@@ -13,17 +14,15 @@ str="$(getstr $id) $*"
 tun=$(db-device <<< "select tunnel from login where id == '$id';")
 [ "$tun" ] && str="$(getstr $tun) $str"
 pass=$(db-device <<< "select password from login where id == '$id';")
+rcmd=$(db-device <<< "select rcmd from login where id == '$id';")
 if [ "$pass" ] ; then
-    expect <(cat <<-EOF
-	set timeout 10
-	spawn -noecho $str
-	expect {
-	  "(yes/no)?" { send "yes\n" ; exp_continue };
-	  "word:" { send "$pass\n" ; exp_continue };
-	  -re ".+ " { interact };
-	}
-	EOF
-    )
+    . set.tempfile expfile
+    echo "set timeout 10" > $expfile
+    echo "spawn -noecho $str" >> $expfile
+    echo "expect word: { send $pass\n };" >> $expfile
+    [ "$rcmd" ] && echo "expect -re \".+ \"  { send \"$rcmd\n\" }" >> $expfile
+    echo "interact" >> $expfile
+    expect $expfile
 else
-    $str
+    $str $rcmd
 fi
