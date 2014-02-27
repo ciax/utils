ef1539c00a259b7761d8120e1954926f060fa385
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-26 18:30:44 -1000

    add comments;

--- cfg-dnsmasq.sh
+++ cfg-dnsmasq.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#Usage: cfg-dnsmasq (subnet)
+# Usage: cfg-dnsmasq (subnet)
+# Required TBL:mac (!id,device,if,hub,host,static-ip,alive,description)
+# Required TBL:hub (!id,subnet,location,hub,description)
 net=${1:-$(lookup-net)}
 echo "select 'dhcp-host='||id,host from mac where hub in (select id from hub where subnet == '$net') and host not null;"|db-device
