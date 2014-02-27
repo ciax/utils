375a6edfa2356b7b5777d41b4e871b9d5404884a
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-26 18:19:57 -1000

    add set.field (set var as db fields); login-remote/ use set.field;

--- login-remote.sh
+++ login-remote.sh
@@ -3,23 +3,22 @@
 # Required DB:db-debice/login (!id,command,tunnel,user,password,host,rcmd)
 #alias lo
 getstr(){
-    db-device ' ' <<< "select command,user,host from login where id = '$1';"|sed -e 's/ssh/ssh -o StrictHostKeyChecking=no -t -l/g'
+    . set.field login $1;shift
+    str="${command//ssh/ssh $sshopt} $user $host $*"
 }
 list(){
     db-device <<< 'select id from login;'|sort|column -c60
 }
 [ "$1" ] || . set.usage "[host]" < <(list)
-id=$1;shift
-str="$(getstr $id) $*"
-tun=$(db-device <<< "select tunnel from login where id == '$id';")
-[ "$tun" ] && str="$(getstr $tun) $str"
-pass=$(db-device <<< "select password from login where id == '$id';")
-rcmd=$(db-device <<< "select rcmd from login where id == '$id';")
-if [ "$pass" ] ; then
+sshopt="-o StrictHostKeyChecking=no -t -l"
+getstr $1
+
+[ "$tunnel" ] && str="$(getstr $tunnel;echo $str) $str"
+if [ "$password" ] ; then
     . set.tempfile expfile
     echo "set timeout 10" > $expfile
     echo "spawn -noecho $str" >> $expfile
-    echo "expect word: { send $pass\n };" >> $expfile
+    echo "expect word: { send $password\n };" >> $expfile
     [ "$rcmd" ] && echo "expect -re \".+ \"  { send \"$rcmd\n\" }" >> $expfile
     echo "interact" >> $expfile
     expect $expfile
---
+++ set.field.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Set variables corresponding field names of table;
+[ "$2" ] || . set.usage "[table] [id]"
+tbl=$1;val=$2
+db=~/.var/db-device.sq3
+case $0 in
+    *set.field*) cmd='echo';;
+    *) cmd='eval';;
+esac
+while read var eq str;do
+    $cmd "$var='$str'"
+done < <(sqlite3 -line $db <<< "select * from $tbl where id == '$val';")
