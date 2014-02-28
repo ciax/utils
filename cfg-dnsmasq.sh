8f5ac57421e3c2cbcbf4cb31c18ebba4ce7a5720
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-27 18:54:08 -1000

    cfg-dnsmasq/ no use separator;

--- cfg-dnsmasq.sh
+++ cfg-dnsmasq.sh
@@ -3,4 +3,5 @@
 # Required TBL:mac (!id,device,if,hub,host,static-ip,alive,description)
 # Required TBL:hub (!id,subnet,location,hub,description)
 net=${1:-$(lookup-net)}
-db-register "select 'dhcp-host='||id,host from mac where hub in (select id from hub where subnet == '$net') and host not null;"
+subq="select id from hub where subnet == '$net'"
+db-register "select 'dhcp-host='||id||','||host from mac where hub in ($subq) and host not null;"
