3219d84e2080986aacb2fa98eef9ed980fa67543
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-02-27 19:05:18 -1000

    db-register/ remove unused option (-s);

--- db-register.sh
+++ db-register.sh
@@ -3,9 +3,5 @@
 #    -s:space separator
 #    -i:ini (a=b) style
 db=~/.var/db-register.sq3
-case "$1" in
-    -s) shift;opt="-separator ' '";;
-    -i) shift;opt="-line";;
-    *) opt="-separator ,";;
-esac
+[ "$1" = -i ] && { shift;opt="-line"; }
 sqlite3 $opt $db ${1:+"$1"}
