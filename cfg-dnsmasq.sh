a748ba42c947835ec71f0f5eb93b0d3d0e192fa0
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-03 13:16:25 -1000

    rename: lookup-net -> net-name;

--- cfg-dnsmasq.sh
+++ cfg-dnsmasq.sh
@@ -2,6 +2,6 @@
 # Usage: cfg-dnsmasq (subnet)
 # Required TBL:mac (!id,device,if,hub,host,static-ip,alive,description)
 # Required TBL:hub (!id,subnet,location,hub,description)
-net=${1:-$(lookup-net)}
+net=${1:-$(net-name)}
 subq="select id from hub where subnet == '$net'"
 db-register "select 'dhcp-host='||id||','||host from mac where hub in ($subq) and host not null;"
