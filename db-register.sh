ff504ca384549e7249510cf31c9a7ff3c715366e
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-03 16:50:10 -1000

    db-register/ default separator will be space;

--- db-register.sh
+++ db-register.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Usage: db-register (opt) (sql)
-#    -s:space separator
 #    -i:ini (a=b) style
 db=~/.var/db-register.sq3
 [ "$1" = -i ] && { shift;opt="-line"; }
-sqlite3 $opt $db ${1:+"$1"}
+sqlite3 -separator ' ' $opt $db ${1:+"$1"}
