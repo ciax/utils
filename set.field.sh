8b9728bb69ba0a5e3f3e7477859b58d7da97c8e6
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-03 16:57:31 -1000

    set.field/ set var of fields even if no result on sql;

--- set.field.sh
+++ set.field.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 # Set variables corresponding field names of table;
+setvar(){
+    db-register -i "$1" > $db
+    if [ -s $db ] ; then
+        while read var eq str;do
+            [ "$2" ] && str=''
+            $cmd "$var='$str'"
+        done < $db
+    else
+        setvar "select * from $tbl where id == (select min(id) from $tbl)" unset
+    fi
+}
 set -f
 [ "$2" ] || . set.usage "[id] [table1] (table2..)"
 case $0 in
     *set.field*) cmd='echo';;
     *) cmd='eval';;
 esac
-val=$1;shift
-ptbl=$1;shift
-sub="from $ptbl where id == '$val'"
+sub="'$1'";shift
 for tbl; do
-    sub="from $tbl where id == (select $tbl $sub)"
+    [ "$sql" ] && sub="(select $tbl $sql)"
+    sql="from $tbl where id == $sub"
 done
-sub="select * $sub;"
-while read var eq str;do
-    $cmd "$var='$str'"
-done < <(db-register -i "$sub")
+sql="select * $sql;"
+. set.tempfile db
+setvar "$sql"
+unset sub sql cmd
