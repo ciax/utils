79fc7b8f53f7c0f24a2af6580d3e6bd510ed33d4
Omata <omata@naoj.org>
2014-03-05 08:49:14 -1000

    add line-uniq,line-dup which is filters for ssh_keys;

---
+++ line-dup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Required script: usage.sh
+# Required packages: coreutils(sort,nkf,uniq)
+# Pick up duplicated lines
+#alias dup
+[ -t 0 ] && . set.usage " < [files]"
+prev=''
+while read line;do
+    [ "$line" = "$prev" ] && echo $line
+    prev=$line
+done < <(nkf -d|sort) | uniq
