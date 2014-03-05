39eb7352057a107470a182cd65f5a88ab4221967
Omata <omata@debone.subaru.nao.ac.jp>
2014-03-04 17:45:15 -1000

    update func

--- rc.func.sh
+++ rc.func.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 # Requied Packages: emacs,most
-e-alias (){
-    file=rc.alias.sh;
-    pushd ~/utils;
-    unalias -a;
-    emacs $file;
-    source $file;
-    git commit -m "update alias";
-    unset file;
-    popd
+e-alias(){
+    file=rc.alias.sh
+    pushd ~/utils >/dev/null
+    unalias $(egrep '^alias' $file|cut -d ' ' -f2|cut -d '=' -f1|tr '\n' ' ')
+    emacs $file
+    source $file
+    git commit -m "update alias"
+    unset file
+    popd >/dev/null
 }
 e-func(){
-    file=rc.func.sh;
-    pushd ~/utils;
-    for i in `egrep -o "^[a-z]+" $file`;
-    do
-        unset $i;
-    done;
-    emacs $file;
-    source $file;
-    git commit -m "update func";
-    unset file i;
-    popd
+    file=rc.func.sh
+    pushd ~/utils >/dev/null
+    unset -f $(egrep '^[a-z]+' $file|cut -d '(' -f1|tr '\n' ' ')
+    emacs $file
+    source $file
+    git commit -m "update func"
+    unset file
+    popd >/dev/null
 }
