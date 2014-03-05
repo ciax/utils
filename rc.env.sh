ed3da4c7ec55d5895dd3de103c25a58920faab34
Omata <omata@naoj.org>
2014-03-05 09:02:05 -1000

    rc.env/ some env which conflicts with conventional env is removed;

--- rc.env.sh
+++ rc.env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Requied Packages: emacs,most
 addenv(){
-    name=$1;shift
-    list=$(IFS=: eval echo \$$name)
+    local name=$1;shift
+    local list=$(IFS=: eval echo \$$name)
     for j ; do
         for i in $list;do
             [ "$j" = "$i" ] && break 2
@@ -14,11 +14,10 @@ addenv(){
 # Local functions
 umask 022
 shopt -s nullglob
-export LANG="C"
 addenv PATH "$HOME/bin" "$HOME/lib"
 addenv RUBYLIB "$HOME/lib"
-export XMLPATH="$HOME/ciax-xml"
 export EDITOR='emacs'
 export PAGER='most'
 export MOST_EDITOR='emacs %s -g %d'
 export GREP_OPTIONS='-r --color=auto'
+unset -f addenv
