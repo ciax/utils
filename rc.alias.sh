a1d9579e96ce797f26f40500c0ebb66b68c0a06a
Omata <omata@naoj.org>
2014-03-05 17:35:39 -1000

    remove script file name because path=~/bin is not set yet;

--- rc.alias.sh
+++ rc.alias.sh
@@ -2,7 +2,7 @@
 # Generate alias by pick up '#alias XXX' line from each files
 while read head name par; do
     alias $name=${head%:*}${par:+ $par}
-done < <(cd ~/bin;file-clean;grep '^#alias' *)
+done < <(cd ~/bin;grep '^#alias' *)
 [[ $0 == *rc.alias* ]] && alias
 
 # General Commands
