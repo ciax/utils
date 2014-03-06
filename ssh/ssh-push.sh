74f7356a61952dbcd34a98e68c55f9d154873e5d
Omata <omata@naoj.org>
2014-03-06 11:12:16 -1000

    refactoring: ssh-push;

--- ssh/ssh-push.sh
+++ ssh/ssh-push.sh
@@ -2,27 +2,29 @@
 # Required script: ssh-setup, func.temp, edit-merge
 # Required packages: coreutils(grep,cut,sort),diffutils(cmp),openssh-client(scp)
 # Impose own trust to the object host (push pub-key anonymously)
+addrem(){
+    local rfile=$1;shift
+    local ltemp=$1;shift
+    scp -pq $rfile $rtemp
+    edit-merge $ltemp $rtemp >/dev/null
+    cmp -s $rtemp $ltemp && return
+    scp -pq $ltemp $rfile
+    echo "${rfile##*/} is updated at $rhost"
+}
 . func.usage "[(user@)host]" $1
 rhost=$1
 ssh-setup
 if [ "$LOGNAME" = ${rhost%@*} ]; then
-    [ ${rhost#*@} = `hostname` ] && { echo "Self push"; exit 1; }
-    [ ${rhost#*@} = 'localhost' ] && { echo "Self push"; exit 1; }
+    [ ${rhost#*@} = `hostname` ] && abort "Self push"
+    [ ${rhost#*@} = 'localhost' ] && abort "Self push"
 fi
 rath=.ssh/authorized_keys
 lath=~/$rath
 rinv=.ssh/invalid_keys
 linv=~/$rinv
-. func.temp trem trath
-scp -pq $rhost:$rath $trem
-{ grep @ $trem;cut -d' ' -f1-2 $lath; }|sort -u > $trath
-if ! cmp -s $trem $trath; then
-    scp -pq $trath $rhost:$rath
-    echo "authorized_keys is updated at $rhost"
-fi
-scp -pq $rhost:$rinv $trem
-edit-merge $linv $trem
-if ! cmp -s $trem $linv ; then
-    scp -pq $linv $rhost:$rinv
-    echo "invalid_keys is updated at $rhost"
-fi
+. func.temp rtemp pushkeys
+cut -d' ' -f1-2 $lath > pushkeys
+# Add to authorized_keys
+addrem $rhost:$rath $pushkeys
+# Add to invalid_keys
+addrem $rhost:$rinv $linv
