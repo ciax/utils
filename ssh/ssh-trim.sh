b8502f282ad2e6a80c1c8491214fae6588997f97
Omata <omata@naoj.org>
2014-03-06 09:37:59 -1000

    ssh-trim/refactoring;

--- ssh/ssh-trim.sh
+++ ssh/ssh-trim.sh
@@ -2,27 +2,26 @@
 # Required script: func.temp, ssh-perm.sh
 # Required packages: coreutils(cp,cut,grep,sort,md5sum)
 # Remove dup key from authorized_keys
+getmd5(){ md5sum <<< $2 | cut -c32; }
 ath=~/.ssh/authorized_keys
 inv=~/.ssh/invalid_keys
 pub=~/.ssh/id_rsa.pub
 #
 # Split file into invalid_keys by #headed line, old key and others
 #
-. func.temp tath tinv
+. func.temp tath tinv tath2
 read rsa mykey me < $pub
-# If the line with own name is found in authorized_keys,
-# maching own id_rsa.pub and the line, otherwise move older one to invalid_keys
-for i in $(egrep -v $mykey $ath|egrep '^#|$me' $ath|cut -d' ' -f2) $(<$inv);do
-    if [ ${#i} -gt 32 ] ; then
-        md5sum <<< $i
-    else
-        echo $i
-    fi
-done|cut -d' ' -f1|sort -u > $tinv
-overwrite $tinv $inv
-while read line; do
-    set - $line
-    grep -q $(md5sum <<< $2 |cut -d' ' -f1) $inv || echo "$line"
-done < $ath > $tath
-edit-merge $pub $tath
-overwrite $tath $ath
+#  If the line with own name is found in authorized_keys,
+#  maching own id_rsa.pub and the line, otherwise move older one to invalid_keys
+# For invalid_keys
+grep -v $mykey $ath > $tath
+while read line;do
+    getmd5 $line
+done < <(edit-cutout '^#|$me' $tath) > $tinv
+edit-merge $tinv $inv
+# For authorized_keys
+while read line;do
+    grep -q $(getmd5 $line) $inv || echo "$line"
+done < $tath > $tath2
+edit-merge $pub $tath2
+overwrite $tath2 $ath
