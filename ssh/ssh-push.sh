0608fb414770d3f903acc81a269ec281e2334c66
Omata <omata@naoj.org>
2014-03-06 13:17:41 -1000

    ssh-push/add join function (by making link of ssh-join);

--- ssh/ssh-push.sh
+++ ssh/ssh-push.sh
@@ -2,6 +2,7 @@
 # Required script: ssh-setup, func.temp, edit-merge
 # Required packages: coreutils(grep,cut,sort),diffutils(cmp),openssh-client(scp)
 # Impose own trust to the object host (push pub-key anonymously)
+#link ssh-join
 addrem(){
     local rfile=$1;shift
     local ltemp=$1;shift
@@ -14,17 +15,21 @@ addrem(){
 . func.usage "[(user@)host]" $1
 rhost=$1
 ssh-setup
-if [ "$LOGNAME" = ${rhost%@*} ]; then
-    [ ${rhost#*@} = `hostname` ] && abort "Self push"
-    [ ${rhost#*@} = 'localhost' ] && abort "Self push"
-fi
+[[ ${rhost#*@} =~ "`hostname`|localhost" ]] && abort "Self push"
 rath=.ssh/authorized_keys
 lath=~/$rath
 rinv=.ssh/invalid_keys
 linv=~/$rinv
-. func.temp rtemp pushkeys
-cut -d' ' -f1-2 $lath > $pushkeys
+. func.temp rtemp
+case $0 in
+    *ssh-push) 
+	temp akeys
+	cut -d' ' -f1-2 $lath > $akeys
+	;;
+    *ssh-join) akeys=$lath;;
+    *) exit;;
+esac
 # Add to authorized_keys
-addrem $rhost:$rath $pushkeys
+addrem $rhost:$rath $akeys
 # Add to invalid_keys
 addrem $rhost:$rinv $linv
