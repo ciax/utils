c5ac35de7e4228179c6e91f34a88dc474a07cb39
Omata <omata@naoj.org>
2014-03-05 17:39:28 -1000

    func.temp/ rename function name create_tempfile() -> temp();

--- func.temp.sh
+++ func.temp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Required packages: coreutils(cat),diffutils(cmp)
-# Usage: create_tempfile [varname1] [varname2] ..
-create_tempfile(){
+# Usage: temp [varname1] [varname2] ..
+temp(){
     local trp="/bin/rm -f -- "
     local i=
     for i ; do
@@ -20,4 +20,4 @@ overwrite(){
         /bin/mv -b $1 $2
     fi
 }
-create_tempfile $*
+temp $*
