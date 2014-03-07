cd683afd218ee255f5d5a5c3adf716044138ab56
Omata <omata@naoj.org>
2014-03-07 13:18:05 -1000

    file-register/ accept any dir for link;

--- file-register.sh
+++ file-register.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 # Required packages: coreutils(readlink)
-# Required scripts: file-clean
+# Required scripts: func.color, file-clean
 # Desctiption: Files in current dir will be classified into 'bin','db' ..
 # "Usage: ${0##*/} [DIR..] | [SRC..]"
-chklink(){
-    # if dst file exists -> dst=regular file:>fail , dst=org link:>skip
-    src=$1
-    [ ! -e $2 ] && return 0 # Create or Overwrite unexist link
-    [ -h $2 ] || { echo "Warning: $2 was regular file"; return 1; }
-    dst=`readlink $2`
-    [ "$src" != "$dst" ] || return 1
-    echo "Warning: link of $2 is different from $src"
+addlist(){
+    if [[ "$1" =~ / ]]; then
+	local dir=${1%/*}
+    else
+	local dir=$(pwd -P)
+    fi
+    dirlist[$dir]="${dirlist[$dir]} ${1##*/}"
 }
-
-mklink(){
+showlist(){
+    for i in ${!dirlist[*]};do
+	echo "[${dirlist[$i]} ] -> $C1$i$C0"
+    done
+}
+makelink(){
+    src=$1;dst=$2
+    # if dst file exists -> dst=regular file:>fail , dst=org link:>skip
+    # Create or Overwrite unexist link
+    if [ -e $dst ] ; then 
+	[ -h $dst ] || { echo "Error: $dst is regular file"; return 1; }
+	[ "$src" = `readlink $dst` ] && return
+	echo "Warning: link of $dst is different from $src"
+    fi
+    ln -sf $src $dst || return 1
+    addlist $dst
+}
+link2dir(){
     [ "$1" = "-x" ] && { local xopt=$1;shift; }
     local objdir="$HOME/$1";shift
     [ -d "$objdir" ] || mkdir "$objdir"
@@ -30,26 +45,29 @@ mklink(){
             local link="$base"
         fi
 	# extra link should be described as #link head
+	pushd $objdir > /dev/null
 	for j in "$link" $(grep '^#link' "$real"|cut -d' ' -f2-);do
-            chklink "$real" "$objdir/$j" || continue
-            ln -sf "$real" "$objdir/$j"
-	    local org="$base "
+	    eval "k=$j" # Fop tilde expansion
+            makelink "$real" "$k"
 	done
-        list="$list$org"
+	popd > /dev/null
     done
-    [ "$list" ] && echo "[ $list] -> $objdir"
 }
 dirreg(){
+    declare -A dirlist
     for i ; do
         pushd $i >/dev/null
-        mklink -x bin *.sh *.pl *.py *.rb *.awk *.exp *.js
-        mklink lib lib*
-        mklink db *.tsv *.csv
-        mklink .emacs.d *.el
+        link2dir -x bin *.sh *.pl *.py *.rb *.awk *.exp *.js
+        link2dir lib lib*
+        link2dir db *.tsv *.csv
+        link2dir .emacs.d *.el
         dirreg */
         popd >/dev/null
     done
+    showlist
 }
+
+. ~/utils/func.color.sh
 shopt -s nullglob
 dirreg ~/utils ~/cfg.* $*
 ~/bin/file-clean ~/bin ~/db ~/lib ~/.var
--- initrc.sh
+++ initrc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#link ~/.bash_completion
 complete -r
 for i in ~/bin/rc.*; do . $i;done
