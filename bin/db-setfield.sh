dbec3bb6c72ccf0cff3e98a11ae58f338ba22303
Koji Omata <omata@naoj.org>
2014-03-09 23:23:59 -1000

    db-setfield.sh/ become reusable;

--- bin/db-setfield.sh
+++ bin/db-setfield.sh
@@ -2,7 +2,7 @@
 # Description: Set variables corresponding field names of last table of param;
 # Required scripts: func.usage, func.temp, db-register(' ')
 # Required tables: *
-getvar(){
+setfield(){
     local id=$1;shift
     local tbl=$1;shift
     local sql="from $tbl where id == '$id'"
@@ -10,13 +10,21 @@ getvar(){
         sql="from $tbl where id == (select $tbl $sql)"
     done
     sql='select * '$sql';'
+    . func.temp list
     for i in $(db-register "pragma table_info($tbl);"|cut -d'|' -f2);do
         read line
         echo "$i='$line'" >> $list
     done < <(db-register "$sql"|tr '|' '\n')
-    source $list
+    if [[ $0 =~ db-setfield ]]; then
+        cat $list
+    else
+        source $list
+    fi
+    rm $list;unset list
 }
-. func.usage "[id] [table1] (table2..)" $2
-. func.temp db list
-getvar $*
-[[ $0 =~ db-setfield ]] && {  cat $list; }
+set -f
+if [[ $0 =~ db-setfield ]]; then
+    . func.usage "[id] [table1] (table2..)" $2
+elif [ "$2" ]; then
+    setfield $*
+fi
