e19feb72be9d6919e4840b8932905d6c3a7ef640
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-12 12:15:20 -1000

    adapt to exported db format (%proj at the head of index);

--- bin/db-split.sh
+++ bin/db-split.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-# Description: split db files into each cfg directory which is categoryzed by group
 # Required packages: coreutils
 # Required scripts: func.usage, func.temp, show-tables
+# Description: split export-db files into db files in the cfg directorys that are categoryzed by project
+#  Export-DB format: (%proj),!id,(!another key),field1,field2...
+#  Config Dir: ~/cfg.(project)/db
 . func.usage "[dbfiles]" $1
 . func.temp dbfile
 for src;do
-    base=db-$(show-tables $src)||continue
+    tbl=$(show-tables $src)||continue
     for d in ~/cfg.*;do
         sfx=${d#*.}
-        grep -h ",$sfx," $src > $dbfile
+        egrep -h "^$sfx," $src|cut -d, -f2- > $dbfile
         if [ -s $dbfile ] ;then
-            overwrite $dbfile $d/db/$base-$sfx.csv && echo "Update $base-$sfx.csv"
+            overwrite $dbfile $d/db/db-$tbl-$sfx.csv && echo "Update db-$tbl-$sfx.csv"
         fi
     done
-    egrep -e "^!" $src > $dbfile
-    egrep -q ",group," $dbfile || egrep -v -e "^#" $src > $dbfile
-    overwrite $dbfile ~/utils/db/$base.csv && echo "Update $base.csv"
+    egrep -h "^%" $src | cut -d, -f2- > $dbfile
+    overwrite $dbfile ~/utils/db/db-$tbl.csv && echo "Update db-$tbl.csv"
 done
