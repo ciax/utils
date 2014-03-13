40f84e9ba387583c13f480a7ca3e68c0fbff4f08
Omata <omata@naoj.org>
2014-03-13 10:24:17 -1000

    debug:net-login/ accept command for ssh and adapt to telnet;

--- bin/net-login.sh
+++ bin/net-login.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 #alias l
 # Required packages: expect,bsdmainutils(column),sed
-# Required scripts: func.usage, db-list, db-setfield, db-sshid, func.temp
+# Required scripts: func.usage, db-list, db-setfield, func.temp
 # Required tables: login (user,password,host,rcmd)
 # Description: login command
-. func.usage "[host]" < <(db-list login) $1
+. func.usage "[host] (command)" < <(db-list login) $1
 sshopt="-o StrictHostKeyChecking=no -t"
 . db-setfield $1 login
-setfield ${host:-$1} host
-str="ssh $sshopt ${user:+$user@}$host $*"
-echo $str
-if [ "$password" ] ; then
-    . func.temp expfile
-    echo "set timeout 10" > $expfile
-    echo "spawn -noecho $str" >> $expfile
-    echo "expect word: { send $password\n };" >> $expfile
-    [ "$rcmd" ] && echo "expect -re \".+ \"  { send \"$rcmd\n\" }" >> $expfile
-    echo "interact" >> $expfile
-    expect $expfile
+setfield ${host:-$1} host;shift
+[ "$1" ] && rcmd="$*"
+if [ $command = telnet ]; then
+    telnet $host
 else
-    $str $rcmd
+    str="ssh $sshopt ${user:+$user@}$host"
+    [ "$VER" ] && echo $str
+    if [ "$password" ] ; then
+	. func.temp expfile
+	echo "set timeout 10" > $expfile
+	echo "spawn -noecho $str" >> $expfile
+	echo "expect word: { send $password\n };" >> $expfile
+	[ "$rcmd" ] && echo "expect -re \".+ \"  { send \"$rcmd\n\" }" >> $expfile
+	echo "interact" >> $expfile
+	expect $expfile
+    else
+	$str $rcmd
+    fi
 fi
