579d912b75e84a5068ab7e51a8c0b986bc28a69f
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-12 18:32:59 -1000

    remove unused file;

--- bin/db-sshid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Desctiption: lookup ssh id by subnet and destination host
-# Required scripts: func.usage, net-name, db-register
-# Required tables: ssh(subnet,login)
-. func.usage "[host]" $1
-[ "$net" ] || net=$(net-name)
-db-register "select id from ssh where subnet == '$net' and login == '$1';"
