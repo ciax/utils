425543068fcefe1b7ad38f6e54d21ce9c6fb4871
Omata <omata@naoj.org>
2014-03-13 18:08:29 -1000

    add -line option;

--- bin/db-register.sh
+++ bin/db-register.sh
@@ -3,5 +3,6 @@
 # Required packages: sqlite3
 # Usage: db-register (opt) (sql)
 #    -i:ini (a=b) style
+[ "$1" = -i ] && { opt=-line;shift; }
 db=~/.var/db-register.sq3
-sqlite3 $db ${1:+"$1"}
+sqlite3 $opt $db ${1:+"$1"}
