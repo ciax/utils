0aca6ebe014a1cc8c661cfc23de9f297f4903745
Omata <omata@naoj.org>
2014-03-13 18:37:53 -1000

    no set field value if no value, return error code if all value is empty;

--- bin/db-setfield.sh
+++ bin/db-setfield.sh
@@ -4,7 +4,6 @@
 # Description: Set variables corresponding field names of last table of param;
 #  default reference key(search key) name is 'id'
 setfield(){
-    local _list
     local _wh
     local _key=$1;shift
     local _tbl=$1;shift
@@ -25,23 +24,19 @@ setfield(){
         _sql="from $_tbl where id == (select $_tbl $_sql)"
     done
     _sql='select * '$_sql';'
-    . func.temp _list
-    for _i in $(db-register "pragma table_info($_tbl);"|cut -d'|' -f2);do
-        read _line
-        echo "$_i='$_line'" >> $_list
-    done < <(db-register "$_sql"|tr '|' '\n')
+    echo -n > $_list
+    while read _key _eq _val;do
+        [ "$_val" ] && echo "$_key='$_val'" >> $_list
+    done < <(db-register -i "$_sql")
     if [[ $0 =~ db-setfield ]]; then
         echo $_sql
         cat $_list
     else
         source $_list
     fi
-    rm $_list
+    [ -s $_list ]
 }
 set -f
-if [[ $0 =~ db-setfield ]]; then
-    . func.usage "[id | key=a,key=b..] [table1] (table2..)" $2
-    setfield $*
-elif [ "$2" ]; then
-    setfield $*
-fi
+[[ $0 =~ db-setfield ]] && . func.usage "[id | key=a,key=b..] [table1] (table2..)" $2
+. func.temp _list
+[ "$2" ] && setfield $*
