e1946bd86ed9d81c1aaad51a4e85ea47b0241568
Koji Omata <omata@naoj.org>
2014-03-14 22:42:07 -1000

    handling for file name having space;

--- file-clean.sh
+++ file-clean.sh
@@ -3,6 +3,7 @@
 #alias clr
 shopt -s nullglob
 chkdir(){
+    [ -h $1 ] && return 1
     [ -d $1 ] && return
     [ -e $1 ] && { echo $C1"Not a directory $1"$C0;return 1; }
     /bin/mkdir $1
@@ -10,24 +11,24 @@ chkdir(){
 nouse(){
         [ "$1" ] || return
         chkdir ~/.trash || exit 1
-        /bin/mv -fb $* ~/.trash
+        /bin/mv -fb "$@" ~/.trash
         /bin/ls -aF --color
 }
 nolink(){
     for i ; do
-        [ -L $i -a ! -e $i ] || continue
-        /bin/rm $i
+        [ -L "$i" -a ! -e "$i" ] || continue
+        /bin/rm "$i"
         echo $C3"[${i##*/}] is not linked"$C0
     done
 }
 clrdir(){
     for i;do
-	chkdir $i || continue
-	pushd $i >/dev/null
-	nouse \#* *~ .*~ *.orig
-	nolink * .*
-	clrdir */
-	popd >/dev/null
+        chkdir "$i" || continue
+        pushd "$i" >/dev/null
+        nouse \#* *~ .*~ *.orig
+        nolink * .*
+        clrdir */
+        popd >/dev/null
     done
 }
 [ "$1" ] || set - .
