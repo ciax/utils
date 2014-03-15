393e40608226e116c0469fdb3681c1886b93e58e
Omata <omata@naoj.org>
2014-03-14 17:04:24 -1000

    add disconnect feature;

--- bin/vpn/client-ipsec.sh
+++ bin/vpn/client-ipsec.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#alias vpni
 # Required packages: vpnc
 # Description: vpn client of ipsec
-_usage "[vpnhost]" $1 < <(db-list vpn)
+_usage "(-d:disconnect) [vpnhost]" $1 < <(db-list vpn)
 _temp config
 cfg-ipsec $* > $config
 PATH=$PATH:/usr/sbin
 sudo vpnc-disconnect
+[ "$1" = -d ] && exit
 sudo vpnc $config
 
--- bin/vpn/client-openvpn.sh
+++ bin/vpn/client-openvpn.sh
@@ -4,8 +4,8 @@
 # Required scripts: rc.app, db-list, cfg-openvpn, vpn-nat, show-syslog
 # Description: client for dd-wrt openvpn server
 . rc.app
-[ "$1" = "-r" ] && { sudo kill $(< ~/.var/openvpn.pid); exit; }
-_usage "(-r:remove) [vpnhost]" $1 < <(db-list vpn)
+[ "$1" = "-d" ] && { sudo kill $(< ~/.var/openvpn.pid); exit; }
+_usage "(-d:disconnect) [vpnhost]" $1 < <(db-list vpn)
 cfgfile=~/.var/openvpn-$1.cfg
 . cfg-openvpn $1 $cfgfile
 ifconfig tun || { sudo openvpn --mktun --dev tun0;sleep 5; }
