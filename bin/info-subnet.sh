50d390af16ba97f6aff66080850daba6e42c6fbe
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-24 10:07:00 -1000

    subnet table format change: network should have '.0' for adapting to route command;

--- bin/info-subnet.sh
+++ bin/info-subnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 shopt -s nullglob
 eth=`netstat -i|cut -d' ' -f1|egrep '^eth'|sort|head -1`
-netstat -nr|egrep -o "^([12].+) +0\.0\.0\.0 +255\.255.*$eth"|cut -d'.' -f1-3
+netstat -nr|egrep -o "^([12].+) +0\.0\.0\.0 +255\.255.*$eth"|cut -d' ' -f1
