4ce46dc04144e1345180a7e0c9435618d7a1758c
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-24 06:33:50 -1000

    add list for usage;

--- bin/vpn/vpn-route.sh
+++ bin/vpn/vpn-route.sh
@@ -5,5 +5,5 @@
 . rc.app
 mode="add"
 [ "$1" = "-r" ] && { shift; mode="del"; }
-_usage "(-r:remove) [vpnhost]" $1
+_usage "(-r:remove) [vpnhost]" $1 < <(db-list vpn)
 db-register "select 'route $mode -net '||network||' netmask '||netmask from subnet where route == (select route from vpn where id == '$1');"
