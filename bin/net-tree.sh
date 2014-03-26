36186afe6c7703009605db8ed43263c8e1a62086
Koji Omata <omata@naoj.org>
2014-03-25 23:27:16 -1000

    description is looked up with id,upper;

--- bin/net-tree.sh
+++ bin/net-tree.sh
@@ -6,20 +6,20 @@
 subtree(){
     local ind="   |$2"
     for i in ${sub[$1]};do
-        echo "$ind"
-        echo -n "${ind}-- "
-        db-register "select description from hub where id == '$i';"
+        echo "${ind}"
+        echo "${ind}-${title[$i]}"
         subtree $i "$ind"
     done
 }
 
 _usage "[subnet]" $1 < <(db-list subnet)
 declare -A sub
-for hub in $(db-register "select id,upper from hub where subnet == '$1';"); do
-    up="${hub#*|}"
-    hub="${hub%|*}"
-    up=${up:-$1}
-    sub[$up]="${sub[$up]} $hub"
-done
-echo "$1"
+declare -A title
+IFS='|'
+while read h u n; do
+    u="${u:-$1}"
+    sub[$u]="${sub[$u]}$h|"
+    title[$h]="$n"
+done < <(db-register "select id,upper,description from hub where subnet == '$1';")
+echo " $1"
 subtree $1
