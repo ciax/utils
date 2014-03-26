bbdff66b70981e5ce5bb47f6fa7e412d8958afd5
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-25 14:29:17 -1000

    add invalid option handling;

--- bin/vpn/client-openvpn.sh
+++ bin/vpn/client-openvpn.sh
@@ -4,11 +4,21 @@
 # Required scripts: rc.app, db-list, cfg-openvpn, vpn-nat, show-syslog
 # Description: client for dd-wrt openvpn server
 . rc.app
-[ "$1" = "-d" ] && { sudo kill $(< ~/.var/openvpn.pid); exit; }
-_usage "(-d:disconnect) [vpnhost]" $1 < <(db-list vpn)
-cfgfile=~/.var/openvpn-$1.cfg
+case "$1" in
+    '')
+        _usage "(-d:disconnect) [vpnhost]" < <(db-list vpn)
+        ;;
+    -d)
+        sudo kill $(< ~/.var/openvpn.pid) && echo "Openvpn Terminated"
+        exit
+        ;;
+    -*)
+        _abort "No such option"
+        ;;
+esac
+_temp cfgfile
 . cfg-openvpn $1 > $cfgfile
-ifconfig tun || { sudo openvpn --mktun --dev tun0;sleep 5; }
+sudo ifconfig tun || { sudo openvpn --mktun --dev tun0;sleep 5; }
 sudo openvpn --config $cfgfile
 vpn-nat set # Need NAT setup (naoj can't look up 172 address)
 show-syslog openvpn
