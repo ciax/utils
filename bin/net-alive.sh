21af503e6b5cfaa371b323c3cb1f5f59f4add3da
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-25 17:01:00 -1000

    refactoring;

--- bin/net-alive.sh
+++ bin/net-alive.sh
@@ -3,18 +3,13 @@
 # Required scripts: rc.app, db-register, info-subnet
 # Required tables: hub, subnet(network)
 . rc.app
-chkhost(){
-    local chk
-    for host in $(db-register "select host from mac where hub == '$1';"); do
-        ping -c1 -w1 $host &>/dev/null && return
-        chk=1
-    done
-    [ "$chk" ] || return
-    db-register "select id||'('||description||') is Down' from hub where id == '$1';"
-    fail=1
-}
 _usage "[subnet]" $1 < <(db-list subnet)
 for hub in $(db-register "select id from hub where subnet == '$1';"); do
-    chkhost $hub
+    for host in $(db-register "select host from mac where hub == '$hub';"); do
+        ping -c1 -w1 $host &>/dev/null && break
+        false
+    done && continue
+    db-register "select id||'('||description||') is Down' from hub where id == '$hub';"
+    fail=1
 done
 [ "$fail" ] || echo "All Hub is OK"
