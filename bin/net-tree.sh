e4d23c797a9915373c0271581a6083cc77946685
Koji Omata <omata@naoj.org>
2014-03-25 22:09:04 -1000

    net-tree/ complete;

--- bin/net-tree.sh
+++ bin/net-tree.sh
@@ -3,14 +3,23 @@
 # Required scripts: rc.app, db-registe
 # Required tables: hub,subnet
 . rc.app
+subtree(){
+    local ind="   |$2"
+    for i in ${sub[$1]};do
+        echo "$ind"
+        echo -n "${ind}-- "
+        db-register "select description from hub where id == '$i';"
+        subtree $i "$ind"
+    done
+}
+
 _usage "[subnet]" $1 < <(db-list subnet)
+declare -A sub
 for hub in $(db-register "select id,upper from hub where subnet == '$1';"); do
-    if [[ "$hub" =~ '|' ]] ;then
-        upper="${hub#*|}"
-        hub="${hub%|*}"
-        db-register "select description from hub where id == '$upper';"
-        echo "  |- $hub"
-    else
-        echo "$hub"
-    fi
+    up="${hub#*|}"
+    hub="${hub%|*}"
+    up=${up:-$1}
+    sub[$up]="${sub[$up]} $hub"
 done
+echo "$1"
+subtree $1
