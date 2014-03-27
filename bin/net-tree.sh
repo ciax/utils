544aec50a3142767b6443857a77346c754f9786d
Koji Omata <omata@naoj.org>
2014-03-26 20:44:56 -1000

    net-tree/ includes hosts;

--- bin/net-tree.sh
+++ bin/net-tree.sh
@@ -3,16 +3,20 @@
 # Required scripts: rc.app, db-registe
 # Required tables: hub,subnet
 . rc.app
+_usage "[subnet]" $1 < <(db-list subnet)
+
 subtree(){
+    [ $depth -gt  10 ] && { echo "Infinity Loop error"; exit 1; }
+    depth=$(( $depth + 1 ))
     local ind="   |$2"
     for i in ${sub[$1]};do
         echo "${ind}"
         echo "${ind}-${title[$i]}"
         subtree $i "$ind"
     done
+    depth=$(( $depth - 1 ))
 }
 
-_usage "[subnet]" $1 < <(db-list subnet)
 declare -A sub
 declare -A title
 IFS='|'
@@ -21,5 +25,12 @@ while read h u n; do
     sub[$u]="${sub[$u]}$h|"
     title[$h]="$n"
 done < <(db-register "select id,upper,description from hub where subnet == '$1';")
+for u in ${!title[*]};do
+    while read h; do
+        sub[$u]="${sub[$u]}$h|"
+        title[$h]="($h)"
+    done < <(db-register "select host from mac where hub == '$u';")
+done
+depth=0
 echo " $1"
 subtree $1
