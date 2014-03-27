1a28c3ad695c84a8fbe1350876b16d692de52f8e
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-27 10:43:45 -1000

    exclude .git dir;

--- bin/file-perm.sh
+++ bin/file-perm.sh
@@ -3,6 +3,6 @@
 # Required packages: findutils
 #alias rwx
 regexp='.*\.(sh|pl|py|rb|exp|js)$'
-sudo find . -regextype posix-awk -type f \
-\( ! -regex $regexp -exec chmod -c 664 {} \; \) , \
-\( -regex $regexp -exec chmod -c 775 {} \; \)
+sudo find . -path ".git" -prune -o -regextype posix-awk -type f \
+\( ! -regex $regexp -exec chmod -c 644 {} \; \) , \
+\( -regex $regexp -exec chmod -c 755 {} \; \)
