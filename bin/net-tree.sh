c16157249ec176e16d67c71e4daa9b5ce5148a9e
Koji Omata <omata@naoj.org>
2014-03-26 21:18:27 -1000

    color change/ make modular;

--- bin/net-tree.sh
+++ bin/net-tree.sh
@@ -5,6 +5,24 @@
 . rc.app
 _usage "[subnet]" $1 < <(db-list subnet)
 
+gethubs(){
+    while read h u n; do
+        u="${u:-$1}"
+        sub[$u]="${sub[$u]}$h|"
+        title[$h]=$C2"$n"$C0
+    done < <(db-register "select id,upper,description from hub where subnet == '$1';")
+}
+
+gethosts(){
+    for u in ${!title[*]};do
+        while read h; do
+            h="($h)"
+            sub[$u]="${sub[$u]}$h|"
+            title[$h]=$C4"$h"$C0
+        done < <(db-register "select host from mac where hub == '$u';")
+    done
+}
+
 subtree(){
     [ $depth -gt  10 ] && { echo "Infinity Loop error"; exit 1; }
     depth=$(( $depth + 1 ))
@@ -19,17 +37,8 @@ subtree(){
 declare -A sub
 declare -A title
 IFS='|'
-while read h u n; do
-    u="${u:-$1}"
-    sub[$u]="${sub[$u]}$h|"
-    title[$h]=$C5"$n"$C0
-done < <(db-register "select id,upper,description from hub where subnet == '$1';")
-for u in ${!title[*]};do
-    while read h; do
-        sub[$u]="${sub[$u]}$h|"
-        title[$h]="($h)"
-    done < <(db-register "select host from mac where hub == '$u';")
-done
+gethubs $1
+gethosts
 depth=0
 echo " $1"
 subtree $1
