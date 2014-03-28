67830ca62a736288a3d3e5f09fbbf6c5204e436a
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-28 13:13:17 -1000

    modify option handling;

--- bin/db-register.sh
+++ bin/db-register.sh
@@ -3,6 +3,8 @@
 # Description: transaction for db file
 # Usage: db-register (opt) (sql)
 #        -i:ini (a=b) style
-[ "$1" = -i ] && { opt=-line;shift; }
+. rc.app
+opt-i(){ opt=-line; }
+_usage
 db=~/.var/db-register.sq3
 sqlite3 $opt $db ${1:+"$1"}
