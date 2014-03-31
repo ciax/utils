c429413929aa08532a1325abe84db278f7042f43
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-31 10:20:43 -1000

    add extra format file handling(just ignore);

--- bin/db-split.sh
+++ bin/db-split.sh
@@ -4,11 +4,15 @@
 # Description: split export-db files into db files in the cfg directorys that are categoryzed by project
 #  Export-DB format: (%proj),!id,(!another key),field1,field2...
 #  Config Dir: ~/cfg.(project)/db
+#  Ignore files that don't have (%proj) line
 . rc.app
 _usage "[dbfiles]"
 _temp dbfile
 for src;do
     tbl=$(show-tables $src)||continue
+    egrep -h "^%" $src | cut -d, -f2- > $dbfile
+    [ -s $dbfile ] || continue
+    _overwrite $dbfile ~/utils/db/db-$tbl.csv && echo "Update db-$tbl.csv"
     for d in ~/cfg.*;do
         sfx=${d#*.}
         egrep -h "^$sfx," $src|cut -d, -f2- > $dbfile
@@ -16,7 +20,5 @@ for src;do
             _overwrite $dbfile $d/db/db-$tbl-$sfx.csv && echo "Update db-$tbl-$sfx.csv"
         fi
     done
-    egrep -h "^%" $src | cut -d, -f2- > $dbfile
-    _overwrite $dbfile ~/utils/db/db-$tbl.csv && echo "Update db-$tbl.csv"
 done
 file-register
