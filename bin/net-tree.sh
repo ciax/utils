20f7206f2cd3f7016db64b3059953607eaade1f2
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-03-31 14:46:17 -1000

    using domain from db instead of fdqn for ping site;

--- bin/net-tree.sh
+++ bin/net-tree.sh
@@ -2,7 +2,7 @@
 # Description: show network tree
 # Required scripts: rc.app, db-exec
 # Required tables: host,hub,subnet
-. rc.app
+. db-setfield
 
 open_super(){
     connect[$1]=1
@@ -16,6 +16,8 @@ get_hubs(){
         sub[$u]="${sub[$u]}$h|" # add itself to parent var
         super[$h]="$u"
         title[$h]=$C2"$n"$C0
+        eval "$(db-trace $h hub subnet domain)"
+        domain[$h]=$name
     done < <(db-exec "select id,super,description from hub where subnet == '$1';")
 }
 
@@ -25,7 +27,7 @@ get_hosts(){
             sub[$u]="${sub[$u]}$h|"
             super[$h]="$u"
             title[$h]="("$C4"$h"$C0")"
-            site="${fdqn:-$h}"
+            site="$h.${domain[$u]}"
             chk_host "$site" && open_super $h
         done < <(db-exec "select id,fdqn from host where hub == '$u';")
     done
@@ -50,7 +52,7 @@ chk_host(){ true; }
 opt-p(){
     chk_host(){
         _progress "Checking"
-        ping -c1 -w1 $1 &>/dev/null
+        ping -c1 -w1 "$1" &>/dev/null
     }
 }
 
@@ -61,6 +63,7 @@ _usage "(-p:ping check) [subnet]"
 declare -A sub
 declare -A super
 declare -A title
+declare -A domain
 declare -A connect
 IFS='|'
 get_hubs $1
