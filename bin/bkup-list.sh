46326a4df8394c572c754c9da266733ed1421263
Omata <omata@naoj.org>
2014-04-03 10:56:21 -1000

    modify display;

--- bin/bkup-list.sh
+++ bin/bkup-list.sh
@@ -3,4 +3,5 @@
 # Required scripts: bkup-exec
 # Descripton: display backed up files
 [ "$(bkup-exec .tables)" ] || bkup-init
-bkup-exec "select name,count(*) from list group by name;"
+IFS=,;read name count < <(bkup-exec "select name,count(*) from list group by name;")
+echo "$name ($count)"
