f96d7a1720a1fdc07334031f825fe8852738b3a8
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-04-03 16:56:57 -1000

    setup includes install apt-fiel,sqlite3

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ which apt-get >/dev/null || { echo "This might not Debian"; exit; }
 which sudo >/dev/null || { echo "Need 'sudo' installed or to be root"; exit; }
 ichk(){ for i ;do which $i >/dev/null || sudo -i apt-get install $i;done; }
 ichk grep sed
-sudo -i apt-get install coreutils diffutils grep
+sudo -i apt-get install apt-file sqlite3
+apt-file update
 ~/utils/bin/file-register.sh
 echo $C1"*** You need to re-login here***"$C0
