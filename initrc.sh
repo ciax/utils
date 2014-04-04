296a70f55d260b6b9d66da59bd6787c4c2847f62
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-04-03 17:42:21 -1000

    link to .bash_login;

--- initrc.sh
+++ initrc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#link ~/.bash_completion
+#link ~/.bash_login
 # Description: login script loader
+[ -f .profile ] && . .profile
 shopt -s nullglob;for i in ~/bin/rc.*;do . $i;done #initrc
