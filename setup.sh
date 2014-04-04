bc5bce68c75226267ee19004f533e4872dc8f4c7
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-04-03 17:00:11 -1000

    add nkf for installing pkg;

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ which apt-get >/dev/null || { echo "This might not Debian"; exit; }
 which sudo >/dev/null || { echo "Need 'sudo' installed or to be root"; exit; }
 ichk(){ for i ;do which $i >/dev/null || sudo -i apt-get install $i;done; }
 ichk grep sed
-sudo -i apt-get install apt-file sqlite3
+sudo -i apt-get install apt-file nkf
 apt-file update
 ~/utils/bin/file-register.sh
 echo $C1"*** You need to re-login here***"$C0
