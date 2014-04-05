216b8676fde062de8f648e888b55476f77bb6afb
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-04-04 16:03:38 -1000

    remove apt-get;

--- bin/ssh/ssh-setup.sh
+++ bin/ssh/ssh-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Required commands: ssh
+# Required packages: openssh-server
 # Required scripts: ssh-config
 # Desctiption: setup ssh files 
 # Usage: ${0##*/} (-r:remove keys)
+. func.app
 echo $C5"Initializing SSH"$C0
 ath=~/.ssh/authorized_keys
 inv=~/.ssh/invalid_keys
@@ -10,7 +11,7 @@ sec=~/.ssh/id_rsa
 pub=~/.ssh/id_rsa.pub
 cfg=~/.ssh/config
 [ "$1" = -r ] && rm $sec $pub
-type ssh > /dev/null || apt-get install ssh
+type ssh > /dev/null || _abort "No ssh installed"
 [ -e $sec ] || ssh-keygen
 [ -e $pub ] || ssh-keygen -y -f $sec > $pub
 [ -e $ath ] || touch $ath
