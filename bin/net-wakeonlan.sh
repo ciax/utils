77db904e176a26bd862a2b19d9b228a71a3253f9
Koji Omata <omata@naoj.org>
2014-04-07 20:28:49 -1000

    show only hosts which belong to same subnet;

--- bin/net-wakeonlan.sh
+++ bin/net-wakeonlan.sh
@@ -5,5 +5,5 @@
 # Required tables: mac(host)
 # Description: make network devices wake up
 . func.getpar
-_usage "[host]" <(db-list host)
-wakeonlan <(db-exec "select id from mac where host == '$1';")
+_usage "[host]" <(db-exec "select host from mac where host in (select id from host where hub in (select id from subnet where network == '$(info-subnet)'));")
+wakeonlan $(db-exec "select id from mac where host == '$1';")
