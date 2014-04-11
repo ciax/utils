5478530646560157a18cbd96a4bc4f6fbbb6ee1e
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-04-11 08:22:23 -1000

    debug: enclosed while,do,done;

--- bin/bkup-list.sh
+++ bin/bkup-list.sh
@@ -3,5 +3,7 @@
 # Required scripts: bkup-exec
 # Descripton: display backed up files
 [ "$(bkup-exec .tables)" ] || bkup-init
-IFS=,;read name count < <(bkup-exec "select name,count(*) from list group by name;")
-echo "$name ($count)"
+IFS=,
+while read name count;do
+    echo "$name ($count)"
+done < <(bkup-exec "select name,count(*) from list group by name;")
