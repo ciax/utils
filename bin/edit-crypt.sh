dcd809859c95da4de4d56975de935fe94e5c8534
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-04-10 17:19:19 -1000

    exit if no passphrse file;

--- bin/edit-crypt.sh
+++ bin/edit-crypt.sh
@@ -4,10 +4,12 @@
 # Description: crypt/decrypt strings
 #   need ~/.ssh/.gpgpass for passphrase
 . func.getpar
-salt="--passphrase-file $HOME/.ssh/.gpgpass"
+pass="$HOME/.ssh/.gpgpass"
+salt="--passphrase-file $pass"
 opt-d(){ #decrypt
     base64 -d|gpg $salt
     exit
 }
+[ -s "$pass" ] || _abort "No passphrse file"
 _usage "<file>"
 cat $*|gpg -c --force-mdc $salt|base64 -w0
