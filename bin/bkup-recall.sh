3d9adaa54ca1650fad43f33688bdb4ae50219b24
CIAX <omata@leonid.subaru.nao.ac.jp>
2014-04-11 15:33:54 -1000

    option write is splitted off;

--- bin/bkup-recall.sh
+++ bin/bkup-recall.sh
@@ -6,32 +6,34 @@
 #  -f means pick the original one (first stashed)
 #  otherwise pick last one
 . func.getpar
-sel="max"
-opt-f(){ sel="min"; } #first
-_usage "[file] (host)"
-name="$1"
-if [ -s "$name" ] ; then
-    cfid=$(bkup-stash $name)
+write(){
+    mv $content $file
+    read mode date < <(bkup-exec "select mode||' '||date from content where id == '$fid';")
+    chmod $mode $file
+    touch -d "@$date" $file
+}
+opt-w(){ write='write'; } #write
+_usage "[file] (count)"
+file="$1"
+if [ -s "$file" ] ; then
+    cfid=$(bkup-stash $file)
     uniq=" and fid != '$cfid'"
 fi
-host=${2:-$(hostname)}
-limit="name=='$name' and host=='$host' and dist=='$(info-dist)'"
+limit="name=='$file' and host=='$(hostname)' and dist=='$(info-dist)'"
 sub_fid="select fid from list where $limit $uniq"
-sub_date="select $sel(date) from content where id in ($sub_fid)"
+sub_date="select max(date) from content where id in ($sub_fid)"
 date=$(bkup-exec "$sub_date;")
 sub_id="select id from content where date == '$date'"
 fid=$(bkup-exec "$sub_id;")
 if [ "$fid" ] ; then
-    bkup-exec "select base64 from content where id == '$fid';"|base64 -d|zcat > $name
-    bkup-exec "select mode,date from content where id == '$fid';" | {
-        IFS=,;read mode date
-        chmod $mode $name
-        touch -d "@$date" $name
-    }
+    . func.temp
+    _temp content
+    bkup-exec "select base64 from content where id == '$fid';"|base64 -d|zcat > $content
+    $write
     sub_fid="select fid from list where $limit"
     total=$(bkup-exec "select count(id) from content where id in ($sub_fid);")
     rank=$(bkup-exec "select count(id) from content where id in ($sub_fid) and date > '$date';")
     echo "Recall OK($rank/$total)"
 else
-    _abort "No such id stored for $host"
+    _abort "No such file stored"
 fi
