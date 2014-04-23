b8948aab557668e531393d24bdf37104b53d7f2e
Koji Omata <omata@leonid.subaru.nao.ac.jp>
2014-04-23 13:18:06 -1000

    add reg-bin/ for just register scripts to ~/bin;

--- rec-dirs.sh
+++ rec-dirs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# Description: recursive dirs
+# Description: move to subdirs recursively except symlinks and exec command
+# Usage: $0 (command)
 shopt -s nullglob
 cmd="${*:-pwd -P}"
-showdir(){
+recdir(){
     [ -h "${1%/}" ] && return
     cd $1 || return
     eval "$cmd"
     for i in */ ;do
-        (showdir $i)
+        (recdir $i)
     done
 }
-showdir ~
+recdir .
---
+++ reg-bin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Description: make links of script files to ~/bin
+# if dst file exists -> dst=regular file:>fail , dst=org link:>skip
+# Create or Overwrite unexist link
+mklink(){
+    local src="$1";shift
+    local dst="$1";shift
+    if [ -e $dst ] ; then
+        if [ -h $dst ]; then
+            [ "$src" = `readlink $dst` ] && return 1
+            echo $C3"Warning: link of $dst is different from $src"$C0
+        else
+            echo $C1"Error: $dst is regular file"$C0
+            return 1
+        fi
+    fi
+    ln -sf $src $dst
+}
+shopt -s nullglob
+[ -d "$HOME/bin" ] || mkdir "$HOME/bin"
+link=''
+for i in *.sh *.pl *.py *.rb *.awk *.exp *.js; do
+    [ -d "$i" -o ! -e "$i" -o -h "$i" -o ! -x "$i" ] && continue
+    mklink "$(pwd -P)/$i" "$HOME/bin/${i%.*}" && link+=" $i"
+done
+[ "$link" ] && echo "[$link ] -> $C1~/bin$C0"
