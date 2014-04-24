921d36d5655db5bf466005183d269dca4fd2064a
Koji Omata <omata@leonid.subaru.nao.ac.jp>
2014-04-24 10:35:19 -1000

    move files;

--- bin/file-reg.sh
+++ bin/file-reg.sh
@@ -3,14 +3,17 @@
 # Description: make links to the specific dirs categorized by file type 
 # Desctiption: Files in current dir will be classified into 'bin','db' ..
 # "Usage: ${0##*/} [DIR..] | [SRC..]"
+selflink(){
+    cd ~/bin
+    while read file dst;do
+        src=${file%:*}
+        _mklink $(readlink $src) $dst
+    done < <(egrep "^[#;]link(\($DIST\)|) " *)
+}
 . func.link
 . func.dirs
 echo $C3"File Registering"$C0
 _subdirs _binreg
-cd ~/bin
-while read file dst;do
-    src=${file%:*}
-    _mklink $(readlink $src) $dst
-done < <(egrep "^#link(\($DIST\)|) " *)
+selflink
 _showlink
 file-clean
