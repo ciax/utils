be6bd099162094736396fa1b92a4466582763156
CIAX <ciax@debmini.subaru.nao.ac.jp>
2014-04-23 15:17:43 -1000

    debug: str should be enclosed '';

--- setup-bashrc.sh
+++ setup-bashrc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Description: setup bashrc
 if ! grep -q '#initrc' ~/.bashrc ; then
-    str="shopt -s nullglob;for i in ~/bin/rc.*;do . $i;done #initrc"
-    echo "$str" >> ~/.bashrc
+    echo 'shopt -s nullglob;for i in ~/bin/rc.*;do . $i;done #initrc' >> ~/.bashrc
 fi
