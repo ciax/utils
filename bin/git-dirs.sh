9d972d2cb842f56f241e9cc837c9a3663bdc1827
CIAX <ciax@debmini.subaru.nao.ac.jp>
2014-04-23 15:40:05 -1000

    use rec-dirs;

--- bin/git-dirs.sh
+++ bin/git-dirs.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
+# Required scripts: rec-dirs
 # Description: dirlist that has .git
-shopt -s nullglob
-showdir(){
-    [ -h "${1%/}" ] && return
-    cd $1 || return
-    if [ -d ".git" ] ; then
-        pwd -P
-    else
-        for i in */ ;do
-            (showdir $i)
-        done
-    fi
-}
-showdir ~
+cd
+rec-dirs '[ -d ".git" ] && pwd -P'
