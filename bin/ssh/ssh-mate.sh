02f566c889aa0154e91f5cd276bda1f092ba587f
Koji Omata <omata@leonid.subaru.nao.ac.jp>
2014-04-28 09:48:26 -1000

    add ssh-mate/ list mate accounts; ssh-merge/ use ssh-mate;

---
+++ bin/ssh/ssh-mate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Required scripts: edit-merge, ssh-push, ssh-valid
+# Description: join to the group which opject host is involved
+# Usage: ssh-merge (user@host) ..
+#   If no args, then hosts are picked up from authorized_keys
+ath=~/.ssh/authorized_keys
+pub=~/.ssh/id_rsa.pub
+read rs mykey me < $pub
+cut -d' ' -f3 $ath|grep @|grep -v $me
