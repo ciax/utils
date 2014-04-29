ae12468bbb67047faa2e977f210593a573c2604d
Koji Omata <omata@naoj.org>
2014-04-29 02:45:52 -1000

    order of conig -> tail;

--- bin/ssh/ssh-setup.sh
+++ bin/ssh/ssh-setup.sh
@@ -10,10 +10,10 @@ inv=~/.ssh/invalid_keys
 sec=~/.ssh/id_rsa
 pub=~/.ssh/id_rsa.pub
 cfg=~/.ssh/config
-ssh-config > $cfg
 [ "$1" = -r ] && rm $sec $pub
 type ssh > /dev/null || _abort "No ssh installed"
 [ -e $sec ] || ssh-keygen
 [ -e $pub ] || ssh-keygen -y -f $sec > $pub
 [ -e $ath ] || touch $ath
 [ -e $inv ] || touch $inv
+ssh-config > $cfg
