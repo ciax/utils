d038567b6317b913352bd44f29d3b975dd5d85ad
Koji Omata <omata@leonid.subaru.nao.ac.jp>
2014-04-30 17:16:18 -1000

    add connection type (fiber '=');

--- bin/net-tree.sh
+++ bin/net-tree.sh
@@ -5,29 +5,30 @@
 . func.getpar
 
 open_super(){
-    connect[$1]="---${2:-$C2}"
+    connect[$1]=true
     local up="${super[$1]}"
     [ "$up" ] && [ ! "${connect[$up]}" ] && open_super "$up"
 }
 
 get_hubs(){
-    while read crh sup desc; do
+    while read self sup wire desc; do
         sup="${sup:-top}"
-        sub[$sup]+="|$crh" # add itself to parent var
-        super[$crh]="$sup"
-        title[$crh]=$desc$C0
-        eval $(db-trace $crh hub subnet)
-    done < <(db-exec "select id,super,description from hub where subnet == '$1';"|sort)
+        sub[$sup]+="|$self" # add itself to parent var
+        super[$self]="$sup"
+        [ "$wire" = opt ] && fiber[$self]=true
+        title[$self]="$desc"
+        eval $(db-trace $self hub subnet)
+    done < <(db-exec "select id,super,wire,description from hub where subnet == '$1';"|sort)
 }
 
 get_hosts(){
     for sup in ${!title[*]};do
-        while read hst static_ip; do
-            sub[$sup]+="|$hst:"
-            super[$hst:]="$sup"
-            title[$hst:]="$hst$C0"
-            site="${static_ip:-$hst}"
-            chk_host "$site" && open_super $hst: "$C4"
+        while read self static_ip; do
+            sub[$sup]+="|$self:"
+            super[$self:]="$sup"
+            title[$self:]="$self"
+            site="${static_ip:-$self}"
+            chk_host "$site" && open_super $self:
         done < <(db-exec "select id,static_ip from host where hub == '$sup';")
     done
 }
@@ -37,7 +38,20 @@ show_tree(){
     last="${sub[$1]##*|}"
     local ind="$2   "
     for i in ${sub[$1]#*|};do
-        echo "${ind}|${connect[$i]:--${C1}X-}${title[$i]}"$C0
+        echo -n "${ind}|"
+        if [ "${fiber[$i]}" ] ; then
+            c='='
+            echo -n $C3
+        else
+            c='-'
+        fi
+        if [ "${connect[$i]}" ] ; then
+            echo -n "$c$c$c"
+            [[ $i == *: ]] && echo -n $C4 || echo -n $C2
+        else
+            echo -n "$c${C1}X$c"
+        fi
+        echo "${title[$i]}"$C0
         if [ "$i" = "$last" ];then
             show_tree $i "$ind "
         else
@@ -63,6 +77,7 @@ _usage "[subnet]" <(db-list subnet)
 declare -A sub
 declare -A super
 declare -A title
+declare -A fiber
 declare -A connect
 IFS='|'
 get_hubs $1
