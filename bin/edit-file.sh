4f9abf565fed91462ff3ba1c0851d1b96a0c3384
CIAX <ciax@debmini.subaru.nao.ac.jp>
2014-05-12 14:33:28 -1000

    takes line number like [file:line];

--- bin/edit-file.sh
+++ bin/edit-file.sh
@@ -4,11 +4,17 @@
 # Required scripts: func.getpar
 # Description: edit file with emacs as its owner
 . func.getpar
-_usage "[file]"
-if [ -s "$1" ];then
-    user=$(stat -c %U $1)
-    cp -pub $1 ~/.trash/
-else
-    user=$LOGNAME
-fi
-sudo -u $user emacs -nw $*
+_usage "[file(:line)]"
+args=''
+for arg;do
+    read file line <<< "${arg//:/ }"
+    if [ -s "$file" ];then
+        user=$(stat -c %U $file)
+        cp -pub $file ~/.trash/
+    else
+        user=$LOGNAME
+    fi
+    [ "$line" ] && args+=" +$line"
+    args+=" $file"
+done
+sudo -u $user emacs -nw $args
