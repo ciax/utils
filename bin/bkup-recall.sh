692184dc4dbb77d9e5ffa0e1965f16cebefc2629
Koji Omata <omata@leonid.subaru.nao.ac.jp>
2014-05-16 10:08:45 -1000

    show timestamp for recalled file;

--- bin/bkup-recall.sh
+++ bin/bkup-recall.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #alias recall
 # Required scripts: func.getpar info-dist bkup-stash bkup-exec
-# Description: recall backed up file
-#  -f means pick the original one (first stashed)
-#  otherwise pick last one
+# Description: recall latest backed up file
 . func.getpar
 write(){
     mv $content $file
-    read mode date < <(bkup-exec "select mode||' '||date from content where id == '$fid';")
+    mode=$(bkup-exec "select mode from content where id == '$fid';")
     chmod $mode $file
     touch -d "@$date" $file
 }
-opt-w(){ write='write'; } #write
-_usage "[file] (count)"
+opt-w(){ func='write'; } #overwrite
+_usage "[file]"
 file="$1"
 if [ -s "$file" ] ; then
     cfid=$(bkup-stash $file)
     uniq=" and fid != '$cfid'"
 fi
+#get fid
 limit="name=='$file' and host=='$(hostname)' and dist=='$(info-dist)'"
 sub_fid="select fid from list where $limit $uniq"
 sub_date="select max(date) from content where id in ($sub_fid)"
@@ -27,12 +26,13 @@ fid=$(bkup-exec "$sub_id;")
 if [ "$fid" ] ; then
     . func.temp
     _temp content
-    bkup-exec "select base64 from content where id == '$fid';"|base64 -d|zcat > $content
-    $write
+    bkup-exec "select base64 from content where id == '$fid';"|base64 -d|zcat|tee $content
+    $func
     sub_fid="select fid from list where $limit"
     total=$(bkup-exec "select count(id) from content where id in ($sub_fid);")
     rank=$(bkup-exec "select count(id) from content where id in ($sub_fid) and date > '$date';")
-    echo "Recall OK($rank/$total)"
+    date=$(bkup-exec "select date from content where id == '$fid';")
+    echo $C3"Recall OK($rank/$total) [$(date --date=@$date)]"$C0
 else
     _abort "No such file stored"
 fi
