1fc6d8088ddaf5273a92fee0a2c6eb937ffa4068
Koji Omata <omata@leonid.subaru.nao.ac.jp>
2014-07-03 10:54:36 -1000

    info-subnet should take active interface;

--- bin/info-subnet.sh
+++ bin/info-subnet.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Required commands: netstat
 shopt -s nullglob
-eth=`netstat -i|cut -d' ' -f1|egrep '^eth'|sort|head -1`
-netstat -nr|egrep -o "^([12].+) +0\.0\.0\.0 +255\.255.*$eth"|cut -d' ' -f1
+netstat -nr|egrep -o "^([12].+) +0\.0\.0\.0 +255\.255.*eth"|cut -d' ' -f1
