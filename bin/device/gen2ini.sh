ad7ece1ddbf2ca4c8a64a078340af46702ab6315
CIAX <ciax@naoj.org>
2014-10-16 12:01:07 -1000

    add gen2 init command;

---
+++ bin/device/gen2ini.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+case `uname` in
+    SunOS)
+        ;;
+    Linux)
+        PYTHONPATH=~/gen2/share/Git/python
+        [ -d "$PYTHONPATH" ] || { echo "NO OSS files"; exit; }
+        ln -sf $PYTHONPATH/Gen2/client/g2cmd.py ~/bin/OSST_ciaxTSCcommand
+        ln -sf $PYTHONPATH/SOSS/status/screenPrint.py ~/bin/OSSC_screenPrint
+        ;;
+esac
