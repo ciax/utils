14698410a5b4ed9f45fac78ea3fbede1d74e0846
Koji Omata <omata@leonid.subaru.nao.ac.jp>
2014-12-05 08:22:15 -1000

    rename net-hosts -> list-hosts;

--- bin/net-wakeonlan.sh
+++ bin/net-wakeonlan.sh
@@ -5,5 +5,5 @@
 # Required tables: mac(host)
 # Description: make network devices wake up
 . func.getpar
-_usage "[host]" <(net-hosts)
+_usage "[host]" <(list-hosts)
 wakeonlan $(db-exec "select id from mac where host == '$1';")
