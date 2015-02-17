dc69c6629a502422e53d6b4c075a1c032a23f251
CIAX <ciax@naoj.org>
2015-02-17 10:50:48 -1000

    add exe used in gen2cmd;

---
+++ bin/device/exe.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Usage: exe (-b|-p) [command]
+# -b:back ground execution
+# command is exclusive
+exit=~/.var/exit.txt
+pid=~/.var/pid.txt
+exelog=~/.var/exelog.txt
+[ -f $exit ] || echo "0" > $exit
+[ -f $pid ] || touch $pid
+case "$1" in
+    -b)
+        shift
+        if [ "$1" ] ; then
+            if [ -s $pid ]; then
+                echo "Rejected" >&2
+                echo "1"
+            else
+                {
+                    echo "[$(date +%D-%T)]" >> $exelog
+                    echo "% $* ($$)" >> $exelog
+                    echo "$$" > $pid
+                    eval $* >> $exelog 2>&1
+                    echo "$?" > $exit
+                    echo -e "[exitcode=$(cat $exit)]\n" >> $exelog
+                    : > $pid #clear
+                } & echo "0"
+            fi
+        elif [ -s $pid ] ; then
+            cat $pid
+        else
+            echo "0"
+        fi;;
+    '') cat $exit;;
+    *)
+        echo "[$(date +%D-%T)]" >> $exelog
+        echo "% $*" >> $exelog
+        eval $* >> $exelog 2>&1
+        echo "$?"|tee $exit
+        echo -e "[exitcode=$(cat $exit)]\n" >> $exelog
+        ;;
+esac
+
