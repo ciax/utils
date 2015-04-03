b777397616e7a67b6c45284566c486bc2b553ae8
Koji Omata <omata@naoj.org>
2015-04-03 03:49:16 -1000

    ssh-upnp/ setting includes generating rc file;

--- bin/ssh/ssh-upnp.sh
+++ bin/ssh/ssh-upnp.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 # Requied packages: miniupnpc
-pfile=~/.var/ssh-export.txt
+dir=/etc/network/if-up.d
+file=ssh-upnp
 case "$1" in
     -d)
-        ext=${2:-$(<$pfile)}
-        upnpc -d $ext tcp
-        ;;
-    -c)
-        file=~/ssh-upnp
-        scr="$HOME/utils/bin/ssh/ssh-upnp.sh ${2:-$(<$pfile)}"
-        echo $scr > $file
-        chmod +x $file
-        sudo mv $file /etc/network/if-up.d/
-        cat /etc/network/if-up.d/ssh-upnp
+        if [ -e $dir/$file ] ; then
+            set - $(<$dir/$file)
+            scr="upnpc -d $5 tcp"
+            sudo rm $dir/$file
+            echo $scr
+            $scr
+        else
+            upnpc -d $2 tcp
+        fi
         ;;
     [0-9]*)
         ext=$1
         a=$(ip route)
-        l=${a##*src }
-        upnpc -a $l 22 $ext tcp
-        echo $ext > $pfile
+        set - ${a##*src }
+        scr="upnpc -a $1 22 $ext tcp"
+        cd
+        echo $scr > $file
+        chmod +x $file
+        sudo mv $file $dir
+        $scr
         ;;
     *)
-        echo "$0 (-d,c) [port]"
+        echo "$0 (-d) [port]"
         ;;
 esac
