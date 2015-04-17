8fa86879286fcfc71939b908bca04847cc5c512e
Koji Omata <omata@naoj.org>
2015-04-16 19:45:22 -1000

    rc missed to exec rc.login;

--- bin/setup-bashrc.sh
+++ bin/setup-bashrc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Description: setup bashrc
 if ! grep -q '#initrc' ~/.profile ; then
-    echo 'shopt -s nullglob;for i in ~/bin/rc.login.*;do . $i;done #initrc' >> ~/.profile
+    echo 'shopt -s nullglob;for i in ~/bin/rc.login*;do . $i;done #initrc' >> ~/.profile
 fi
 if ! grep -q '#initrc' ~/.bash_logout ; then
-    echo 'shopt -s nullglob;for i in ~/bin/rc.logout.*;do . $i;done #initrc' >> ~/.bash_logout
+    echo 'shopt -s nullglob;for i in ~/bin/rc.logout*;do . $i;done #initrc' >> ~/.bash_logout
 fi
