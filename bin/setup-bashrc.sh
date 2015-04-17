bd13aa92e53abf73741bd1532bf4fcda6799056b
Koji Omata <omata@naoj.org>
2015-04-16 19:38:54 -1000

    rename rc files (rc.utils.* -> rc.login.*);

--- bin/setup-bashrc.sh
+++ bin/setup-bashrc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Description: setup bashrc
-if ! grep -q '#initrc' ~/.bashrc ; then
-    echo 'shopt -s nullglob;for i in ~/bin/rc.*;do . $i;done #initrc' >> ~/.bashrc
+if ! grep -q '#initrc' ~/.profile ; then
+    echo 'shopt -s nullglob;for i in ~/bin/rc.login.*;do . $i;done #initrc' >> ~/.profile
+fi
+if ! grep -q '#initrc' ~/.bash_logout ; then
+    echo 'shopt -s nullglob;for i in ~/bin/rc.logout.*;do . $i;done #initrc' >> ~/.bash_logout
 fi
