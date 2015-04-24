408619cc8ef6b3b8d2d4f78cb91ec56414f9b34d
Koji Omata <omata@raspi3.(none)>
2015-04-23 16:15:17 -1000

    cfg-append/ ignore existent line;

--- bin/server/cfg-append.sh
+++ bin/server/cfg-append.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 # Required scripts: func.temp
 # Description: append stdin to config file which is included in stdin as a comment
+#              and ignore existent line
 # Usage: cfg-append < cfgfile
+# Example: cfg-hosts | cfg-append
 . func.temp
 _usage "<newcfg>"
-_temp copy
-rem=$(tee $copy|grep "^#/")
+_temp infile outfile
+rem=$( tee $infile | grep "^#/" )
 file=${rem#*#}
 [ "$file" ] || _abort "No cfg file name"
-[ -e "$file" ] || sudo touch $file
-[ -e "$file.org" ] || sudo mv $file $file.org
-sudo cp $file.org $file
-sudo grep -v "^#" $copy | sudo tee -a $file
+user=$(_fuser $file)
+cat $file > $outfile
+while read line; do
+    [ "$line" ] || continue
+    egrep -q "$line" $file && continue
+    echo "$line" >> $outfile
+done < <(egrep -v "^#" $infile)
+sudo install -o $user $outfile $file
--- devices/raspi/raspi-cam.sh
+++ devices/raspi/raspi-cam.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+dir=/var/www/media
+[ -d $dir ] || sudo mkdir -p $dir
 fname=raspi$(date +%y%m%d-%H%M%S).jpg
 sudo raspistill -o /var/www/media/$fname -w 640 -h 480
 
