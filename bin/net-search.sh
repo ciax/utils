ae4f7a31b4f7c28552759f3ac30e904f7db308c2
Koji Omata <omata@leonid.subaru.nao.ac.jp>
2015-04-24 16:23:33 -1000

    net-search/ reserve arp list;

--- bin/net-search.sh
+++ bin/net-search.sh
@@ -4,41 +4,37 @@
 # Description: search IP addres by mac
 #alias mac
 . func.temp
+xopt-r(){ #Refresh arp
+    eval "$(info-net)"
+    echo "Scannig network ($cidr)"
+    nmap -sn $cidr > /dev/null 2>&1
+    arp -n|grep -v incomplete > $alist
+}
 opt-s(){ #Set to /etc/hosts
-    egrep -v " ($reg)$" /etc/hosts > $hosts
+    egrep -v " ($hreg)$" /etc/hosts > $hosts
     cat $hlist >> $hosts
     _overwrite $hosts /etc/hosts
     echo "Set to /etc/hosts"
 }
+alist=~/.var/arplist.txt
 _usage "[host].."
-declare -A macs
+declare -A hos
 for host ; do
     mac=$(search-mac $host)
     if [ "$mac" ] ; then
-        macs[$host]=$mac
-        reg="$reg${reg:+|}$host"
+        hos[$mac]=$host
+        mreg="$mreg${mreg:+|}$mac"
+        hreg="$hreg${hreg:+|}$host"
     else
         echo "No such host $host"
     fi
 done
-[ "$reg" ] || exit 1
-
-_temp mlist hlist hosts
-eval "$(info-net)"
-echo "Scannig network ($cidr)"
-nmap -sn $cidr > /dev/null 2>&1
-arp -n|grep -v incomplete > $mlist
-for host in ${!macs[*]} ; do
-    mac=${macs[$host]}
-    set - $(egrep -i "($mac)" $mlist)
-    ip=$1
-    if [ "$ip" ] ; then 
-        echo "Find $ip for $host ($mac)"
-        echo "$ip    $host" >> $hlist 
-    else
-        echo "Can't find $host"
-    fi
-done
+[ "$hreg" ] || exit 1
+[ -f $alist ] || xopt-r
+_temp hlist hosts
+while read ip type mac other ; do
+    host=${hos[$mac]}
+    echo "Find $ip for $host ($mac)"
+    echo "$ip    $host" >> $hlist 
+done < <(egrep -i "($mreg)" $alist)
 _exe_opt
-
-
