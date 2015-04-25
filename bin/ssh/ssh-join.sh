5f4cb23f810e0e8dc443f2c93d118664f70e55fe
Koji Omata <omata@leonid.subaru.nao.ac.jp>
2015-04-24 16:17:23 -1000

    ssh-join didn't work (wrong url handling);  port takes from ssh-config;

--- bin/ssh/ssh-join.sh
+++ bin/ssh/ssh-join.sh
@@ -3,15 +3,15 @@
 # Desctiption: share authorized keys with remote host (Accepts join)
 . func.temp
 getrem(){
-    scp $sshopt -pq -P $port $rhost:$1 $2
+    scp $sshopt -pq ${port:+-P $port} $rhost:$1 $2
 }
 putrem(){
     cmp -s $1 $2 && return
-    scp $sshopt -pq -P $port $1 $rhost:$3
+    scp $sshopt -pq ${port:+-P $port} $1 $rhost:$3
     echo "${3##*/}($(stat -c%s $1)) is updated at $rhost"
 }
 _usage "[(user@)host(:port)] .."
-setup-ssh
+#setup-ssh
 sshopt="-o StrictHostKeyChecking=no"
 ath=.ssh/authorized_keys
 inv=.ssh/invalid_keys
@@ -19,11 +19,10 @@ lath=~/$ath
 linv=~/$inv
 _temp rath rinv tath tinv
 for url;do
-    rhost=${url%:*}
-    p=${url#$rhost:}
-    port=${p:-22}
-    [[ ${rhost#*@} =~ "`hostname`|localhost" ]] && _abort "Self push"
-    echo "Host:$rhost:$port"
+    IFS=:; set - $url; unset IFS
+    rhost=$1; port=$2
+    [[ "$rhost" =~ (`hostname`|localhost) ]] && _abort "Self push"
+    echo "Host $rhost${port:+:$port}"
 # Get files from remote
     getrem $ath $rath
     getrem $inv $rinv
