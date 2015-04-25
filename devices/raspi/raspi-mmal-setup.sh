3462e606ed8e291ea8ba3cec69fdf05c2be24428
Koji Omata <omata@raspi3.(none)>
2015-04-24 17:38:26 -1000

    mmal setup for binary;

--- devices/raspi/raspi-mmal-setup.sh
+++ devices/raspi/raspi-mmal-setup.sh
@@ -2,11 +2,6 @@
 # You need to join video group
 [ -d ~/mmal ] || mkdir ~/mmal
 sudo apt-get install motion libjpeg62-dev libavcodec-dev
-sudo ln -s /usr/lib/arm-linux-gnueabihf /usr/lib64
 sudo modprobe bcm2835-v4l2
-git clone https://github.com/dozencrows/motion.git
-cd ~/motion
-./configure
-make
-sudo make install
-
+cd ~/mmal
+tar xvzf ~/utils/devices/raspi/motion-mmal.tar.gz
