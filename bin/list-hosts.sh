6a020fdb6a1cbbd7586ed3c5a6a37c8e9ffa98db
Koji Omata <omata@naoj.org>
2015-05-05 17:13:35 -1000

    add dhcp host only option (-d);

--- bin/list-hosts.sh
+++ bin/list-hosts.sh
@@ -3,10 +3,14 @@
 # Required tables: host
 # Description: show hosts which belong to same subnet
 . func.getpar
+opt-d(){
+    rstr="and assign == 'dhcp'"
+}
 subnet="${1:-$(net-name)}"
+_exe_opt
 db-exec <<EOF
 select id from host where hub in (
-  select id from hub where subnet == '$subnet'
+  select id from hub where subnet == '$subnet' $rstr
 )
 ;
 EOF
