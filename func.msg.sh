2099880e09644bdbf5a2f563d65b20902f5c329a
CIAX <ciax@numoircs.com>
2015-05-06 13:52:44 -1000

    colorling is moved from rc.bash to func.msg;

--- func.msg.sh
+++ func.msg.sh
@@ -3,6 +3,15 @@
 # Usage:
 #  souce $0 at head of file,
 shopt -s nullglob
+# Coloring for Console
+#  ESC[(A);(B)(C)m # A: 0=dark 1=light # B: 3=FG 4=BG # C: 1=R 2=G 4=B
+#  environment variable "C?" are provided
+if [ -t 2 ] ; then
+    for i in 1 2 3 4 5 6 7 ; do
+        eval "export C${i}=\$'\\e[1;3${i}m'"
+    done
+    C0=$'\e[0m'
+fi
 # Description: Print message to stderr
 _msg(){ echo "$C2$*$C0" 1>&2; }
 # Description: Print message to stderr
--- rc.bash.sh
+++ rc.bash.sh
@@ -4,15 +4,6 @@
 umask 022
 shopt -s nullglob
 complete -r
-# Coloring for login
-#  ESC[(A);(B)(C)m # A: 0=dark 1=light # B: 3=FG 4=BG # C: 1=R 2=G 4=B
-#  environment variable "C?" are provided
-if [ -t 2 ] ; then
-    for i in 1 2 3 4 5 6 7 ; do
-        eval "export C${i}=\$'\\e[1;3${i}m'"
-    done
-    C0=$'\e[0m'
-fi
 # Set environment for login
 # For PATH setting without duplication
 addpath(){
@@ -31,6 +22,5 @@ GREP_OPTIONS='--color=auto'
 # PROMPT
 PS1="\[\033[01;31m\][$SHLVL]\[\033[00m\]$PS1"
 # SET Dist
-info-os
 os=$(info-os)
 export DIST=${os#*/}
