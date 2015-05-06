9919b053e22dfce986d129eca8dc3343644d5a10
Koji Omata <omata@naoj.org>
2015-05-06 09:04:00 -1000

    ssh doesn't work if home dir perm is wrong;

--- bin/ssh/ssh-perm.sh
+++ bin/ssh/ssh-perm.sh
@@ -8,6 +8,7 @@ ath=~/.ssh/authorized_keys
 inv=~/.ssh/invalid_keys
 sec=~/.ssh/id_rsa
 pub=~/.ssh/id_rsa.pub
+cd;chmod 755 .
 [ -d ~/.ssh ] && cd ~/.ssh || exit
 chmod 700 .
 filemod 600 $ath $inv $sec $pub
