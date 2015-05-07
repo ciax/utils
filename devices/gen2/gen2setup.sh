2b3c060fea7c6b2446551069565b3f560c418645
CIAX <ciax@naoj.org>
2015-05-07 11:55:50 -1000

    gen2setup does only making links;

--- devices/gen2/gen2setup.sh
+++ devices/gen2/gen2setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Gen2 Boot up levels
-source ~/gen2/conf/bashrc
+[ -d ~/gen2 ] || { echo "NO OSS files"; exit; }
+ln -sf $HOME/gen2/conf/bashrc ~/bin/rc.bash.gen2
 PYTHONPATH=~/gen2/share/Git/python
-[ -d "$PYTHONPATH" ] || { echo "NO OSS files"; exit; }
 ln -sf $PYTHONPATH/Gen2/client/g2cmd.py ~/bin/OSST_ciaxTSCcommand
 ln -sf $PYTHONPATH/SOSS/status/screenPrint.py ~/bin/OSSC_screenPrint
