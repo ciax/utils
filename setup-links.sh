c1c4c4a61385e516364f4e692e7f640feb14fb7f
Koji Omata <omata@naoj.org>
2015-05-08 10:23:36 -1000

    setup-utils doesn't use setup-links; setup-links default target is utils/bin;

--- setup-links.sh
+++ setup-links.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-# Description: Dir setup and File registration
-for dir in bin .var .trash; do
-    [ -d "$HOME/$dir" ] || mkdir -p "$HOME/$dir"
-done
 PATH=$HOME/bin:$PATH
 . ~/utils/bin/func.msg.sh
 . ~/utils/bin/func.link.sh
 . ~/utils/bin/func.dirs.sh
 _warn "File Registering (~/bin)"
-for i in ~/utils $*;do
+for i in ~/utils/bin $*;do
     pushd $i >/dev/null
     _subdirs _binreg
     popd >/dev/null
--- setup-utils.sh
+++ setup-utils.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # Description: install utils (need to re-login for environment effective)
 echo "Setting up utils"
-. ~/utils/setup-links.sh
+# Description: Dir setup and File registration
+for dir in bin .var .trash; do
+    [ -d "$HOME/$dir" ] || mkdir -p "$HOME/$dir"
+done
+PATH=$HOME/bin:$PATH
+cd ~/utils/bin
+. ./func.link.sh
+_binreg
+cd
 file-selflink
 db-update
 bashrc-setup
