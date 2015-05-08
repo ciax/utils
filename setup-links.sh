06250181268c81c7f51a1f4f2d4080809f49d94f
Koji Omata <omata@naoj.org>
2015-05-08 09:46:48 -1000

    move shared files into util/bin;

--- setup-links.sh
+++ setup-links.sh
@@ -4,9 +4,9 @@ for dir in bin .var .trash; do
     [ -d "$HOME/$dir" ] || mkdir -p "$HOME/$dir"
 done
 PATH=$HOME/bin:$PATH
-. ~/utils/func.msg.sh
-. ~/utils/func.link.sh
-. ~/utils/func.dirs.sh
+. ~/utils/bin/func.msg.sh
+. ~/utils/bin/func.link.sh
+. ~/utils/bin/func.dirs.sh
 _warn "File Registering (~/bin)"
 for i in ~/utils $*;do
     pushd $i >/dev/null
