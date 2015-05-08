2c81b146937ee93af833a77f74a25f50df42e3f1
Koji Omata <omata@naoj.org>
2015-05-08 11:45:43 -1000

    debug: file name;

--- bin/file-register.sh
+++ bin/file-register.sh
@@ -2,7 +2,7 @@
 # Required scripts: func.link file-clean file-selflink
 # Description: Script file registration to ~/bin
 . file-clean ~/bin
-. func.link.sh
+. func.link
 _warn "File Registering ($HOME/bin)"
 for i in ~/utils/bin $*;do
     pushd $i >/dev/null
