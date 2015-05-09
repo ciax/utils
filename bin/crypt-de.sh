9e4059dd90c20dfb25a956c3035db14735f99e48
Koji Omata <omata@naoj.org>
2015-05-09 12:03:27 -1000

    bugfix: wrong dir for gpgpass;

--- bin/crypt-de.sh
+++ bin/crypt-de.sh
@@ -4,7 +4,7 @@
 # Description: decrypt strings
 #   need ~/.ssh/.gpgpass for passphrase
 . func.getpar
-passfile="$HOME/.var/gpgpass"
+passfile="$HOME/.ssh/gpgpass"
 salt="--passphrase-file $passfile"
 _usage "<file>"
 base64 -d|gpg -q $salt || rm $passfile
