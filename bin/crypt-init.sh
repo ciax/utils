68f3c66aa452d3016106a26e1fa71daa64d63ea4
Koji Omata <omata@naoj.org>
2015-05-08 15:44:31 -1000

    move files;

--- bin/crypt-init.sh
+++ bin/crypt-init.sh
@@ -3,6 +3,6 @@
 #   need ~/.ssh/.gpgpass for passphrase
 passfile="$HOME/.var/gpgpass"
 grep -q . "$passfile" >/dev/null 2>&1 && exit
-read -p "GPG Password:" -e passphrase < /dev/tty
+read -sp "GPG Password:" -e passphrase < /dev/tty
 echo "$passphrase" > $passfile
 chmod 600 $passfile
