69c14aae58d082f375119f2b1956b9da655ac9c6
Koji Omata <omata@naoj.org>
2015-05-11 22:26:01 -1000

    add some func in func.ssh;

--- bin/func.ssh.sh
+++ bin/func.ssh.sh
@@ -2,7 +2,7 @@
 # Required scripts: func.temp
 # Description: mark '#' if the line with own name is found in authorized_keys,
 #   maching own id_rsa.pub and the line, otherwise move older one to invalid_keys
-#link ssh-mark
+#link auth-mark
 #link ssh-trim
 #link ssh-mates
 #link ssh-perm
@@ -12,12 +12,17 @@
 . func.text
 ATH=.ssh/authorized_keys
 INV=.ssh/invalid_keys
-SEC=.ssh/id_rsa
-PUB=.ssh/id_rsa.pub
-CFG=.ssh/config
-_ssh-mark(){ # Mark '#' for own old pubkey [authorized_keys]
-    local ath=${1:-~/$ATH}
-    local pub=~/$PUB
+SEC=~/.ssh/id_rsa
+PUB=~/.ssh/id_rsa.pub
+CFG=~/.ssh/config
+LATH=~/$ATH
+LINV=~/$INV
+MATH=$LATH.merge
+MINV=$LINV.merge
+AATH=$LATH.anony
+_auth-mark(){ # Mark '#' for own old pubkey [authorized_keys]
+    local ath=${1:-$LATH}
+    local pub=$PUB
     [ -f $ath -a -f $pub ] || _abort "No ssh files"
     local rsa mykey me tath line
     read rsa mykey me < $pub
@@ -32,16 +37,16 @@ _ssh-mark(){ # Mark '#' for own old pubkey [authorized_keys]
 }
 # Required scripts: line-dup
 _ssh-trim(){ # Remove dup key [authorized_keys] [invalid_keys]
-    local ath=${1:-~/$ATH}
-    local inv=${2:-~/$INV}
+    local ath=${1:-$LATH}
+    local inv=${2:-$LINV}
     # Split file into invalid_keys by #headed line
     local tath tinv tdup
     _temp tath tinv tdup
     cp $inv $tinv
     ## For invalid_keys (increase only -> merge)
     grep "^#" $ath|\
-	while read line;do
-	    md5sum <<< $line | cut -c-32
+        while read line;do
+            md5sum <<< $line | cut -c-32
         done >> $tinv
     sort -u $tinv | _overwrite $inv
     ## For authorized_keys (can be reduced -> _overwrite)
@@ -51,18 +56,18 @@ _ssh-trim(){ # Remove dup key [authorized_keys] [invalid_keys]
     local line
     while read line;do
         if grep -q "$line" $tdup; then
-	    _warn "Remove Duplicated Key ${line##* }"
-	elif grep $(md5sum <<< $line | cut -c-32) $inv > $tinv; then
-	    _warn "Remove Invalid Key for ${line##* } ($(< $tinv))"
-	else
+            _warn "Remove Duplicated Key ${line##* }"
+        elif grep $(md5sum <<< $line | cut -c-32) $inv > $tinv; then
+            _warn "Remove Invalid Key for ${line##* } ($(< $tinv))"
+        else
             echo "$line"
-	fi
+        fi
     done < $tath | _overwrite $ath && _warn "authorized_key was updated"
 }
 _ssh-mates(){ # List the mate accounts in authorized_keys
     local dmy me
-    read dmy dmy me < ~/$PUB
-    cut -d' ' -f3 ~/$ATH|grep @|grep -v $me
+    read dmy dmy me < $PUB
+    cut -d' ' -f3 $LATH|grep @|grep -v $me
 }
 _ssh-perm(){ # Set ssh related file permission
     _setp 755 ~
@@ -75,11 +80,11 @@ _ssh-perm(){ # Set ssh related file permission
 }
 _ssh-setup(){ # Setup ssh
     type ssh > /dev/null || _abort "No ssh installed"
-    [ -e ~/$SEC ] || ssh-keygen
-    [ -e ~/$PUB ] || ssh-keygen -y -f ~/$SEC > ~/$PUB
-    [ -e ~/$INV ] || touch ~/$INV
-    [ -e ~/$ATH ] || cp ~/$PUB ~/$ATH
-    grep -q "$(< ~/$PUB)" ~/$ATH || cat ~/$PUB >> ~/$ATH
+    [ -e $SEC ] || ssh-keygen
+    [ -e $PUB ] || ssh-keygen -y -f $SEC > $PUB
+    [ -e $LINV ] || touch $LINV
+    [ -e $LATH ] || cp $PUB $LATH
+    grep -q "$(< $PUB)" $LATH || cat $PUB >> $LATH
 }
 _sshopt(){
     IFS=:;set - $1;rhost=$1;port=$2;unset IFS
@@ -90,37 +95,43 @@ _ssh-fetch(){ # Fetch and merge auth key (user@host:port)
     _sshopt $1 || return 1
     local rath rinv
     _temp rath rinv
-    local math=~/$ATH.merge
-    local minv=~/$INV.merge
     echo "Host $rhost${port:+:$port}"
     # Get files from remote
     scp -pq $sshopt $rhost:$ATH $rath
     scp -pq $sshopt $rhost:$INV $rinv
     # Join with local file
-    cat $rath ~/$ATH > $math
-    cat $rinv ~/$INV > $minv
+    cat $rath $LATH > $MATH
+    cat $rinv $LINV > $MINV
+    cut -d' ' -f1-2 $LATH > $AATH
+    cat $rath >> $AATH
     # Trimming
-    _ssh-mark $math
-    _ssh-trim $math $minv >/dev/null
+    _auth-mark $MATH
+    _auth-mark $AATH
+    _ssh-trim $MATH $MINV >/dev/null
+    _ssh-trim $AATH $MINV >/dev/null
 }
 _ssh-admit(){
     _ssh-fetch $1 || return 1
-    _overwrite < ~/$ATH.merge ~/$ATH
-    _overwrite < ~/$INV.merge ~/$INV
+    _overwrite < $LATH.merge $LATH
+    _overwrite < $LINV.merge $LINV
 }
 _ssh-anonymous(){
     local anonymous
     _temp anonymous
-    cut -d' ' -f1-2 < ~/$ATH.merge > $anonymous
-    _overwrite < $anonymous > ~/$ATH.merge
+    cut -d' ' -f1-2 < $LATH.merge > $anonymous
+    _overwrite < $anonymous > $LATH.merge
 }
-_ssh-push(){
-    scp -pq $sshopt ~/$ATH.merge $rhost:$ATH
-    scp -pq $sshopt ~/$INV.merge $rhost:$INV
+_ssh-push-all(){
+    scp -pq $sshopt $AATH $rhost:$ATH
+    scp -pq $sshopt $LINV.merge $rhost:$INV
+}
+_ssh-share(){
+    scp -pq $sshopt $LATH $rhost:$ATH
+    scp -pq $sshopt $LINV $rhost:$INV
 }
 #link ssh-push-inv
 _ssh-push-inv(){
     _sshopt $1 || return 1
-    scp -pq $sshopt ~/$INV $rhost:$INV
+    scp -pq $sshopt $LINV $rhost:$INV
 }
 _chkfunc $*
