89a9206afca96bb99454b9372068641391eceb22
Koji Omata <omata@naoj.org>
2015-05-12 16:56:39 -1000

    remove unused file; func.text:text-dup() -> use 'uniq -d' instead; edit-write() -> _overwrite() can take STDIN;

--- bin/edit-write.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#alias wri
-# Required scripts: func.getpar
-# Description: Overwrite if these are different.
-. func.getpar
-_usage "[file] < (input)"
-file=$1;shift
-cat $* | _overwrite $file
