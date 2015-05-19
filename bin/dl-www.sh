8cabbea90f9d2adc85ab866d7a14ac7f1aae8e13
Koji Omata <omata@naoj.org>
2015-05-18 19:06:54 -1000

    remove cr from indexfile;

--- bin/dl-www.sh
+++ bin/dl-www.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Required packages : wget
+# Required packages : wget nkf
 # Description : Repricate web site. The config file is in ~/cfg.*/env.
 proj=$1
 source ~/cfg.*/env/$proj.env > /dev/null 2>&1 || {
@@ -13,15 +13,15 @@ source ~/cfg.*/env/$proj.env > /dev/null 2>&1 || {
 dir=~/.var/$proj
 exp=/var/www/$proj
 [ -d $dir ] || mkdir $dir
-[ -h $exp ] || sudo ln -sf $dir $exp 
+[ -h $exp ] || sudo ln -sf $dir $exp
 cd $dir
 if [ "$index" ]; then
     [ -e $index ] && rm $index
     wget $url/$index
     while read line; do
         file=${line#*=}
-        [ -e $file ] || echo $file #wget $url/$file 
-    done < <(grep "file=" $index)
+        [ -e $file ] || wget $url/$file
+    done < <(grep "$file" $index|nkf -Lu)
 else
     wget $url
 fi
