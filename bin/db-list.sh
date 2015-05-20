7f562846ae64bb579a7164f3ea3f33be260f5fb7
Koji Omata <omata@naoj.org>
2015-05-20 12:40:07 -1000

    db-list: show selected fields;

--- bin/db-list.sh
+++ bin/db-list.sh
@@ -3,5 +3,23 @@
 # Required tables: *
 # Desctiption: show table list or table entry
 . func.getpar
-_usage "[table]" < <(db-tables)
-db-exec "select id from $1;"|grep .
+_usage "[table] (field(=cond))" < <(db-tables)
+tbl=$1;shift
+_temp _tmp_tbl
+db-exec "select id from $tbl;"|grep . > $_tmp_tbl || { db-tables; exit 1; }
+
+case "$1" in
+    *=*)
+        whr="where ${1%=*} = '${1#*=}'"
+        db-exec "select id from $tbl $whr;"|grep .|sort -u
+        ;;
+    '')
+        cat $_tmp_tbl
+        ;;
+    *)
+        {
+            db-exec "select $1 from $tbl;" ||\
+                db-exec "pragma table_info($tbl);"
+        } | grep .| sort -u
+        ;;
+esac
