86de4a4e1af4dae0504a6d63b65afe08dca15656
Koji Omata <omata@naoj.org>
2015-05-31 12:46:45 -1000

    add ret-auth for decripted pw;

---
+++ bin/ret-auth.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#alias auth
+# Required packages: expect
+# Required scripts: func.getpar db-trace crypt-de
+# Required tables: auth
+# Description: Get auth info
+. func.getpar
+_usage "[id]"
+id=$1;shift
+[ "$id" ] && eval "$(db-trace $id auth)"
+crypt-init
+[[ "$password" == jA0EA* ]] && password=$(crypt-de <<< "$password")
+echo $password
--- opt/vpn/cfg-ipsec.sh
+++ opt/vpn/cfg-ipsec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Required scripts: func.getpar db-list db-trace route-ipsec
+ # Required scripts: func.getpar db-list db-trace ret-auth route-ipsec
 # Required tables: subnet(network,netmask,vpn) vpn(login) login(command,user,password,host)
 # Description: generate ipsec configulation text (for /etc/vpnc/default.conf)
 . func.getpar
@@ -9,19 +9,13 @@ eval "$(db-trace $vid vpn)"
 [ "$id" ] || _abort "No such id"
 echo "IPSec gateway $(search-ip $host)"
 echo "IPSec ID $user"
-echo "IPSec secret $password"
+echo "IPSec secret $(ret-auth $auth)"
 #Uncomment line below for ver.5.3 usage
 echo "Vendor netscreen"
 # IKE Authmode hybrid
-eval "$(db-trace $LOGNAME auth)"
-if [ "$password" ]; then
-    pw=$(crypt-de <<< "$password")
-else
-    pw=$1
-fi
 echo "Xauth username $LOGNAME"
-echo "Xauth password $1"
+echo "Xauth password $(ret-auth $vid-$LOGNAME)"
 #Xauth password <password>
 set - $(route-ipsec $route)
-echo "Target Networks $*"
+#echo "Target Networks $*"
 echo "Nat Traversal Mode natt"
