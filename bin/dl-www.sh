5f973a19357859ada613bcaded93ead7b5b02a3b
Koji Omata <omata@naoj.org>
2015-06-03 04:34:16 -1000

    get newer files;

--- bin/dl-www.sh
+++ bin/dl-www.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 # Required packages : wget nkf
 # Description : Repricate web site. The config file is in ~/cfg.*/env.
-proj=$1
-source ~/cfg.*/env/$proj.env > /dev/null 2>&1 || {
+usage(){
+    #Show Usage and List
     while read cfg; do
         str="$str $(basename $cfg .env)"
     done < <(ls ~/cfg.*/env/*.env)
     echo "Usage: ${0##*/} [project]"
     echo "  $str"
     exit 1
-} 
+}
+proj=$1
+source ~/cfg.*/env/$proj.env 2> /dev/null || usage
 dir=~/.var/$proj
 exp=/var/www/html/$proj
 [ -d $dir ] || mkdir $dir
 [ -h $exp ] || sudo ln -sf $dir $exp
 cd $dir
 if [ "$index" ]; then
-    [ -e $index ] && rm $index
-    wget $url/$index
+    wget -N $url/$index
     echo "Retriving files in $index"
     while read line; do
         file=${line#*=}
-        [ -e $file ] || wget $url/$file
-    done < <(grep "$file" $index|nkf -Lu)
+        wget -N $url/$file
+    done < <(grep "file=" $index|nkf -Lu)
 else
-    wget $url
+    wget -N $url
 fi
 
