0560b01045f421deb7c8c0c29bffdd8cb2c0389b
Koji Omata <omata@naoj.org>
2015-06-05 11:50:56 -1000

    if db is newer than csv, do nothing;

--- bin/db-update.sh
+++ bin/db-update.sh
@@ -3,6 +3,23 @@
 # Required tables: mac ssl ssh
 # Description: update databases
 . func.msg
+latest-csv(){
+    local file
+    shopt -s nullglob
+    for file in ~/{cfg.*,utils}/db/db-$base*.csv;do
+        stat -c%Y $file
+    done|sort|tail -1
+}
+oldest-db(){
+    local file
+    for file in ~/.var/db-*.sq3;do
+        stat -c%Y $file
+    done|sort|head -1
+}
+dt=$(oldest-db)
+ct=$(latest-csv)
+[ "$dt" -gt "$ct" ] && exit
+_warn "DB($(date -d@$dt)) is older than CSV($(date -d@$ct))"
 set - $(table-ends)
 _warn "Database update for $*"
 sql-make $*|db-exec
