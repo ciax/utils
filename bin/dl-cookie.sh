32371ea992c5f0ff2ad09a1310e6d7ab2f2852e4
Koji Omata <omata@naoj.org>
2015-06-06 20:27:10 -1000

    cookie location file name was changed;

--- bin/dl-cookie.sh
+++ bin/dl-cookie.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 id=$1
-urlfile=~/cfg.*/etc/cookie.$1.url
+urlfile=~/cfg.*/etc/cookie_location.$1.txt
 sqlfile=~/.var/cookie.$id.sq3
 cookie=~/.var/cookie.$id.txt
 [ -s $urlfile ] || {
     echo "Usage: dl-cookie [user]" 1>&2
-    for i in ~/cfg.*/etc/cookie.*.url;do
-        i=${i#*cookie.}
+    for i in ~/cfg.*/etc/cookie_location.*.txt;do
+        i=${i#*location.}
         echo "    ${i%.*}" 1>&2
     done
     exit
