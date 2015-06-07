17aec0e6ea4d0bf5bf2222e615e135844a9ff3df
Koji Omata <omata@naoj.org>
2015-06-07 08:54:35 -1000

    info-date2sec:param format is changed; dl-kml: use info-date2sec;

--- bin/dl-kml.sh
+++ bin/dl-kml.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 # Original script to get content using Firefox cookies. by Jean-Sebastien Morisset (http://surniaulula.com/)
 # Required packages: sqlite3 curl
+# Required scripts: info-date2sec
 # Get KML by Firefox (Need firefox)
 # Darwin is Mac OS-X
 #alias kml
-conv_duration(){ # print 20XX-XX-XX startsec endsec
-    local day=${1:-$(date +%d)}
-    local mon=${2:-$(date +%m)}
-    local dur=${3:-1}
-    case $(uname) in
-        Linux) startday="-d $mon/$day";;
-        Darwin) startday="-j $(printf %02d%02d0000 $mon $day)";;
-    esac
-    local start=$(date $startday +%s)
-    local end=$(( start + 86400*dur )) #One Day is 86400sec
-    local day=$(date $startday +%F) # 20XX-XX-XX
-    echo "$start $end ${day}_$dur"
-
-}
-[ "$1" ]||{ echo "Usage: dl-kml [day|-] (month) (days)"; exit; }
-[ $1 = - ] && shift
+[ "$1" ]||{ echo "Usage: dl-kml [(month/)day|-]"; exit; }
+eval "$(info-date2sec $*)"
 # Make URL and DL file name
 site="https://maps.google.com/locationhistory/b/0/kml"
-set - $(conv_duration $*)
-url="$site?startTime=${1}000&endTime=${2}000"
-outfile=~/.var/history-$3.kml
+url="$site?startTime=${start}000&endTime=${end}000"
+outfile=~/.var/history-$date.kml
 # Convert cookie
 cookie=~/.var/cookie.txt
 [ -s $cookie ] || { echo "No cookie file" 1>&2; exit; }
@@ -33,3 +19,4 @@ user_agent=~/.var/user_agent.txt
 [ -s $user_agent ]|| { echo "No user agent file" 1>&2; exit; }
 # Get file
 curl -b $cookie -A "$(<$user_agent)" -o $outfile $url
+
--- bin/info-date2sec.sh
+++ bin/info-date2sec.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-# Description: Convert Date to Unix Second
-# Usage: info-date2sec [month] (day)
+# Description: Convert Date (Beggining the daty) to Unix Second
+# Usage: info-date2sec [(month/)day|-]
 #alias d2s
-mon=${1:-$(date +%m)}
-day=${2:-$(date +%d)}
-case $(uname) in
-    Linux)
-        date -d "$mon/$day" +%s
-    ;;
-    Darwin) # Mac OSX
-        date -j $(printf "%02d%02d0000" $mon $day) +%s
-        ;;
-    *)
-        exit;;
-esac
+conv_duration(){ # print 20XX-XX-XX startsec endsec
+    local date=$1
+    case "$1" in
+        '') date=$(date +%m/%d);;
+        */*) date=$1;;
+        *) date="$(date +%m)/$1";;
+    esac
+    case $(uname) in
+        Linux) startday="-d $date";;
+        Darwin) startday="-j $(printf %02d%02d0000 ${date//// })";;
+    esac
+    start=$(date $startday +%s)
+    echo "start=$start"
+    echo "end=$(( start + 86400 ))" #One Day is 86400sec
+    echo "date=$(date $startday +%F)" # 20XX-XX-XX
+}
+[ "$1" ]||{ echo "Usage: info-date2sec [(month/)day|-]"; exit; }
+[ $1 = - ] && shift
+conv_duration $*
