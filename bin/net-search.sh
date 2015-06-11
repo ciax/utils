7b1924166c069a322d7e930d94f1afb258b0a869
Koji Omata <omata@naoj.org>
2015-06-10 14:53:11 -1000

    -r option is not exclusive;

--- bin/net-search.sh
+++ bin/net-search.sh
@@ -4,25 +4,16 @@
 # Description: search IP addres by mac
 #alias mac
 . func.getpar
-xopt-r(){ #Refresh arp
+opt-r(){ #Refresh arp
     eval "$(info-net)"
     _msg "Scannig network ($cidr)"
     nmap -sn $cidr > /dev/null 2>&1
     arp -n|grep -v incomplete > $alist
 }
-opt-s(){ #Set to /etc/hosts
-    egrep -v " ($hreg)$" /etc/hosts > $hosts
-    if [ -s $hlist ] ; then
-        cat $hlist >> $hosts
-        msg="Add to /etc/hosts"
-    else
-        msg="Delete from /etc/hosts"
-    fi
-    _overwrite /etc/hosts $hosts || msg="No changes on /etc/hosts"
-    _msg "$msg"
-}
+opt-s(){ hset=1; } #Set to /etc/hosts
 alist=~/.var/arplist.txt
 _usage "[host].."
+_exe_opt
 declare -A hos
 for host ; do
     mac=$(search-mac $host)
@@ -35,11 +26,21 @@ for host ; do
     fi
 done
 [ "$hreg" ] || exit 1
-[ -f $alist ] || xopt-r
+[ -f $alist ] || opt-r
 _temp hlist hosts
 while read ip type mac other ; do
     host=${hos[$mac]}
     echo "Find $ip for $host ($mac)" 1>&2
     echo "$ip    $host" >> $hlist 
 done < <(egrep -i "($mreg)" $alist)
-_exe_opt
+# /etc/hosts rewreite
+[ "$hset" ] || exit
+egrep -v " ($hreg)$" /etc/hosts > $hosts
+if [ -s $hlist ] ; then
+    cat $hlist >> $hosts
+    msg="Add to /etc/hosts"
+else
+    msg="Delete from /etc/hosts"
+fi
+_overwrite /etc/hosts $hosts || msg="No changes on /etc/hosts"
+_msg "$msg"
