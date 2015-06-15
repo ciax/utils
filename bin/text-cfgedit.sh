8d610ed8e94a3365368852ba55cfecfa1acbb81c
Koji Omata <omata@naoj.org>
2015-06-15 11:11:23 -1000

    add cfgedit for set new value in cfg files;

--- bin/text-cfgedit.sh
+++ bin/text-cfgedit.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 # Required scripts: func.getpar
-# Description: Append STDIN to an original file which is described as a comment in it.
+# Description: Comment out lines in original file and append new line which is listed in STDIN
 #              No overwrite if the line exists in original file.
-# Usage: text-append < file
-# Example: cfg-hosts | text-append (STDIN indludes output file name "#/etc/hosts")
-#alias append
+# Usage: text-cmtout < file
+#alias cfgedit
 . func.getpar
 _usage "<input>"
 _temp infile outfile
 rem=$( tee $infile | grep "^#/" )
 file=${rem#*#}
 [ "$file" ] || _abort "No output file name"
-cat $file > $outfile
 while read line; do
     [ "$line" ] || continue
-    egrep -q "$line" $file && continue
-    echo "$line" >> $outfile
-done < <(egrep -v "^#" $infile)
+    set - $line
+    if new=$(egrep "^$1" $infile); then
+        echo "#$line" >> $outfile
+    else
+        echo "$line" >> $outfile
+    fi
+done < $file
+grep -v "^#/" $infile >> $outfile
 if cmp -s $outfile $file ; then
     user=$(_fuser $file)
     sudo install -o $user $outfile $file
-    _msg "Add to $file"
+    _msg "$file is modified"
 else
     _msg "No changes on $file"
 fi
