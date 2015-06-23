fe9ff91bee6c4a76dc5b04de4363baecef79ae4d
Omata <omata@naoj.org>
2015-06-23 10:26:38 -1000

    wol: activate feature(-s);

--- bin/net-wakeonlan.sh
+++ bin/net-wakeonlan.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #alias wol
-# Required packages(Debian,Raspbian,Ubuntu): wakeonlan
+# Required packages(Debian,Raspbian,Ubuntu): wakeonlan ethtool
 # Required scripts: func.getpar search-mac
 # Description: make network devices wake up
 . func.getpar
-_usage "[host]" $(list-hosts)
-#wakeonlan $(search-mac $1)
+xopt-s(){
+    if sudo ethtool eth0 | grep -q 'Wake-on: d' ; then
+        sudo ethtool -s eth0 wol g
+        _warn "Wake on lan is set"
+    fi
+}
+_usage "(-s) [host]" $(list-hosts)
+
+wakeonlan $(search-mac $1)
