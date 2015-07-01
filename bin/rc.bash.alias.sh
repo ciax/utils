4d126b15523f8d5a8fbc0c3430fe718b8ca16b9e
Koji Omata <omata@naoj.org>
2015-07-01 10:52:22 -1000

    rc.bash.alias is divided into rc.bash.rund and itself;

--- bin/rc.bash.alias.sh
+++ bin/rc.bash.alias.sh
@@ -1,57 +1,5 @@
 #!/bin/bash
-### Set alias/func for human interface
-# Generate alias by pick up '#alias XXX' line from each files
-self-alias(){
-    grep '^#alias' ~/bin/* > ~/.var/tempfile
-    while read head name par; do
-        eval "alias $name='${head%:*}${par:+ $par}'"
-    done < ~/.var/tempfile
-    rm ~/.var/tempfile
-}
-# Edit this file and update alias/func
-ae(){
-    file=rc.bash.alias.sh
-    pushd ~/utils/bin >/dev/null
-    unalias $(egrep '^alias' $file|cut -d ' ' -f2|cut -d '=' -f1|tr '\n' ' ')
-    unset -f $(egrep '^[a-z]+' $file|cut -d '(' -f1|tr '\n' ' ')
-    emacs $file
-    source $file
-    unset file
-    popd >/dev/null
-}
-# File registration
-reg(){
-    file-register $*
-    self-alias
-}
-# Grep recursive
-gr(){
-    [ "$1" ] || return
-    if [[ "$1" =~ [A-Z] ]]; then
-        grep -rn "$*" *
-    else
-        grep -irn "$*" *
-    fi
-}
-# Switch user
-sb(){
-    sudo -s ${1:+-u $1}
-}
-# Search alias/func
-ag(){
-    alias|grep -i ${1:-.}
-    set|egrep "^[-_a-zA-Z]+\(\)"|grep -i ${1:-.}
-}
-# Search env/var
-eg(){
-    (set;env)|egrep "^[-_a-zA-Z]+="|sort -u|grep -i ${1:-.}
-}
-# Search process
-psg(){
-    ps -ef|grep -i ${1:-.}
-}
-
-## Aliasing
+### Set alias for human interface
 # General Commands
 alias update='git-update;db-update'
 alias mo='more'
@@ -79,5 +27,3 @@ alias pki='pkg install'
 alias pku='pkg upd'
 alias pkw='pkg where'
 alias pkf='pkg files'
-
-self-alias
