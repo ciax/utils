fcc98dd4b4c870713dbd77f4803f4744566cdd16
Koji Omata <omata@naoj.org>
2015-08-05 09:12:17 -1000

    cfg-hosts: adapt to multiple self subnet, add -a(show all) option;

--- bin/cfg-hosts.sh
+++ bin/cfg-hosts.sh
@@ -4,14 +4,23 @@
 # Description: generate hosts file
 # Usage: cfg-hosts > /etc/hosts
 . func.getpar
+opt-a(){ #Show all list
+    where="host.host_ip != ''"
+}
 xopt-s(){ #Set to /etc/hosts
     $0 | _overwrite /etc/hosts || _warn "No changes on /etc/hosts"
 }
 _usage "(subnet)" $(db-list subnet)
+_exe_opt
+[ "$where" ] || {
+    for net in ${1:-$(net-name)}; do
+        subnet="$subnet and host.subnet != '$net'"
+    done
+    where="host.resolv == 'hosts' or (host.resolv =='dns'$subnet)"
+}       
 echo "#/etc/hosts"
 echo "127.0.1.1       $(hostname)"
 echo "127.0.0.1       localhost.localdomain   localhost"
-net=${1:-$(net-name)}
 IFS='|'
 db-exec <<EOF | while read a b c sub ip host domain; do echo "$a.$b.$(($c+$sub)).$ip    $host   $host.$domain";done
 select
@@ -20,8 +29,9 @@ from host
     inner join subnet on host.subnet=subnet.id
     inner join domain on subnet.domain=domain.id
 where
-    host.resolv == 'hosts' or (host.subnet != '$net' and host.resolv == 'dns')
+    $where
 order by subnet.network,
+      subnet.sub_ip,
       length(host.host_ip),
       host.host_ip
 ;
