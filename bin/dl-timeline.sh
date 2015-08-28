d7baa111e80797c29b0eb2769c72e4b2c5fb7014
Koji Omata <omata@naoj.org>
2015-08-28 10:45:01 -1000

    add dl-timeline;

---
+++ bin/dl-timeline.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Required packages: sqlite3 curl
+# Required scripts: info-date2sec conv-cookie
+# Get KML by Firefox (Need firefox)
+# Darwin is Mac OS-X
+#alias kml
+[ "$1" ]||{ echo "Usage: dl-timeline [tag] ((month/)day|-day)"; exit; }
+tag=$1;shift
+eval "$(info-date2sec ${1:-0})"
+# Make URL and DL file name
+time="startTime=${sec}000&endTime=$(( sec + 86400 ))000" # 1day=86400sec
+site="https://www.google.com/maps/timeline"
+url="$site?authuser=0&$time"
+dir=${STORE:-~/.var}
+[ -d $dir/location ] || mkdir $dir/location
+outfile=$dir/location/history-$tag-$date.kml
+# Check cookie
+cookie=~/.var/cookie.$tag.txt
+[ -s $cookie ] || { echo "No cookie file" 1>&2; exit; }
+user_agent=~/.var/user_agent.txt
+for file in ~/cfg.*/etc/user_agent.$tag.txt; do
+    if [ -s $file ]; then
+        ln -sf $file $user_agent
+        break
+    fi
+done
+# Check user_agent
+[ -s $user_agent ]|| { echo "No user agent file" 1>&2; exit; }
+# Get file
+curl -b $cookie -A "$(<$user_agent)" -o $outfile $url
