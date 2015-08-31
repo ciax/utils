09e32c35582b4b53f0977af58ce18708ccfb6a5d
Koji Omata <omata@naoj.org>
2015-08-30 17:31:42 -1000

    info-date2sec will also output google date;

--- bin/info-date2sec.sh
+++ bin/info-date2sec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Description: Convert Date (Beggining the daty) to Unix Second
-# Usage: info-date2sec [(month/)day|(-day)]
-#alias d2s
+# Description: Convert Date (Beggining the daty) to Unix Second,Google date.
+# Usage: info-date [(month/)day|(-day)]
+#alias d2g
 conv_duration(){ # print 20XX-XX-XX startsec endsec
     local date=$1
     case "$1" in
@@ -14,8 +14,12 @@ conv_duration(){ # print 20XX-XX-XX startsec endsec
         Linux) setopt="-d \"$date${dif:+ $1day}\"";;
         Darwin) setopt="-j $(printf %02d%02d0000 ${date//// })${dif:+ -v$1d}";;
     esac
+    year=$(eval "date $setopt +%Y")
+    month=$(eval "date $setopt +%-m")
+    day=$(eval "date $setopt +%-d")
     echo date=$(eval "date $setopt +%F") # 20XX-XX-XX
     echo sec=$(eval "date $setopt +%s")
+    echo gdate="!1i$year!2i$(( $month - 1))!3i$day"
 }
-[ "$1" ]||{ echo "Usage: info-date2sec [(month/)day|(-day)]"; exit; }
+[ "$1" ]||{ echo "Usage: info-date [(month/)day|(-day)]"; exit; }
 conv_duration $*
