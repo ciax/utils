a289c93fd2ae4741382819c5ec70e4d8f9400373
Koji Omata <omata@naoj.org>
2015-09-10 10:34:14 -1000

    sys-ntp: get default server name from /etc/ntp.conf;

--- bin/sys-ntp.sh
+++ bin/sys-ntp.sh
@@ -2,11 +2,12 @@
 #alias ntp
 # Required packages: ntp ntpdate
 # sys-ntp (-s) (server)
-
+# general server is 'pool.ntp.org'
+[ -f /etc/ntp.conf ] || { echo "No ntp config file"; exit; }
 if [ "$1" = "-s" ] ; then
-    server=${2:-pool.ntp.org}
+    [ "$2" ] || set $(grep '^server' /etc/ntp.conf|head -1)
     sudo /etc/init.d/ntp stop
-    sudo ntpdate -b -u $server
+    sudo ntpdate -b -u $2
     sudo /etc/init.d/ntp start
     sleep 1
 fi
