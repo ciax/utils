6f63cd07ac6370c1496111725f628d578d3e30ed
Koji Omata(pct) <omata@naoj.org>
2015-10-26 17:35:02 -1000

    chx function is splitted;

---
+++ sbin/dir-chown.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#link chx
+# Usage dir-chown [dir..]
+# Description: File owner will be uniformed under the current or specified dir tree
+source func.msg
+_warn "Uniform The Owner"
+for dir in ${*:-.}; do
+    [ -d "$dir" ] || continue
+    ug=$(stat -c "%U:%G" $dir)
+    echo "`cd $dir;pwd -P`($ug)"
+    sudo chown -R $ug $dir
+done
+
