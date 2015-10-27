f406295efab520c2f4ba72ddbff9313bab70e1f8
Koji Omata(pct) <omata@naoj.org>
2015-10-27 08:58:15 -1000

    func.temp: remove sudo;

--- bin/func.temp.sh
+++ bin/func.temp.sh
@@ -38,21 +38,17 @@ _overwrite(){ # Overwrite if these are different. [dst_file] <src_file>, return 
     fi
     if [ ! -e $dstfile ] ; then
         dir=$(dirname $dstfile)
-        user=$(_fuser $dir)
-        sudo -u $user mkdir -p "$dir"
-        sudo mv $srcfile $dstfile
-        sudo chown $user $dstfile
+        mkdir -p "$dir"
+        mv $srcfile $dstfile
         _warn "$dstfile is created"
-    elif sudo cmp -s $srcfile $dstfile ; then
+    elif cmp -s $srcfile $dstfile ; then
         rm $srcfile
         return 1
     else
-        user=$(_fuser $dstfile)
         _verbose "file diff" && diff $dstfile $srcfile 1>&2
         chmod --reference=$dstfile $srcfile
-        sudo mv -b $dstfile ~/.trash/ || _warn "Failed backup $dstfile"
-        sudo mv $srcfile $dstfile
-        sudo chown $user $dstfile
+        mv -b $dstfile ~/.trash/ || _warn "Failed backup $dstfile"
+        mv $srcfile $dstfile
     fi
 }
 
