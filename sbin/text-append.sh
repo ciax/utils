c7209742b09db635e4cc1d626e663a6e33747f56
Koji Omata(pingu) <koji.omata@gmail.com>
2015-10-27 02:18:26 -1000

    move text-append to sbin;

--- sbin/text-append.sh
+++ sbin/text-append.sh
@@ -3,13 +3,13 @@
 # Description: Append STDIN to an original file which is described as a comment in it.
 #              No overwrite if the line exists in original file.
 # Usage: text-append < file
-# Example: cfg-hosts | text-append (STDIN indludes output file name "#/etc/hosts")
+# Example: cfg-hosts | text-append (STDIN indludes output file name "#file /etc/hosts")
 #alias append
 . func.getpar
 _usage "<input>"
 _temp infile outfile
-rem=$( tee $infile | grep "^#/" )
-file=${rem#*#}
+rem=$( tee $infile | grep "^#file" )
+file=${rem#* }
 [ "$file" ] || _abort "No output file name"
 cat $file > $outfile
 while read line; do
