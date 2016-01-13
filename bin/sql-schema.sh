b3abbd2a59631a468642a55b1883dce261d41f27
Koji Omata <omata@naoj.org>
2016-01-12 14:44:16 -1000

    RDB-tsv: field name format is changed. Forign key must have '*'. ex. name -> *name, name(ref) -> name*ref;

--- bin/sql-schema.sh
+++ bin/sql-schema.sh
@@ -5,49 +5,61 @@
 
 # TSV file rule:
 #  The file location:  ~/utils/db/db-(table name).tsv
-#  Field string format: !name(table:key)
+#  Field string format: "!name", "*name" "name*table:key"
 #    ! : if the field string has '!', it is 'primary key'.
-#    name : treated as field name, if this matches with 'table name' of another file,
+#    name : treated as field name, if name have prefix of '*',
 #           it is treated as a refarence table name of the 'foreign key'
 #    table : you can specify the refarence table instead of the 'name'.
 #    key : reference key can be specified, otherwise 'id' will be used.
 #    The available charactors for 'field name' are [a-zA-Z0-9] and '_'
 . func.getpar
 schema(){
-    local tbl=$(table-core $1) || return 1
+    local tbl=$(table-core $1) || return 1 #return if file dosn't exist
     [[ "$tables" =~ $tbl ]] && return
     tables="$tables $tbl"
     local drop="drop table if exists $tbl;"
     local create="create table $tbl ("
-    local pkeys=''
-    local fkeys=''
+    local pkeys='' # primary keys
+    local fkeys='' # foreign keys
     _temp tmpline
     egrep "^!" db-$tbl.tsv|head -1|tr ",\t" "\n" > $tmpline
     while read col; do
-        local field="${col%(*}"
-        if [[ $field =~ '!' ]] ; then
-            field=${field#!}
-            pkeys="${pkeys:+$pkeys,}$field"
-        fi
-        local ref="${col#*(}";ref="${ref%)*}"
-        local rtable="${ref%:*}";rtable=${rtable:-$field}
-        if [[ $ref =~ ':' ]]; then
-            local rkey="${ref#*:}"
-        else
-            local rkey="id"
-        fi
+        # Add to Primary key list
+        case "$col" in
+            !*)
+                local field=${col#!}
+                pkeys="${pkeys:+$pkeys,}$field"
+                ;;
+            *"*"*)
+                # Add to Refence key list
+                local ref=${col#*\*}
+                local field=${col%\**}
+                field=${field:-$ref}
+                # ref -> rtable, rkey
+                local rtable="${ref%:*}";rtable=${rtable:-$field} 
+                if [[ $ref =~ ':' ]]; then 
+                    local rkey="${ref#*:}"
+                else
+                    local rkey="id"
+                fi
+                for rfile in $(table-core $rtable); do
+                    fkeys="$fkeys,foreign key('$field') references $rfile('$rkey')"
+                    reftbl="$reftbl $rfile"
+                    schema $rfile
+                done
+                ;;
+            *)
+                local field=$col
+                ;;
+        esac
         create="$create'$field',"
-        for rfile in $(table-core $rtable); do
-            fkeys="$fkeys,foreign key('$field') references $rfile('$rkey')"
-            reftbl="$reftbl $rfile"
-            schema $rfile
-        done
     done < $tmpline
     echo "$drop"
     echo $create"primary key($pkeys)$fkeys);"
 }
 shopt -s nullglob
 _usage "[table] .."
+cd ~/utils/db
 tables=''
 echo "pragma foreign_keys=on;"
 for i; do
