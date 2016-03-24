6185f58375aec4eb470626d81762d2267213c226
Koji Omata <omata@naoj.org>
2016-03-24 11:41:54 -1000

    ssh-upnp: use host ip for forwarding port;

--- bin/ssh-upnp.sh
+++ bin/ssh-upnp.sh
@@ -3,6 +3,8 @@
 # Required packages: miniupnpc
 # Description: request ssh port forwarding to router
 . func.getpar
-_usage "[wan port]"
+_usage "[wan port base]"
 eval $(info-net)
-upnpc -a $hostip 22 $1 tcp
+port=$(( $1 + ${hostip##*.}))
+echo "Requesting ssh port forwarding to wan:$port"
+upnpc -a $hostip 22 $port tcp
