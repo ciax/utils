815c90f76782a0f71a9e03d8f6e6f70588c2b950
Raspi4 <koji.omata@gmail.com>
2016-07-11 23:56:27 -1000

    motion(mmal) use cfg/motion.conf;

--- opt/raspi/raspi-mmal-setup.sh
+++ opt/raspi/raspi-mmal-setup.sh
@@ -6,4 +6,4 @@ cd ~/mmal
 sudo apt-get install -y libjpeg-dev libavformat56 libavformat-dev libavcodec56 libavcodec-dev libavutil54 libavutil-dev libc6-dev zlib1g-dev libmysqlclient18 libmysqlclient-dev libpq5 libpq-dev
 wget https://www.dropbox.com/s/6ruqgv1h65zufr6/motion-mmal-lowflyerUK-20151114.tar.gz
 tar -zxvf motion-mmal-lowflyerUK-20151114.tar.gz
-
+ln -sf ~/cfg.*/etc/motion.conf .
--- opt/raspi/raspi-mmal.sh
+++ opt/raspi/raspi-mmal.sh
@@ -2,4 +2,4 @@
 # You need to join video group
 [ -d ~/mmal ] || mkdir ~/mmal
 cd ~/mmal
-sudo ./motion -c motion-mmalcam-both.conf > ~/.var/mmal.log 2>&1 &
+sudo ./motion -c motion.conf > ~/.var/mmal.log 2>&1 &
