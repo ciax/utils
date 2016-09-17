aabad01a996a3e7254afc2d2da3ed85d94d51c3b
Raspi4 <koji.omata@gmail.com>
2016-09-17 12:50:06 -1000

    raspi_ap: set both eth0 and tun0; add netutils.txt

--- opt/raspi/ap/raspi_ap.sh
+++ opt/raspi/ap/raspi_ap.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Reference: https://frillip.com/using-your-raspberry-pi-3-as-a-wifi-access-point-with-hostapd/
 # Router Setting
+
+
 IF=tun0
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-sudo iptables -t nat -A POSTROUTING -o $IF -j MASQUERADE
-sudo iptables -A FORWARD -i $IF -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i wlan0 -o $IF -j ACCEPT
+for IF in eth0 tun0; do
+    sudo iptables -t nat -A POSTROUTING -o $IF -j MASQUERADE
+    sudo iptables -A FORWARD -i $IF -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+    sudo iptables -A FORWARD -i wlan0 -o $IF -j ACCEPT
+done
 sudo systemctl restart dnsmasq
 sudo /usr/sbin/hostapd -B /etc/hostapd/hostapd.conf
 
