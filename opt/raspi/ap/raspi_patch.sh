b1e3db7e0af06e60bf58d1731a0abd4e1703fa8b
Raspi4 <koji.omata@gmail.com>
2016-09-17 15:25:40 -1000

    add sys.forward setting;

--- opt/raspi/ap/raspi_patch.sh
+++ opt/raspi/ap/raspi_patch.sh
@@ -2,3 +2,4 @@
 # Reference: https://frillip.com/using-your-raspberry-pi-3-as-a-wifi-access-point-with-hostapd/
 # diff -ur /etc .|grep -v Only > raspi_ap.patch
 sudo patch -u -p0 -d /etc < ~/utils/opt/raspi/ap/raspi_ap.patch
+sudo sysctl -p /etc/sysctl.conf
