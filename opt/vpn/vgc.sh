58fc533fe660f7a6d661fdd91fae72b59cd387de
Raspi5 <koji.omata@gmail.com>
2016-11-09 11:42:34 -1000

    add vpngate script;

---
+++ opt/vpn/vgc.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# http://qiita.com/mt08/items/9f357be12c126d8976b3
+# git clone https://github.com/mt08xx/vgc.git
+
+OPT_DAEMON= #--daemon
+
+VPNGATE_CONF=/etc/openvpn/vpngate.conf
+USER_VPNGATE=/etc/openvpn/user_vpngate.txt
+VPNGATE_CSV=~/.vpngate.csv
+CMD=$1
+CMD=${CMD,,} #lowercase
+
+if [ ! -z $CMD ] ; then
+    if [ $CMD == "update" ] ; then OPT_UPDATE=update ; fi
+    if [ $CMD == "clean" ] ; then sudo rm $USER_VPNGATE $VPNGATE_CSV $VPNGATE_CONF; echo cleaned ; exit ; fi
+fi
+
+
+function create_userpassfile() {
+    echo -e "vpn\nvpn" | sudo tee $USER_VPNGATE
+    sudo chmod 700 $USER_VPNGATE
+}
+
+function download_vpngate_csv() {
+    wget http://www.vpngate.net/api/iphone/ -O - | tail -n +3 | grep Japan > $VPNGATE_CSV
+    return 0
+}
+
+mkconf(){
+    cut -d ',' -f 15 | base64 -d | sed -e "s/#auth-user-pass/auth-user-pass\ \/${USER_VPNGATE//\//\\\/}/g" | sudo tee $VPNGATE_CONF > /dev/null
+}
+
+#
+if [ -e $USER_VPNGATE ]; then
+    echo found $USER_VPNGATE
+else
+    create_userpassfile
+fi
+
+#download_vpngate_csv
+
+# Show Server List
+#cat $VPNGATE_CSV | cut -d ',' -f 1-10 | column -s, -t  | less -#2 -N -S
+
+# Select Server
+#echo -n "Enter #"
+#read
+# openvpn is running ???
+pidof openvpn > /dev/null ; if [ $? -eq 0 ]; then sudo killall openvpn ; sleep 5; fi
+sudo rm -f $VPNGATE_CONF
+while read;do
+    mkconf
+    exec sudo openvpn ${OPT_DAEMON} --config $VPNGATE_CONF --connect-retry-max 1
+done < $VPNGATE_CSV
