9bbeb7431d8db82fe5ad7641bbb496441eb0730c
Omata@leonid <omata@naoj.org>
2017-02-21 15:19:30 -1000

    text-format: separate processing for xml and html;

--- bin/text-format.sh
+++ bin/text-format.sh
@@ -3,18 +3,26 @@
 # Required scripts: func.getpar
 # Description: Overwrite if these are different.
 #alias fm
+safe_ow(){
+    if [ -s $1 ] ; then
+        _overwrite $2 < $1 && _msg "Update $2"
+    fi
+}
 . func.getpar
 _usage "[file] ..."
 _temp temp
 for file ;do
     ext=${file#*.}
     case $ext in
-        html|xml)
+        xml)
             echo "XML Processing"
             xmllint --format $file > $temp
-            if [ -s $temp ] ; then
-                _overwrite $file < $temp && _msg "Update $file"
-            fi
+            safe_ow $temp $file
+            ;;
+        html)
+            echo "HTML Processing"
+            xmllint --html $file > $temp
+            safe_ow $temp $file
             ;;
         js)
             echo "JS Processing"
