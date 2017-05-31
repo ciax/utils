574acbd291363b39d97156f8541d891d8f4d68ab
Omata@leonid <omata@naoj.org>
2017-05-31 11:56:38 -1000

    ngvpn: don't use exit for taking over the $TUN to caller;

--- opt/vpn/client-ng-openvpn.sh
+++ opt/vpn/client-ng-openvpn.sh
@@ -12,22 +12,25 @@ xopt-d(){ #disconnect
         echo "openvpn was killed"
     fi
 }
+wait_tun(){
+    for (( i=0; i < 20; i++));do
+        set - $(ifconfig |grep tun)
+        if [[ $1 =~ tun ]]; then
+            export TUN=$1
+            echo "TUN interface is $TUN"
+            sudo iptables -t nat -A POSTROUTING -o $TUN -j MASQUERADE
+            return
+        else
+            echo -n '.'
+            sleep 1
+        fi
+    done
+    echo "Tun doesn't exist"
+    xopt-d
+    return 1
+}
 _usage "[vpnhost]" $( _basename_list ~/cfg.*/ngvpn/*.conf)
 sel=$1
 set - ~/cfg.*/ngvpn/$sel.conf
 cd ${1%/*}
-sudo openvpn --daemon --config $sel.conf --pull 2>/dev/null|tee -a ~/.var/ngvpn-$sel.log || exit
-for (( i=0; i < 20; i++));do
-    set - $(ifconfig |grep tun)
-    if [[ $1 =~ tun ]]; then
-        export TUN=$1
-        echo "TUN interface is $TUN"
-        sudo iptables -t nat -A POSTROUTING -o $TUN -j MASQUERADE
-        exit
-    else
-        echo -n '.'
-        sleep 1
-    fi
-done
-echo "Tun doesn't exist"
-xopt-d
+sudo openvpn --daemon --config $sel.conf --pull 2>/dev/null|tee -a ~/.var/ngvpn-$sel.log && wait_tun
