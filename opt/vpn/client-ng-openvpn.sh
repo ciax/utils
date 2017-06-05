05d63d729db32025554d4129e81712e4d4093d85
Raspi5 <koji.omata@gmail.com>
2017-06-05 11:37:25 -1000

    client-ng-openvpn: kill proc shows counter;

--- opt/vpn/client-ng-openvpn.sh
+++ opt/vpn/client-ng-openvpn.sh
@@ -4,33 +4,44 @@
 # Required scripts: func.getpar show-syslog
 # Description: client for netgear router openvpn
 . func.getpar
-xopt-d(){ #disconnect
-    pidof openvpn > /dev/null
-    if [ $? -eq 0 ]; then
-        sudo killall openvpn
-        sleep 5
-        echo "openvpn was killed"
+retry(){
+    for (( i=0; i < 20; i++));do
+        $* && return
+        echo -n '.'
+        sleep 1
+    done
+    return 1
+}
+no_proc(){
+    ! pidof $1 > /dev/null
+}
+kill_proc(){
+    no_proc $1 && return
+    sudo killall $1
+    retry no_proc $1 && echo "$1 was killed"
+}
+chk_tun(){
+    set - $(ifconfig |grep tun)
+    [[ $1 =~ tun ]] || return 1
+    export TUN=$1
+    sudo iptables -t nat -A POSTROUTING -o $TUN -j MASQUERADE
+}
+exe_vpn(){
+    set - ~/cfg.*/ngvpn/$sel.conf
+    cd ${1%/*}
+    sudo openvpn --daemon --config $sel.conf --pull 2>/dev/null|tee -a ~/.var/ngvpn-$sel.log || return 1
+    if retry chk_tun ; then
+        echo "TUN interface is $TUN"
+    else
+        echo "TUN doesn't exist"
+        kill_proc openvpn
+        return 1
     fi
 }
-wait_tun(){
-    for (( i=0; i < 20; i++));do
-        set - $(ifconfig |grep tun)
-        if [[ $1 =~ tun ]]; then
-            export TUN=$1
-            echo "TUN interface is $TUN"
-            sudo iptables -t nat -A POSTROUTING -o $TUN -j MASQUERADE
-            return
-        else
-            echo -n '.'
-            sleep 1
-        fi
-    done
-    echo "Tun doesn't exist"
-    xopt-d
-    return 1
+xopt-d(){
+    kill_proc openvpn
 }
 _usage "[vpnhost]" $( _basename_list ~/cfg.*/ngvpn/*.conf)
 sel=$1
-set - ~/cfg.*/ngvpn/$sel.conf
-cd ${1%/*}
-sudo openvpn --daemon --config $sel.conf --pull 2>/dev/null|tee -a ~/.var/ngvpn-$sel.log && wait_tun
+kill_proc openvpn
+exe_vpn
