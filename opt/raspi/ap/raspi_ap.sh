26a4a0cea0da943d505a7c7b3c006f23faf34f68
Raspi5 <koji.omata@gmail.com>
2017-06-06 22:16:43 -1000

    raspi_ap: safe delete nat;

--- opt/raspi/ap/raspi_ap.sh
+++ opt/raspi/ap/raspi_ap.sh
@@ -5,7 +5,7 @@
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 pidof hostapd || sudo /usr/sbin/hostapd -B /etc/hostapd/hostapd.conf
 sudo systemctl restart dnsmasq
-sudo iptables -t nat -F
+sudo iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 #    sudo iptables -A FORWARD -i $IF -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 #    sudo iptables -A FORWARD -i wlan0 -o $IF -j ACCEPT
