022568e307f5e3b743a3fc5c9dcf070f5a240c55
Koji Omata(pingu) <koji.omata@gmail.com>
2017-06-07 15:42:37 -1000

    add func.auth;

---
+++ bin/func.auth.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Required scripts: func.getpar db-trace crypt-de
+# Required tables: auth
+# Description: Get auth info
+. func.msg
+_show_pw(){
+    [[ "$password" == jA0EA* ]] && crypt-de || cat
+
+}
+_auth(){ # Set $user and $password
+    id=$1;shift
+    [ "$id" ] && eval "$(db-trace $id auth)"
+    crypt-init
+    password=$(echo $password | _show_pw)
+    mid=${password:1:-1}
+    ast=${mid//?/*}
+    _warn "Using Password (${password/$mid/$ast})"
+}
--- bin/search-auth.sh
+++ bin/search-auth.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 #alias auth
 # Required packages: expect
-# Required scripts: func.getpar db-trace crypt-de
-# Required tables: auth
+# Required scripts: func.auth
 # Description: Get auth info
 . func.getpar
-show(){
-    [[ "$password" == jA0EA* ]] && crypt-de || cat
-}
+. func.auth
 _usage "[id]" $(db-list auth)
-id=$1;shift
-[ "$id" ] && eval "$(db-trace $id auth)"
-crypt-init
-password=$(echo $password | show)
-mid=${password:1:-1}
-ast=${mid//?/*}
-_warn "Using Password (${password/$mid/$ast})"
+_auth $1
 echo $password
