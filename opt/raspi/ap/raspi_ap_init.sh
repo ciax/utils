43a7359914d12d6db86f6ea594c55dc9444fa679
Raspi5 <koji.omata@gmail.com>
2017-06-14 14:12:03 -1000

    ap setting files update;

--- opt/raspi/ap/raspi_ap_init.sh
+++ opt/raspi/ap/raspi_ap_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Reference: https://frillip.com/using-your-raspberry-pi-3-as-a-wifi-access-point-with-hostapd/
 # diff -ur /etc .|grep -v Only > raspi_ap.patch
+sudo apt-get install hostapd dnsmasq
 sudo patch -u -p0 -d /etc < ~/utils/opt/raspi/ap/raspi_ap.patch
 sudo sysctl -p /etc/sysctl.conf
