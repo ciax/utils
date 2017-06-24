ece554a45121a858a7384e2a98f52e5abc7e628d
Koji Omata(pingu) <koji.omata@gmail.com>
2017-06-23 21:23:11 -1000

    info-net: add mac address;

--- bin/info-net.sh
+++ bin/info-net.sh
@@ -2,16 +2,16 @@
 # Required commands: ip netstat
 # Recommended app: ipcalc
 shopt -s nullglob
-while read cidr x b x c x x s hostip x; do
-    [ $b = "$netif" -a "$s" = "src" ] && break
-    [ $cidr = 'default' ] && { gw=$b; netif=$c; }
-done < <(ip route|sort -r)
-read x x x bcast x < <(ip addr|grep $netif$)
-read subnet x netmask x < <(netstat -nr|egrep -v ^0|grep $netif)
+set - $(ip route|sort -r)
+gw=$3
+cidr=$6
+netif=$8
+set - $(ifconfig $netif | egrep '(inet |ether)')
 echo "netif=$netif"
-echo "hostip=$hostip"
-echo "subnet=$subnet"
-echo "netmask=$netmask"
+echo "hostip=$2"
+echo "subnet=${cidr%/*}"
+echo "netmask=$4"
 echo "cidr=$cidr"
-echo "bcast=$bcast"
+echo "bcast=$6"
 echo "gw=$gw"
+echo "mac=$8"
