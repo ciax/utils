d5561f589641a28d242e31141a2c92697c6228cf
Omata@leonid <omata@naoj.org>
2017-11-17 17:46:33 -1000

    update for newer version;

--- bin/link-self.sh
+++ bin/link-self.sh
@@ -2,6 +2,7 @@
 # Required scripts: func.link func.temp
 # Description: make links to the specific dirs categorized by file type 
 # Desctiption: Files in cfg.*/ utils/ and current dirs will be classified into 'bin','db' ..
+# (Ubuntu need to install realpath)
 # "Usage: ${0##*/} [DIR..] | [SRC..]"
 #alias slink
 . func.link
@@ -10,7 +11,7 @@ _warn "File Self Registering"
 _temp linklist
 egrep -Hr "^[#;]link(\((.+,)*($DIST|$HOSTNAME)(,.+)*\)|) " ~/{cfg.*,utils} $*|sort -u > $linklist
 while read spath dst;do
-    src=$(realpath ${spath%:*})
+    src=${spath%:*}
     if [[ $src =~ \.(sh|pl|py|rb|awk|exp|js)$ ]]; then
         dst=~/bin/$(basename $dst)
     fi
