a3478bbd4c224e2fd7078362e43ed5c6a0044034
Koji Omata(pingu) <koji.omata@gmail.com>
2017-11-21 09:04:40 -1000

    modify comment;

--- opt/netdev/net-setarp.sh
+++ opt/netdev/net-setarp.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 # Require scripts: func.getpar search-mac
 # Description: Setting IP address for Devices
+#alias setarp
 . func.getpar
-opt-r(){
-#   -r: arping only(root)
+opt-r(){ # arping only(root)
     sudo arping $mac;echo OK
 }
-opt-l(){
-#   -l: setip for Lantronix
+opt-l(){ # setip for Lantronix/APC PDU
     sudo arp -s $ip $mac
     ping $ip -s 113
 }
-opt-p(){
-#   -p: setip for PLIZZI
+opt-p(){ # setip for PLIZZI
     sudo arp -s $ip $mac
     telnet $ip 1 >& /dev/null
     telnet $ip 9999
