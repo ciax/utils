7674045daa3552a5828b24d316ece4ff878798f3
Omata@leonid <omata@naoj.org>
2017-11-30 12:24:24 -1000

    text-grep: remove function specific commands;

--- bin/text-grep.sh
+++ bin/text-grep.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Description: grep recursive
 #alias gr
-local opt;
-[ "$1" ] || return
+[ "$1" ] || exit
 while [[ "$1" == -* ]]; do
     opt="$opt $1"
     shift
 done
-local reg="$1";shift
+reg="$1";shift
 [[ "$reg" =~ [A-Z] ]] || opt="$opt -i"
 egrep -rn $opt --exclude-dir=.git "$reg"|egrep -v '^.+:[0-9]+: *#'|egrep --color $opt "$reg"
