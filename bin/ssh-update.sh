99f32f5b7108bac088ba6990f348a348c4c46d49
Koji Omata(pingu) <koji.omata@gmail.com>
2018-05-06 17:55:07 -1000

    rename ssh-merge -> ssh-update;

--- bin/ssh-update.sh
+++ bin/ssh-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Required scripts: ssh-join func.ssh
-# Description: join to the group which opject host is involved
-# Usage: ssh-merge (user@host) ..
+# Description: update partnership with all accounts listed in autholized_keys
+# Usage: ssh-update (user@host) ..
 #   If no args, then hosts are picked up from authorized_keys
 . func.ssh
 ssh-join $(_ssh_validate $(_ssh_auth_mates))
