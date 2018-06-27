7e1f4846fd83987bd9b03c6bea34e8624494172a
CIAX <ciax@naoj.org>
2018-06-27 10:45:03 -1000

    gen2: iid reading/setting is separated;

--- opt/gen2/gen2init-ciax.sh
+++ opt/gen2/gen2init-ciax.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Prepare for CIAX daemon
 gen2prt iid > /dev/null 2>&1 || {
+    echo "* init"
     gen2cmd init
+    echo "* login"
     gen2cmd login
+    echo "* set ID"
     gen2cmd setinst 1
+    echo "* logout"
     gen2cmd logout
 }
 gen2red
