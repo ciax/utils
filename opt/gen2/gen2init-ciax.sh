312a3199c7da1691f481b00994deef69992920f8
CIAX <ciax@naoj.org>
2018-11-27 13:44:05 -1000

    Bugfix: gen2init-ciax: wrong usage of gen2cmd;

--- opt/gen2/gen2init-ciax.sh
+++ opt/gen2/gen2init-ciax.sh
@@ -2,13 +2,13 @@
 # Prepare for CIAX daemon
 gen2prt iid > /dev/null 2>&1 || {
     echo "* init"
-    gen2cmd init
+    gen2mkcmd init|gen2cmd
     echo "* login"
-    gen2cmd login
+    gen2mkcmd login|gen2cmd
     echo "* set ID"
-    gen2cmd setinst 1
+    gen2mkcmd setinst 1|gen2cmd
     echo "* logout"
-    gen2cmd logout
+    gen2mkcmd logout|gen2cmd
 }
 gen2red
 
