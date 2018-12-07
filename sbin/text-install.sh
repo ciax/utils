4c952f8b25822e7006e190b690f7de30f72cf7dd
Omata@leonid <omata@naoj.org>
2018-12-06 16:51:07 -1000

    Update: rename sys-hosts -> cfg-hosts (just show);         refile text-install;

--- sbin/text-install.sh
+++ sbin/text-install.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 # Description: overwrite the file in a hash directive (#file) by stdin
 . func.sudo
-if [ ! -t 0 ] ; then
-    _temp srcfile
-    while read line; do
-        if [[ $line =~ ^#file ]] ; then
-            dstfile=${line#* }
-        else
-            echo "$line" >> $srcfile
-        fi
-    done
-elif [ ! "$srcfile" ] ; then
-    _usage " < input file"
-fi
+_usage "<input>"
+_temp srcfile
+while read line; do
+    if [[ $line =~ ^#file ]] ; then
+        dstfile=${line#* }
+    else
+        echo "$line" >> $srcfile
+    fi
+done
 _overwrite_s $dstfile $srcfile
 
