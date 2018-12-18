f3dbd7c76f8204d00d0ea853a03016c74b28a736
Omata@leonid <omata@naoj.org>
2018-12-18 10:57:11 -1000

    Bugfix: text-grep: ignored files which doesn't contain __FILE__;

--- bin/text-grep.sh
+++ bin/text-grep.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # Description: grep recursive
 #alias gr
-[ "$1" ] || exit
+[ "$1" ] || { echo "Usage: text-grep [regexp] (option) (files...)"; exit 1; }
+# Remove lines below specified word per file
+rm_below(){
+    egrep -Hnm 1 -B 9999 "$1" $(egrep $ropt -l "$1")
+    grep -Hn . $(egrep $ropt -L "$1")
+}
 while [[ "$1" == -* ]]; do
     opt="$opt $1"
     shift
 done
+ropt='-r --exclude-dir=.git'
 reg="$1";shift
+# Ignore case unless $reg contains upper case
 [[ "$reg" =~ [A-Z] ]] || opt="$opt -i"
-# Exclude ^# line (actual line format:  filename:num:content
-head='^.+-[0-9]+-'
-egrep -rn --exclude-dir=.git -m 1 -B 9999 __FILE__ $* |
+head='^.+[-:][0-9]+[-:]'
+rm_below __FILE__ |
+    # Exclude ^# line (actual line format:  filename:num:content
     egrep -v "$head *#" |
     egrep "$head.*$reg" |
     egrep --color $opt "$reg"
