c9aaa07b3f5e86dee10744ff2746fbb46cbc12fc
Omata@leonid <omata@naoj.org>
2018-12-18 15:18:02 -1000

    Update: text-grep: can specify target files;

--- bin/text-grep.sh
+++ bin/text-grep.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 # Description: grep recursive
 #alias gr
-[ "$1" ] || { echo "Usage: text-grep [regexp] (option) (files...)"; exit 1; }
+[ "$1" ] || { echo "Usage: text-grep (option) [regexp] (files...)"; exit 1; }
 # Remove lines below specified word per file
 rm_below(){
-    egrep -Hnm 1 -B 9999 "$1" $(egrep $ropt -l "$1")
-    grep -Hn . $(egrep $ropt -L "$1")
+    sep=__FILE__
+    target=${*:-'-r --exclude-dir=.git'}
+    egrep -Hnm 1 -B 9999 $sep $(egrep -l $sep $target)
+    grep -Hn . $(egrep -L $sep $target)
 }
 while [[ "$1" == -* ]]; do
     opt="$opt $1"
     shift
 done
-ropt='-r --exclude-dir=.git'
 reg="$1";shift
 # Ignore case unless $reg contains upper case
 [[ "$reg" =~ [A-Z] ]] || opt="$opt -i"
 head='^.+[-:][0-9]+[-:]'
-rm_below __FILE__ |
+rm_below $*|
     # Exclude ^# line (actual line format:  filename:num:content
     egrep -v "$head *#" |
     egrep "$head.*$reg" |
