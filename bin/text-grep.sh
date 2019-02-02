cba98b273ed7d3a65f46001bce9e1f0bea52c371
Koji Omata(pingu) <koji.omata@gmail.com>
2019-02-02 13:10:33 -1000

    Update: text-format: show both Before and After of separator;

--- bin/text-grep.sh
+++ bin/text-grep.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-# Description: grep recursive
+# Description: grep recursive and show before and after in splited by separator
 #alias gr
-[ "$1" ] || { echo "Usage: text-grep (option) [regexp] (files...)"; exit 1; }
 # Remove lines below specified word per file
-rm_below(){
-    sep=__FILE__
-    target=${*:-'-r --exclude-dir=.git'}
-    egrep -Hnm 1 -B 9999 $sep $(egrep -l $sep $target)
-    grep -Hn . $(egrep -L $sep $target)
+list(){
+    egrep -$1 $SEP $target
 }
+split(){
+    egrep -Hnm 1 -${1:0:1} 9999 $SEP $inc | egrep -v "$head#" |
+        search > $temp && {
+        echo "-------- $1 [$SEP] --------"
+        form < $temp
+    }
+    rm $temp
+}
+search(){
+    egrep $opt "$head.*$reg"
+}
+form(){
+    sed -E "s/$head/\1:\2:\t/"|
+        sort | expand -t 24 |
+        egrep --color $opt "$reg"
+}
+# Main
+[ "$1" ] || { echo "Usage:(SEP=?) text-grep (option) [regexp] (files...)"; exit 1; }
 while [[ "$1" == -* ]]; do
     opt="$opt $1"
     shift
@@ -16,9 +30,11 @@ done
 reg="$1";shift
 # Ignore case unless $reg contains upper case
 [[ "$reg" =~ [A-Z] ]] || opt="$opt -i"
-head='^.+[-:][0-9]+[-:]'
-rm_below $*|
-    # Exclude ^# line (actual line format:  filename:num:content
-    egrep -v "$head *#" |
-    egrep $opt "$head.*$reg" |
-    egrep --color $opt "$reg"
+head='^(.+)[-:]([0-9]+)[-:] *'
+SEP=${SEP:-__FILE__}
+target=${*:-'-r --exclude-dir=.git'}
+temp=~/.var/.gtemp.txt
+# File list that excludes $SEP
+exc=$(list L) && egrep -Hnv '^ *#' $exc | search | form
+# File list that includes $SEP
+inc=$(list l) && { split Before; split After; }
