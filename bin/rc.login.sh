b4469a68792e712078cce7fa8c7c95dbdd4808a4
Koji Omata(pingu) <koji.omata@gmail.com>
2019-03-26 09:17:53 -1000

    Update: rc.login/logout: combine them into rc.logio and symlink separately;

--- bin/rc.login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-shopt -s nullglob
-log=~/.var/log/login.log
-date > $log
-for i in ~/bin/rc.login.*;do
-    source $i
-done >> $log 2>&1 &
--- bin/rc.logout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-shopt -s nullglob
-log=~/.var/log/logout.log
-date > $log
-for i in ~/bin/rc.logout.*;do
-    source $i
-done >> $log 2>&1 &
