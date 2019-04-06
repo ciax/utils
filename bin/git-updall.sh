76cbd9ca815ca68d21c930693cfb949e8b8a8481
Koji Omata(pingu) <koji.omata@gmail.com>
2019-04-06 11:47:00 -1000

    Update: git-updall: add 'git fetch' to get remote repository;

--- bin/git-updall.sh
+++ bin/git-updall.sh
@@ -4,6 +4,7 @@
 xopt-b(){ main $* > /dev/null 2>&1 & }
 main(){
     cd ${1:-.}
+    git fetch
     crnt=$(git branch --contains|cut -d' ' -f2)
     others="$(git branch|grep -v '*')"
     for i in $others $crnt; do
