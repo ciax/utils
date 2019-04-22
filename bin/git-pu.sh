84e901a1caf3945d2f08e923737818c7ba31790e
Omata@leonid <omata@naoj.org>
2019-04-22 08:48:04 -1000

    Bugfix: git-pu: current branch had been changed every time for some reason. so branch forced get back to crrent one;

--- bin/git-pu.sh
+++ bin/git-pu.sh
@@ -2,19 +2,11 @@
 # Description: git-pull/push for all branches
 #link git-pull
 #link git-push
-branches(){
-    # list ids which exists in remote branch and local branch
-    #  current branch will be placed on the tail to prevent double checkout
-    for i in $(git branch -a|tr -d ' *'|cut -d/ -f3|sort|uniq -d);do
-        [ $i = "$1" ] && shift || echo $i
-    done
-    [ "$1" ] || echo $crnt
-}
 cmd=${0##*-}
 git fetch -a
 git remote prune origin
 crnt=$(git branch|grep '*'|tr -d ' *')
-for i in $(branches $crnt); do
+for i in $(git branch -a|tr -d ' *'|cut -d/ -f3|sort|uniq -d); do
     git checkout $i
     git $cmd
 done
