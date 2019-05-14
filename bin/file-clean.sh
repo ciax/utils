80f29beb39a4b773f3695b96cae5e7cbaf21b089
Omata@leonid <omata@naoj.org>
2019-05-14 11:53:27 -1000

    Bugfix: file-clean: typo miss (~/bin was ~bin);

--- bin/file-clean.sh
+++ bin/file-clean.sh
@@ -37,7 +37,7 @@ _clrdir(){
 }
 clrcmd=_do_here
 _exe_opt
-[ $PWD = ~/bin ] || def=~bin
+[ $PWD = ~/bin ] || def=~/bin
 for i in ${*:-.} $def;do
     if [[ $PWD =~ $HOME ]] ; then
         pushd $i >/dev/null
