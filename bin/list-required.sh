9745ee9180f4d7a35c13ac33c333da5fbba91e1d
Omata@raspi5 <koji.omata@gmail.com>
2019-06-10 09:30:19 -1000

    Bugfix: list-required: search dir should be ~/bin;

--- bin/list-required.sh
+++ bin/list-required.sh
@@ -5,9 +5,9 @@
 # Format: "# Required [type]s: item1 item2 ...
 #alias req
 . func.getpar
-_usage "[type]" $( egrep -oh "^# *Required [^:(]+" *|cut -d' ' -f 3|sort -u)
+_usage "[type]" $( egrep -oh "^# *Required [^:(]+" ~/bin/* | cut -d' ' -f 3 | sort -u)
 while read line;do
     for i in ${line#*:};do
         echo "${i%(*}"
     done
-done < <(egrep -ih "^# *re.* $1(\(.*,?$DIST,?.*\)|):" ~/bin/*|tr -d '*') | sort -u
+done < <(egrep -ih "^# *re.* $1(\(.*,?$DIST,?.*\)|):" ~/bin/* | tr -d '*') | sort -u
