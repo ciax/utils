29dd753ae247857abe094d9291c775013b41e095
Omata@raspi6 <koji.omata@gmail.com>
2019-06-11 18:14:07 -1000

    Update: unsetup-utils: remove all cfg.* dirs;

--- bin/unsetup-utils.sh
+++ bin/unsetup-utils.sh
@@ -2,7 +2,7 @@
 # Description: uninstall utils environment
 bashrc-reset
 cd
-for i in bin .trash .var/cache;do
+for i in bin .trash .var/cache cfg.*;do
     [ -d $i ] && rm -rf $i
 done
 echo "Unsetup complete!"
