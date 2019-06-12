9c6360eab229acb8a867b2a0ad69729cb6502765
Omata@raspi6 <koji.omata@gmail.com>
2019-06-11 19:28:08 -1000

    Update: unsetup-utils: remove ~/utils;

--- bin/unsetup-utils.sh
+++ bin/unsetup-utils.sh
@@ -2,7 +2,7 @@
 # Description: uninstall utils environment
 bashrc-reset
 cd
-for i in bin .trash .var/cache cfg.*;do
+for i in bin .trash .var/cache cfg.* utils;do
     [ -d $i ] && rm -rf $i
 done
 echo "Unsetup complete!"
