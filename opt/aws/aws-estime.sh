8ed2a9b821e4622d5631373b2fa42733d3ff3772
Omata@leonid <omata@naoj.org>
2021-04-11 15:14:16 -1000

    Update: aws-estime implemented;

--- opt/aws/aws-estime.sh
+++ opt/aws/aws-estime.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-[ "$1" ] | { echo "Usage:aws-esttime -(se)"; exit; }
-
-echo "Start time:"
-echo "Current time:"
-echo "Elapsed time:"
-echo "Processing rate(file/s):"
-echo "Estimated remaining time:"
-echo "Estimated date of finish:"
-
+#[ "$1" ] || { echo "Usage:aws-esttime -(se)"; exit; }
+. aws-conf
+elap(){
+    set - $(date -ud @$1 +"%j %R:%S")
+    echo "$(( $1 -1 ))day $2"
+}
+start=$(stat -c %Z $dellist)
+last=$(stat -c %Z $dellog)
+(( elapsed=last-start ))
+allsize=$(stat -c %s $dellist)
+delsize=$(stat -c %s $dellog)
+rate=$(echo "scale=5; $delsize/$elapsed" | bc)
+remain=$(echo "scale=0; ($allsize - $delsize)/$rate" | bc)
+(( finish=last+remain ))
+echo -n "Start time: "
+date -ud @$start
+echo -n "Last updated: "
+date -ud @$last
+echo -n "Elapsed time: "
+elap $elapsed
+echo "Processing rate(byte/s): $rate"
+echo -n "Estimated remaining time: "
+elap $remain
+echo -n "Estimated date of finish: "
+date -ud @$finish
 
