83365f54bf9436af320016a598a45688bcc57931
Omata@raspberrypi <koji.omata@gmail.com>
2021-07-08 04:11:23 +0900

    info-net: add ipv6 address;

--- bin/info-net.sh
+++ bin/info-net.sh
@@ -13,7 +13,7 @@ do
         cidr=$h
     fi
 done < <(ip route)
-eval $(ifconfig eth0|sed -e 's/  /\n/g'|egrep '^(i|n|br|ether)'|tr ' ' =)
+eval $(ifconfig eth0|egrep -v 'link'|sed -e 's/  /\n/g'|egrep '^(i|n|br|ether)'|tr ' ' =)
 echo "netif=$netif"
 echo "hostip=$inet"
 echo "subnet=${cidr%/*}"
@@ -22,3 +22,4 @@ echo "cidr=$cidr"
 echo "bcast=$broadcast"
 echo "gw=$gw"
 echo "mac=$ether"
+echo "ipv6=$inet6"
