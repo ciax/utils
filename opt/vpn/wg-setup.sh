5ac5cdeac26256b029d0a1b322525b71560184fc
Omata@raspberrypi <koji.omata@gmail.com>
2021-07-08 10:41:41 +0900

    Update wg-setup;

--- opt/vpn/wg-setup.sh
+++ opt/vpn/wg-setup.sh
@@ -6,48 +6,53 @@
 #
 . func.getpar
 mkprv(){
-    if [ ! -e $prvkey ] ; then
-	wg genkey # > $prvkey
-	#    chmod 600 $prvkey
-	echo "prvkey was generated"
+    mkdir -p -m 700 ~/.wg
+    cd ~/.wg
+    if [ ! -e privkey ] ; then
+	wg genkey > privkey
+	chmod 600 privkey
+	echo "privkey was generated"
     fi
 }
-mkif(){
+setvar(){
+    eval $(info-net)
+    myaddr=$(myaddr)
+    pubkey=$(wg pubkey < privkey)
+    host=$(hostname)
+}
+prif(){
     echo "[Interface]"
     echo "ListenPort = 51820"
-    echo "PrivateKey = $(< $prvkey)"
+    echo "PrivateKey = $(< privkey)"
     echo "Address = $myaddr/24"
     echo "PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $netif -j MASQUERADE"
     echo "PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $netif -j MASQUERADE"
 }
-mkpeer(){
+prpeer(){
     echo "[Peer]"
     echo "PublicKey = $pubkey"
     echo "AllowedIPs = $myaddr/32, $cidr"
     echo "EndPoint = [$ipv6]:51820"
-}    
+}
 myaddr(){
     IFS=.
     set - $subnet
     echo "10.0.0.1$3"
 }
-setvar(){
-    eval $(info-net)
-    myaddr=$(myaddr)
-    pubkey=$(wg pubkey < $prvkey)
-    host=$(hostname)
+getpeer(){
+    peer=$host.peer
+    touch ~/cfg.*/etc/$peer
+    ln -sf ~/cfg.*/etc/*.peer .
+    prpeer > $peer
+    rm $peer
 }
 
 setfw(){
     sudo sysctl -w net.ipv4.ip_forward=1
 }
-prvkey=~/.ssh/privkey
 mkprv
 setvar
-mkpeer > $host.peer
-mkif
+getpeer
+prif
 echo
-for i in *.peer; do
-    [ $i = $host.peer ] && continue
-    cat $i
-done
+[ "$(echo *.peer)" ] && cat *.peer
