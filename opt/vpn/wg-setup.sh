617aaf6360fbefde82bddcc72f9ffa741ee3db3f
Omata@raspi4-a <koji.omata@gmail.com>
2021-07-10 12:06:07 +0900

    Update wg-setup (not complete);

--- opt/vpn/wg-setup.sh
+++ opt/vpn/wg-setup.sh
@@ -5,58 +5,86 @@
 # Description: setting nat table
 #
 . func.getpar
+# Options
+xopt-s(){ #Write to /etc/wireguard/wg0.conf"
+    $0 | text-update
+    sudo sysctl -w net.ipv4.ip_forward=1
+}
+xopt-c(){ #Generate client QR code
+    svpvkey=$privkey
+    privkey=privkey.cli
+    mkprv
+    setnet
+    prcliif
+    prclipeer > ~/cfg.def/etc/$host.cli.peer
+}
+# Subroutines
+prnat(){
+    echo "iptables -$1 FORWARD -i wg0 -j ACCEPT; iptables -t nat -$1 POSTROUTING -o $netif -j MASQUERADE"
+}
+# Shared Procedures
 mkprv(){
     mkdir -p -m 700 ~/.wg
     cd ~/.wg
-    if [ ! -e privkey ] ; then
-	wg genkey > privkey
-	chmod 600 privkey
-	echo "privkey was generated"
+    [ "$privkey" ] || { echo "No prv file setting"; return; }
+    if [ ! -e $privkey ]; then
+	wg genkey > $privkey
+	chmod 600 $privkey
+	echo "$privkey was generated"
     fi
+    pubkey=$(wg pubkey < $privkey)
 }
-setvar(){
+setnet(){
     eval $(info-net)
-    myaddr=$(myaddr)
-    pubkey=$(wg pubkey < privkey)
+    IFS=.
+    set - $subnet
+    IFS=
+    myaddr="10.0.0.1$3"
     host=$(hostname)
 }
-prif(){
+# Printing Interface Part
+prcliif(){
     echo "[Interface]"
-    echo "ListenPort = 51820"
-    echo "PrivateKey = $(< privkey)"
-    echo "Address = $myaddr/24"
-    echo "PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $netif -j MASQUERADE"
-    echo "PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $netif -j MASQUERADE"
+    echo "PrivateKey = $(< $privkey)"
+    echo "Address = 10.0.0.100/24"
+    echo "DNS = 8.8.8.8"
 }
-prpeer(){
+prsrvif(){
+    echo "[Interface]"
+    echo "PrivateKey = $(< $privkey)"
+    echo "Address = $myaddr/24"
+    echo "ListenPort = 51820"
+    echo "PostUp = $(prnat A)"
+    echo "PostDown = $(prnat D)"
+}
+# Printing Peer Part
+prclipeer(){
+    echo "[Peer]"
+    echo "PublicKey = $pubkey"
+    echo "AllowedIPs = 0.0.0.0/0"
+    echo "EndPoint = [$ipv6]:51820"
+}
+prsrvpeer(){
     echo "[Peer]"
     echo "PublicKey = $pubkey"
     echo "AllowedIPs = $myaddr/32, $cidr"
     echo "EndPoint = [$ipv6]:51820"
 }
-myaddr(){
-    IFS=.
-    set - $subnet
-    echo "10.0.0.1$3"
-}
-getpeer(){
-    peer=$host.peer
-    touch ~/cfg.def/etc/$peer
+setpeer(){
+    peer=$1.peer
+    prsrvpeer > ~/cfg.def/etc/$peer
     ln -sf ~/cfg.*/etc/*.peer .
-    prpeer > $peer
     rm $peer
 }
-
-setfw(){
-    sudo sysctl -w net.ipv4.ip_forward=1
-}
-prcfg(){
-    prif
+prsvcfg(){
+    echo "#file /etc/wireguard/wg0.conf"
+    prsrvif
     echo
     [ "$(echo *.peer)" ] && cat *.peer
 }
-
+_usage
+privkey=privkey
 mkprv
-setvar
-getpeer
-prcfg
+setnet
+setpeer $host
+prsvcfg
