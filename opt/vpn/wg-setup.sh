5ea52fc6ad27cf90de528aaa008e2ad4a98243fa
Omata@raspi4-a <koji.omata@gmail.com>
2021-07-15 02:25:38 +0900

    Update: info-net: add global IP and hostname; wg-setup: server address is ipv4;

--- opt/vpn/wg-setup.sh
+++ opt/vpn/wg-setup.sh
@@ -11,27 +11,18 @@ xopt-s(){ #Write to /etc/wireguard/wg0.conf"
     sudo sysctl -w net.ipv4.ip_forward=1
 }
 xopt-c(){ #Generate client
-    mkprv privkey.cli
-    myaddr=10.0.0.100
-    prclpeer > ~/cfg.def/etc/wg0.client.peer
+    mkclcfg
     prclcfg
 }
 xopt-q(){ #Generate client QR code
     xopt-c | qrencode -t ansiutf8
 }
-server(){
-    mkprv privkey
-    tunnet
-    prsvpeer > ~/cfg.def/etc/wg0.$host.peer
-    prsvcfg
-}
-
 # Subroutines
 prnat(){
     echo "iptables -$1 FORWARD -i wg0 -j ACCEPT; iptables -t nat -$1 POSTROUTING -o $netif -j MASQUERADE"
 }
 # Shared Procedures
-mkprv(){
+mkkeys(){
     [ "$1" ] || { echo "No prv file setting"; return; }
     privkey=$1
     if [ ! -e $privkey ]; then
@@ -40,36 +31,56 @@ mkprv(){
 	echo "$privkey was generated"
     fi
     pubkey=$(wg pubkey < $privkey)
-    host=$(hostname)
+    eval $(info-net)
 }
 tunnet(){
-    eval $(info-net)
     IFS=.
     set - $subnet
     IFS=
-    myaddr="10.0.0.1$3"
+    tunaddr="10.0.0.1$3"
 }
-# Printing Interface Part
+# Printing Common Part
 prif(){
     echo "[Interface]"
     echo "PrivateKey = $(< $privkey)"
-    echo "Address = $myaddr/24"
+    echo "Address = $tunaddr/24"
 }
-# Printing Peer Part
-prclpeer(){
+prpeer(){
     echo "[Peer]"
     echo "PublicKey = $pubkey"
-    echo "AllowedIPs = $myaddr/32"
+}
+# Printing CL/SV Peers
+prclpeer(){
+    prpeer
+    echo "AllowedIPs = $tunaddr/32"
 }
 prsvpeer(){
-    echo "[Peer]"
-    echo "PublicKey = $pubkey"
-    echo "AllowedIPs = $myaddr/32, $cidr"
+    prpeer
+    echo "AllowedIPs = 0.0.0.0/0"
+    echo "EndPoint = $global:51820"
+}
+prv6peer(){
+    prpeer
+    echo "AllowedIPs = $tunaddr/32, $cidr"
     echo "EndPoint = [$ipv6]:51820"
 }
-prsvcfg(){
+# Making Config Files
+mkclcfg(){ #Generate client
+    mkkeys privkey
+    prsvpeer > ~/cfg.def/etc/wg0.server.peer
+    mkkeys privkey.cli
+    tunaddr=10.0.0.100
+    prclpeer > ~/cfg.def/etc/wg0.client.peer
+}
+mkv6cfg(){
+    mkkeys privkey
+    tunnet
+    prv6peer > ~/cfg.def/etc/wg0.$hostname.peer
+}
+# Printing Config Files
+prv6cfg(){
     ln -sf ~/cfg.*/etc/*.peer .
-    rm wg0.$host.peer
+    rm wg0.{$hostname,server}.peer
     echo "#file /etc/wireguard/wg0.conf"
     prif
     echo "ListenPort = 51820"
@@ -80,9 +91,11 @@ prsvcfg(){
 prclcfg(){
     prif
     echo "DNS = 8.8.8.8"
-    cat ~/cfg.def/etc/wg0.$host.peer
+    cat ~/cfg.def/etc/wg0.server.peer
 }
+# Main
 mkdir -p -m 700 ~/.wg
 cd ~/.wg
 _usage
-server
+mkv6cfg
+prv6cfg
