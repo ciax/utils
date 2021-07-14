24c998a7c3d922c2d2f527578b32cccf3d17c1ec
Omata@raspi4-a <koji.omata@gmail.com>
2021-07-14 16:40:59 +0900

    Update: wg-setup: separate -c and -q(qr code);

--- opt/vpn/wg-setup.sh
+++ opt/vpn/wg-setup.sh
@@ -10,11 +10,22 @@ xopt-s(){ #Write to /etc/wireguard/wg0.conf"
     $0 | text-update
     sudo sysctl -w net.ipv4.ip_forward=1
 }
-xopt-c(){ #Generate client QR code
+xopt-c(){ #Generate client
     mkprv privkey.cli
+    myaddr=10.0.0.100
     prclpeer > ~/cfg.def/etc/client.peer
-    prclcfg | qrencode -t ansiutf8
+    prclcfg
 }
+xopt-q(){ #Generate client QR code
+    xopt-c | qrencode -t ansiutf8
+}
+server(){
+    mkprv privkey
+    tunnet
+    prsvpeer > ~/cfg.def/etc/$host.peer
+    prsvcfg
+}
+
 # Subroutines
 prnat(){
     echo "iptables -$1 FORWARD -i wg0 -j ACCEPT; iptables -t nat -$1 POSTROUTING -o $netif -j MASQUERADE"
@@ -39,25 +50,16 @@ tunnet(){
     myaddr="10.0.0.1$3"
 }
 # Printing Interface Part
-prclif(){
-    echo "[Interface]"
-    echo "PrivateKey = $(< $privkey)"
-    echo "Address = 10.0.0.100/24"
-    echo "DNS = 8.8.8.8"
-}
-prsvif(){
+prif(){
     echo "[Interface]"
     echo "PrivateKey = $(< $privkey)"
     echo "Address = $myaddr/24"
-    echo "ListenPort = 51820"
-    echo "PostUp = $(prnat A)"
-    echo "PostDown = $(prnat D)"
 }
 # Printing Peer Part
 prclpeer(){
     echo "[Peer]"
     echo "PublicKey = $pubkey"
-    echo "AllowedIPs = 10.0.0.100/24"
+    echo "AllowedIPs = $myaddr/32"
 }
 prsvpeer(){
     echo "[Peer]"
@@ -69,17 +71,18 @@ prsvcfg(){
     ln -sf ~/cfg.*/etc/*.peer .
     rm $host.peer
     echo "#file /etc/wireguard/wg0.conf"
-    prsvif
+    prif
+    echo "ListenPort = 51820"
+    echo "PostUp = $(prnat A)"
+    echo "PostDown = $(prnat D)"
     [ "$(echo *.peer)" ] && cat *.peer
 }
 prclcfg(){
-    prclif
+    prif
+    echo "DNS = 8.8.8.8"
     cat ~/cfg.def/etc/$host.peer
 }
 mkdir -p -m 700 ~/.wg
 cd ~/.wg
 _usage
-mkprv privkey
-tunnet
-prsvpeer > ~/cfg.def/etc/$host.peer
-prsvcfg
+server
