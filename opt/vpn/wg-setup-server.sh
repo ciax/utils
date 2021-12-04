45f912e9927a4ef1beec53ebea0491c35083f067
Omata@debian <koji.omata@gmail.com>
2021-12-04 08:27:34 +0000

    Add: wiregurard server config generator;

---
+++ opt/vpn/wg-setup-server.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#alias mksv
+# Required packages: wireguard qrencode
+# Required scripts: func.getpar
+# Description: Making server config
+#
+# Internal IP Assignment
+#  Server: 10.0.(subnet).254
+#  Client: 10.0.(server subnet).n (n=1..253)
+. func.getpar
+# Options
+opt-s(){
+    prcfg | text-update
+}
+_usage
+# Subroutines
+prnat(){
+    echo "iptables -$1 FORWARD -i wg0 -j ACCEPT; iptables -t nat -$1 POSTROUTING -o $netif -j MASQUERADE"
+}
+# Shared Procedures
+getnet(){
+    eval $(info-net)
+    IFS=.
+    set - $subnet
+    IFS=
+    tunaddr="10.0.$3.254"
+}
+# Printing Common Part
+prif(){
+    echo "[Interface]"
+    echo "PrivateKey = $(< privkey)"
+    echo "Address = $tunaddr/16"
+}
+# Making Config Files
+prcfg(){
+    ln -sf ~/cfg.*/etc/wg0.*.peer .
+    rm wg0.$(hostname).peer
+    echo "#file /etc/wireguard/wg0.conf"
+    getnet
+    prif
+    echo "ListenPort = 51820"
+    echo "PostUp = $(prnat A)"
+    echo "PostDown = $(prnat D)"
+    [ "$(echo wg0.*.peer)" ] && { cat wg0.*.peer| grep -v EndPoint; }
+}
+# Main
+mkdir -p ~/.var/wg
+mkdir -p -m 700 ~/.wg
+cd ~/.wg
+prcfg
+_exe_opt
