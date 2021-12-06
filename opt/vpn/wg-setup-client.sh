7d8180178baf7bbd9df128996fb89c7e5f82ffee
Omata@raspi4-b <koji.omata@gmail.com>
2021-12-07 00:04:53 +0900

    Update: can connect multiple servers;

--- opt/vpn/wg-setup-client.sh
+++ opt/vpn/wg-setup-client.sh
@@ -13,9 +13,6 @@ xopt-s(){ # Write to /etc
     prcfg $servers| text-update
 }
 # Subroutines
-list_peer(){
-    ls ~/cfg.*/etc/wg0.*.peer|grep -v $HOSTNAME|cut -d. -f3|sort -u|tr $'\n' ' '
-}
 prnat(){
     echo "iptables -$1 FORWARD -i wg0 -j ACCEPT; iptables -t nat -$1 POSTROUTING -o $netif -j MASQUERADE"
 }
@@ -37,24 +34,30 @@ prif(){
 }
 # Making Config Files
 prcfg(){
-    dst=wg0.$1.peer
+    # sorting
     ln -sf ~/cfg.*/etc/wg0.*.peer .
     rm wg0.$HOSTNAME.peer
+    # printing
     echo "#file /etc/wireguard/wg0.conf"
     getnet
     prif
-    grep -v AllowedIPs $dst
-    allow="$(grep AllowedIPs $dst|cut -d, -f1)"
-    IFS=' '
-    for i in $(list_peer); do
-	allow+=", $(grep AllowedIPs wg0.$i.peer | cut -d, -f2)"
-    done
-    echo $allow
-}
+    for i ;do
+	file=wg0.$i.peer
+	[ -e $file ] || continue
+	cat $file; rm $file
+    done | head -c -1
+    if [ wg0.*.peer ]; then
+	allow=$(grep AllowedIPs wg0.*.peer | cut -d, -f2 | tr $'\n' , )
+	echo ",${allow%,}"
+    else
+	echo
+    fi
+} 
 # Main
 mkdir -p ~/.var/wg
 mkdir -p -m 700 ~/.wg
 cd ~/.wg
 servers="$*"
-_usage "[server]" $(list_peer)
+list_peer="$(ls ~/cfg.*/etc/wg0.*.peer|grep -v $HOSTNAME|cut -d. -f3|sort -u)"
+_usage "[servers]" $list_peer
 prcfg $servers
