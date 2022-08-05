2f5bf71377df8cdad28879d231ac182a09c37476
Omata@raspi4-b <koji.omata@gmail.com>
2022-08-05 13:27:17 +0900

    Add net-scan for search mac on localnet;

---
+++ bin/net-scan.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Required packages: nmap
+# Required scripts: search-mac info-net
+# Description: Scan Localnet
+#alias scl
+. func.getpar
+xopt-r(){ #Clear arp
+    for i in $(arp |grep ether| cut -d' ' -f1);do
+        sudo arp -d $i
+    done
+}
+opt-u(){ #Udp Scan
+    opt="-PU"
+}
+opt-s(){ #Syn Scan
+    opt="-PS"
+}
+opt-a(){ #Ack Scan
+    opt="-PA"
+}
+alist=~/.var/cache/arplist.txt
+_usage
+_exe_opt
+eval "$(info-net)"
+_msg "Scannig network ($cidr)"
+nmap -n -sn $opt $cidr > /dev/null 2>&1
+arp -n|grep -v incomplete | tee $alist
+
+
