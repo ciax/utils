47792c1497b341e408aa0ff27ea291be2ffb8bca
omata@irisj-sde1.mtk.nao.ac.jp <omata@irisj-sde1.mtk.nao.ac.jp>
2023-09-11 13:34:30 +0900

    Add: info-host: lookup host ip from host -> if the /etc/hosts is unavailable;

---
+++ bin/info-host.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Required scripts: func.getpar db-list db-exec
+# Required tables: host(host_ip),domain(name),subnet(network)
+# Description: lookup ip by hostname
+# Usage: info-host [hostname]
+#alias hostdb
+. func.getpar
+query(){
+    db-exec <<EOF
+select
+    subnet.network,subnet.sub_ip+host.host_ip
+from host
+    inner join subnet on host.subnet=subnet.id
+where
+    host.host_ip != '' AND host.id == '$1'
+;
+EOF
+}
+
+_usage "(hostname)" $(db-list host)
+_exe_opt
+IFS='|'
+while read a b others; do
+    echo -e "$a.$b"
+done < <(query "$1")
