176be1a8781ccf75498a5c1702a1a222a3b0318a
Omata@mini <koji.omata@gmail.com>
2023-10-24 16:53:42 +0900

    Bugfis: sys-chown: avoid to change the system files;

--- sbin/sys-chown.sh
+++ sbin/sys-chown.sh
@@ -6,8 +6,11 @@
 _warn "Uniform The Owner"
 for dir in ${*:-.}; do
     [ -d "$dir" ] || continue
+    pwd=$(cd $dir;pwd -P)
+    # Avoid to change perm in system dir
+    [[ $pwd =~ ^/(usr)*/sbin ]] && continue
     ug=$(stat -c "%U:%G" $dir)
-    echo "`cd $dir;pwd -P`($ug)"
+    echo "$pwd($ug)"
     _sudy chown -R $ug $dir
 done
 
