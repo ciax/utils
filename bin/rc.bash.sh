b1158490c4a6c5689d0496e333ea8c65219f335b
Omata@raspi4-a <koji.omata@gmail.com>
2023-12-10 04:20:41 +0900

    Update: rc.bash, test-udpfw: update required packages;

--- bin/rc.bash.sh
+++ bin/rc.bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 shopt -s nullglob
-# Recommended packages(Debian): vim byobu ranger most rclone bash-completion
+# Recommended packages(Debian,Raspbian): vim byobu ranger most rclone bash-completion
 # Boot strap
 for i in ~/bin/rc.bash.*;do . $i;done
 uniqenv PATH RUBYLIB
--- opt/vpn/wireguard/test-udpfw.sh
+++ opt/vpn/wireguard/test-udpfw.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Required packages: socat
 port=${1:-51820}
 host=${2:-localhost}
 socat -v PIPE udp-recvfrom:$port,fork &
