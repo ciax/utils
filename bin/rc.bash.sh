1deb81de1361cc1466652a311316f36a558cf274
Omata@U59 <koji.omata@gmail.com>
2024-03-27 15:10:34 +0900

    Update: add recommended packages(https://gigazine.net/news/20240327-linux-crisis-tools/);

--- bin/rc.bash.sh
+++ bin/rc.bash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 shopt -s nullglob
-# Recommended packages(Debian,Raspbian): vim byobu ranger most rclone bash-completion
+# Recommended packages(Debian,Raspbian,Ubuntu): vim byobu ranger most rclone bash-completion
+# Recommended packages: procps util-linux sysstat iproute2 numactl tcpdump linux-tools-common bcc trace-cmd nicstat ethtool tiptop cpuid msr-tools 
 # Boot strap
 for i in ~/bin/rc.bash.*;do . $i;done
 uniqenv PATH RUBYLIB
