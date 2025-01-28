b0cdfd57f69a4425a7cda5a62f2bebc6551d560b
Omata@naovm <koji.omata@gmail.com>
2025-01-28 15:50:36 +0900

    Update: init python3-full install;

--- bin/rc.bash.sh
+++ bin/rc.bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 shopt -s nullglob
-# Recommended packages(Debian,Raspbian,Ubuntu): vim byobu ranger most rclone bash-completion
+# Recommended packages(Debian,Raspbian,Ubuntu): vim byobu ranger most rclone bash-completion python3-full
 # Recommended packages(Debian,Ubuntu): cpuid msr-tools
 # Recommended packages(Ubuntu): linux-tools-common
 # Recommended packages: procps util-linux sysstat iproute2 numactl tcpdump bcc trace-cmd nicstat ethtool tiptop
