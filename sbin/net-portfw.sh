dd7d204248c87d0ac3ca371115ca1f110be2b425
Omata@raspi4-a <koji.omata@gmail.com>
2025-08-06 10:18:37 +0900

    Update: add options;

--- sbin/net-portfw.sh
+++ sbin/net-portfw.sh
@@ -8,12 +8,17 @@
 xopt-s(){ # save setting mode
     _sudy netfilter-persistent save
 }
+xopt-c(){ # clear setting
+    _sudy iptables -t nat -F
+}
+xopt-l(){ # list tables
+    _sudy iptables -t nat -vL
+}
 _usage "[port] [dstip] (dstport)"
 
-pf1=
+pf1="iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $1 -j DNAT --to-destination $2:${3:-$1}"
+pf2="iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
 
-pf1="_sudy iptables -t nat -A PREROUTING -i eth0 --dport $1 -j DNAT --to-destination $2:${3:-$1}"
-pf2="_sudy iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
-
-echo $pf1
-echo $pf2
+_sudy $pf1
+_sudy $pf2
+_sudy iptables -t nat -L
