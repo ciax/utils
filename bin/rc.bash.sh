c8d9a08d482ed5797c7e5adba5e7bdcb292e0738
Omata@bee <koji.omata@gmail.com>
2025-12-01 10:32:33 +0900

    Update: add recommended packages;

--- bin/rc.bash.sh
+++ bin/rc.bash.sh
@@ -3,7 +3,7 @@ shopt -s nullglob
 # Recommended packages(Debian,Raspbian,Ubuntu): vim byobu ranger most rclone bash-completion python3-full
 # Recommended packages(Debian,Ubuntu): cpuid msr-tools
 # Recommended packages(Ubuntu): linux-tools-common
-# Recommended packages: procps util-linux sysstat iproute2 numactl tcpdump bcc trace-cmd nicstat ethtool tiptop
+# Recommended packages: procps util-linux sysstat iproute2 numactl tcpdump bcc trace-cmd nicstat ethtool tiptop tree
 # Boot strap
 for i in ~/bin/rc.bash.*;do . $i;done
 uniqenv PATH RUBYLIB
